35110515a7467efca09403a952b7a0b748e60b77
asfand687 <asfandyar687@gmail.com>
2023-02-07 18:15:25 +0500

    added signout functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom'
 import Logo from '../assets/logo.png'
 import { RxDotFilled } from 'react-icons/rx'
 import Drawer from './Drawer'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
   const [showDrawer, setShowDrawer] = useState(false)
   const location = useLocation()
+  const navigate = useNavigate()
+  const token = localStorage.getItem("jwtToken")
+
+  const handleSignout = () => {
+    localStorage.removeItem("jwtToken")
+    navigate("/")
+  }
 
   return (
     <nav className="md:border-b border-primary pb-[2px] md:pb-[6px] shadow">
@@ -53,18 +60,31 @@ const Navbar = () => {
             <Link to="/services">services</Link>
           </li>
           </ul>
-          <div className="lg:pr-24 text-light text-base space-x-4 font-main font-light">
-            <Link to="/login">
-              <button className="rounded-full px-9 py-1 border border-primary text-primary bg-transparent hover:bg-primary hover:text-white transition-all ease-in-out duration-300">
-                Login
-              </button>
-            </Link>
-            <Link to="/register">
-              <button className="rounded-full px-9 py-1 border border-primary text-white bg-primary hover:bg-transparent hover:text-primary transition-all ease-in-out duration-300">
-                Sign Up
-              </button>
-            </Link>
-          </div>
+          {
+            token ? (
+              <div className="lg:pr-24">
+                <button
+                onClick={handleSignout}
+                  className="rounded-full px-9 py-1 border border-primary text-white bg-primary hover:bg-transparent hover:text-primary transition-all ease-in-out duration-300">
+                  Signout                 
+                </button>
+              </div>
+            ):
+          (
+            <div className="lg:pr-24 text-light text-base space-x-4 font-main font-light">
+              <Link to="/login">
+                <button className="rounded-full px-9 py-1 border border-primary text-primary bg-transparent hover:bg-primary hover:text-white transition-all ease-in-out duration-300">
+                  Login
+                </button>
+              </Link>
+              <Link to="/register">
+                <button className="rounded-full px-9 py-1 border border-primary text-white bg-primary hover:bg-transparent hover:text-primary transition-all ease-in-out duration-300">
+                  Sign Up
+                </button>
+              </Link>
+            </div>
+          )
+          }
         </article>
         <article onClick={() => setShowDrawer(!showDrawer)} className="absolute cursor-pointer flex flex-col justify-center space-y-1 top-0 right-0 bg-primary text-white py-6 px-4 md:py-8 md:px-6">
           <span className="w-6 h-[2px] bg-white"></span>
