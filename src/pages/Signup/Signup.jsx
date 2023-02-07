63e91fcacf02e7ae028ed258506c2d11b18c255c
asfand687 <asfandyar687@gmail.com>
2023-02-07 14:04:24 +0500

    added signup page

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,6 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import FormField from '../../components/FormField'
 
 const Signup = () => {
+  const navigate = useNavigate()
+  const [form, setForm] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    phone: ''
+  })
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleSignup = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(form)
+      })
+
+      setForm({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        phone: ''
+      })
+      await response.json()
+      navigate('/dashboard')
+    } 
+    catch (error) {
+      console.log(error) 
+    }
+  }
+
   return (
     <section className="py-20 flex justify-center text-white">
       <article className="flex items-center">
@@ -9,26 +50,56 @@ const Signup = () => {
         </div>
         <div className="py-16 px-12 bg-[#f9f9f9] shadow-cardService rounded-[35px] text-gray-800 font-body">
           <h3 className="pb-4">Required Field *</h3>
-          <form className="space-y-4 w-[500px] text-gray-800">
+          <form onSubmit={handleSignup} className="space-y-4 w-[500px] text-gray-800">
             <div className="w-full relative">
               <p className="absolute left-0 -top-3">*</p>
-              <input className="w-full px-6 py-3 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" type="text" placeholder="First Name" />
+              <FormField
+                type="text"
+                name="firstName"
+                placeholder="First Name"
+                handleChange={handleChange}
+                value={form.firstName}
+              />
             </div>
             <div className="w-full relative">
               <p className="absolute left-0 -top-3">*</p>
-              <input className="w-full px-6 py-3 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" type="text" placeholder="Last Name" />
+              <FormField
+                type="text"
+                name="lastName"
+                placeholder="Last Name"
+                handleChange={handleChange}
+                value={form.lastName}
+              />
             </div>
             <div className="w-full relative">
-            <p className="absolute left-0 -top-3">*</p>
-              <input className="w-full px-6 py-3 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" type="password" placeholder="Password" />
+              <p className="absolute left-0 -top-3">*</p>
+              <FormField
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  handleChange={handleChange}
+                  value={form.email}
+                />  
             </div>
             <div className="w-full relative">
-            <p className="absolute left-0 -top-3">*</p>
-              <input className="w-full px-6 py-3 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" type="email" placeholder="Email" />
+              <p className="absolute left-0 -top-3">*</p>
+              <FormField
+                type="password"
+                name="password"
+                placeholder="Password"
+                handleChange={handleChange}
+                value={form.password}
+              /> 
             </div>
             <div className="w-full relative">
-            <p className="absolute left-0 -top-3">*</p>
-              <input className="w-full px-6 py-3 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" type="tel" placeholder="Phone" />
+              <p className="absolute left-0 -top-3">*</p>
+              <FormField
+                  type="tel"
+                  name="phone"
+                  placeholder="Phone"
+                  handleChange={handleChange}
+                  value={form.phone}
+                /> 
             </div>
             <div className='w-full pt-3'>
               <button type="submit" className={`rounded-full font-main text-xl group hover:ring-1 hover:ring-primary px-12 py-3 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
