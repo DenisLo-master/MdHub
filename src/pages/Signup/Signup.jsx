f0f6ceb64303ee40d365fad11cb6614a92027110
asfand687 <asfandyar687@gmail.com>
2023-02-09 13:08:13 +0500

    removed registerationStore import

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { registerationStore } from "../../store"
 import { BsPlusCircle } from 'react-icons/bs'
 import {
   formStep,
