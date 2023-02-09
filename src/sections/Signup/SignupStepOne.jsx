2b86a2f80c5137d81483a87c78d6306bcf6bf614
asfand687 <asfandyar687@gmail.com>
2023-02-09 13:58:34 +0500

    added add famiy member

--- src/sections/Signup/SignupStepOne.jsx
+++ src/sections/Signup/SignupStepOne.jsx
@@ -5,8 +5,9 @@ import { BsPlusCircle } from 'react-icons/bs'
 const SignupStepOne = () => {
   const registerationFormData = registerationStore(state => state.registerationFormData)
   const handleRegisterationFormDataChange = registerationStore(state => state.handleRegisterationFormDataChange)
+  const selectedAccountType = registerationStore(state => state.selectedAccountType)
   return (
-    <div className="py-16 border space-y-4 px-8 shadow-cardService rounded-[35px] text-primary">
+    <div className="py-16 border space-y-4 px-8 shadow-cardService rounded-[35px] text-primary relative">
       <div className="w-full flex flex-wrap gap-y-4 gap-x-6">
         <input
           className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
@@ -58,6 +59,12 @@ const SignupStepOne = () => {
           required
         />
       </div>
+      {selectedAccountType === "family" && (
+        <div className="absolute bottom-4 left-8 flex gap-x-2 text-primary font-body items-center text-3xl cursor-pointer">
+          <BsPlusCircle />
+          <p className='text-xl'>add family member</p>
+        </div>
+      )}
     </div>
   )
 }
