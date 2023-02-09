c007fed570f7b784976bd4cb571389448a765bd7
asfand687 <asfandyar687@gmail.com>
2023-02-09 13:07:39 +0500

    made store imports dynamic

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,21 @@
 import { registerationStore } from './registerationStore'
 
+const formStep = registerationStore(state => state.formStep)
+const increaseFormStep = registerationStore(state => state.increaseFormStep)
+const decreaseFormStep = registerationStore(state => state.decreaseFormStep)
+const registerationFormData = registerationStore(state => state.registerationFormData)
+const handleRegisterationFormDataChange = registerationStore(state => state.handleRegisterationFormDataChange)
+const clearRegisterationFormData = registerationStore(state => state.clearRegisterationFormData)
+const selectedAccountType = registerationStore(state => state.selectedAccountType)
+const changeSelectedAccountType = registerationStore(state => state.changeSelectedAccountType)
+
 export {
-  registerationStore
+  formStep,
+  increaseFormStep,
+  decreaseFormStep,
+  registerationFormData,
+  handleRegisterationFormDataChange,
+  clearRegisterationFormData,
+  selectedAccountType,
+  changeSelectedAccountType
 }
