1a33325c1510053083626e72b47b4d8bfb0ed319
asfand687 <asfandyar687@gmail.com>
2023-02-13 23:15:06 +0500

    changed the login link

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     const form = new FormData(e.target)
     const loginFormData = Object.fromEntries(form)
     try {
-      const response = await fetch('http://localhost:8080/api/v1/auth/login', {
+      const response = await fetch('http://mdhub-backend.onrender.com/api/v1/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
