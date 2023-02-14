f7f316f3617afc380db0a827ffa1dec6d0a8c6c1
asfand687 <asfandyar687@gmail.com>
2023-02-14 22:06:21 +0500

    disabled buttons after firsts step

--- src/sections/Signup/SignupAccountsTypeNav.jsx
+++ src/sections/Signup/SignupAccountsTypeNav.jsx
@@ -4,16 +4,19 @@ import { registerationStore } from '../../store/registerationStore'
 const SignupAccountsTypeNav = () => {
   const changeSelectedAccountType = registerationStore(state => state.changeSelectedAccountType)
   const selectedAccountType = registerationStore(state => state.selectedAccountType)
+  const formStep = registerationStore(state => state.formStep)
   return (
-    <div className="w-full flex justify-around md:justify-between text-primary flex-wrap gap-y-4">
+    <div className={`w-full flex justify-around md:justify-between text-primary flex-wrap gap-y-4`}>
       <button
         onClick={() => changeSelectedAccountType("individual")}
         type="button"
-        className={`w-40 md:48  rounded-full text-xl hover:ring-1 hover:ring-primary px-16 py-2 border flex justify-center items-center border-primary bg-transparent ${selectedAccountType === "individual" && "bg-primary text-white"}`}
+        disabled={formStep > 0}
+        className={`w-40 md:48  rounded-full text-xl hover:ring-1 hover:ring-primary px-16 py-2 border flex justify-center items-center border-primary bg-transparent ${selectedAccountType === "individual" && "bg-primary text-white"} $`}
       >
         Individual
       </button>
       <button
+        disabled={formStep > 0}
         onClick={() => changeSelectedAccountType("family")}
         type="button"
         className={`w-40 md:48 rounded-full text-xl hover:ring-1 hover:ring-primary px-16 py-2 border flex justify-center items-center border-primary bg-transparent ${selectedAccountType === "family" && "bg-primary text-white"}`}
@@ -21,6 +24,7 @@ const SignupAccountsTypeNav = () => {
         Family
       </button>
       <button
+        disabled={formStep > 0}
         onClick={() => changeSelectedAccountType("corporate")}
         type="button"
         className={`w-40 md:48 rounded-full text-xl hover:ring-1 hover:ring-primary px-16 py-2 border flex justify-center items-center border-primary bg-transparent ${selectedAccountType === "corporate" && "bg-primary text-white"}`}
