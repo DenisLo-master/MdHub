e238c24fdb1c0fbda0c5be86627a5c4b213985e7
asfand687 <asfandyar687@gmail.com>
2023-02-14 13:30:59 +0500

    changed the text in cta section

--- src/sections/Home/HomeMembershipCostCtaSection.jsx
+++ src/sections/Home/HomeMembershipCostCtaSection.jsx
@@ -12,7 +12,7 @@ const HomeMembershipCostCtaSection = () => {
       <img className="w-32 md:w-80 opacity-30 absolute top-2 right-0" src={HeartRight} alt="Heart" />
       <div className="relative z-10 flex flex-col md:flex-row md:space-x-20 gap-y-6">
         <h2 className="text-3xl text-center md:text-left md:text-5xl font-medium max-w-[800px]">
-          Your membership can cost you as little as $29.99 a month.
+          All of this, for as little as $29.99 a month. 
         </h2>
         <div className="flex flex-col items-center justify-center space-y-6">
           <button className="text-lg transition-opacity ease-in duration-200 hover:opacity-80 md:text-xl rounded-full w-64 py-2 border flex justify-center items-center space-x-2  border-transparent shadow-dark text-primary bg-white">
