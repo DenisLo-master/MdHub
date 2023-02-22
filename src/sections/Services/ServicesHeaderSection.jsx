a0a422269717cecf3a7f2ad38097f7ff3fee7dc0
asfand687 <asfandyar687@gmail.com>
2023-02-23 02:23:52 +0500

    changed header nav

--- src/sections/Services/ServicesHeaderSection.jsx
+++ src/sections/Services/ServicesHeaderSection.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ServicesCard from '../../components/ServicesCard'
 import { Link, useLocation } from 'react-router-dom'
 import {
-  TestingIcon,
-  DoctorIcon,
-  MedicineIcon,
-  NursingIcon,
-  PharmacyIcon
+  OnlinePharmacyIcon,
+  VirtualDoctorIcon,
+  StethoscopeIcon,
+  NursingHomecareIcon,
+  MentalHealthIcon
 } from '../../assets'
 
 
@@ -15,22 +15,33 @@ const ServicesHeaderSection = () => {
   return (
     <section className="bg-[#F9F9F9]">
       <h2 className="font-main py-10 text-4xl text-center">Services</h2>
-      <div className="flex justify-center items-center py-10 gap-x-6 text-white font-main">
+      <div className="w-full max-w-[1050px] mx-auto rounded-full shadow-xl flex justify-center items-center py-3 gap-x-6 text-[#3F3F3F] font-main">
         <Link to="/services">
           <ServicesCard title={"virus testing"} bgPrimary={location.pathname === "/services"}>
-            <TestingIcon className={`${location.pathname === "/services" ? "text-white" : "text-primary"}`} />
+            <MentalHealthIcon className={`${location.pathname === "/services" ? "text-white" : "text-primary"}`} />
           </ServicesCard>
         </Link>
 
         <Link to="/services/virtual-doctor">
           <ServicesCard title={"virtual doctor"} bgPrimary={location.pathname === "/services/virtual-doctor"}>
-            <DoctorIcon className={`${location.pathname === "/services/virtual-doctor" ? "text-white" : "text-primary"}`} />
+            <VirtualDoctorIcon className={`${location.pathname === "/services/virtual-doctor" ? "text-white" : "text-primary"}`} />
+          </ServicesCard>
+        </Link>
+
+        <Link to="/services/online-pharmacy">
+          <ServicesCard
+            title={"online pharmacy"}
+            bgPrimary={location.pathname === "/services/online-pharmacy"}
+          >
+            <OnlinePharmacyIcon
+              className={`${location.pathname === "/services/online-pharmacy" ? "text-white" : "text-primary"}`}
+            />
           </ServicesCard>
         </Link>
 
         <Link to="/services/medical-laboratory">
-          <ServicesCard title={"medical laboratory"} bgPrimary={location.pathname === "/services/medical-laboratory"}>
-            <MedicineIcon
+          <ServicesCard title={"diagnostics"} bgPrimary={location.pathname === "/services/medical-laboratory"}>
+            <StethoscopeIcon
               className={`${location.pathname === "/services/medical-laboratory" ? "text-white" : "text-primary"}`}
             />
         </ServicesCard>
@@ -38,20 +49,8 @@ const ServicesHeaderSection = () => {
 
         <Link to="/services/nursing-homecare">
           <ServicesCard title={"nursing & homecare"} bgPrimary={location.pathname === "/services/nursing-homecare"}>
-            <NursingIcon
+            <NursingHomecareIcon
              className={`${location.pathname === "/services/nursing-homecare" ? "text-white" : "text-primary"}`}
-            />
-          </ServicesCard>
-        </Link>
-
-        <Link to="/services/online-pharmacy">
-          <ServicesCard 
-            title={"online pharmacy & delivery"} 
-            bgPrimary={location.pathname === "/services/online-pharmacy"} 
-            wideCaption
-          >
-            <PharmacyIcon
-             className={`${location.pathname === "/services/online-pharmacy" ? "text-white" : "text-primary"}`}
             />
           </ServicesCard>
         </Link>
