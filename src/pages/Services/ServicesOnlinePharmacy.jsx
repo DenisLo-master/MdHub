556959ed9625ffe643528068d7d583b990dcbd01
asfand687 <asfandyar687@gmail.com>
2023-02-23 19:43:56 +0500

    added online pharmacy how it works data

--- src/pages/Services/ServicesOnlinePharmacy.jsx
+++ src/pages/Services/ServicesOnlinePharmacy.jsx
@@ -1,99 +1,71 @@
 import React from 'react'
-import { OnlinePharmacyImage } from '../../assets'
+import { OnlinePharmacyImage, OnlinePharmacyBanner } from '../../assets'
 import Button from '../../components/Button'
+import { onlinePharmacyHowItWorksData } from '../../constants'
 
 const ServicesOnlinePharmacy = () => {
   return (
-    <section className="pt-20 leading-4">
-      <div className="flex gap-x-10 justify-center font-body">
-        <article>
-          <h2 className="font-main text-4xl pb-2">Online Pharmacy</h2>
-          <div className="font-body font-light w-full max-w-[380px] text-justify space-y-4">
-            <p>
-              Our members may access our virtual medicine platform to obtain a new prescriptions or renewals for a majority of medical requirements.
-            </p>
-            <p>
-              All medication prescriptions may be fulfilled by your pharmacy or choice or conveniently through our partnership platform Medzy, shipped for free quickly to your location of choice
-            </p>
-          </div>
-          <div className="font-main py-8 space-y-4">
-            <button className={`rounded-full w-full max-w-[380px] group hover:ring-1 hover:ring-primary  py-2 text-xl border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
-              See a doctor for a new prescription
-            </button>
-
-            <button className={`rounded-full w-full max-w-[380px] group hover:ring-1 hover:ring-primary  py-2 text-xl border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
-              Order an existing prescription
-            </button>
-          </div>
-          <section className="w-full max-w-[320px] font-body font-light">
-            <h2 className="font-main text-4xl pb-2">How It Works</h2>
-            <div className="space-y-3">
-              <article>
-                <p className="py-2 font-semibold">
-                  If you need a new prescription.
-                </p>
-                <p>
-                  Visit our virtual doctor We’ll ask you some questions about your medical history, and your symptoms and provide you with your prescription
-                </p>
-              </article>
-
-              <article>
-                <p className="py-2 font-semibold">
-                  If you already have a prescription
-                </p>
-                <p>
-                  We take care of transferring your prescriptions from your old pharmacy.
-                </p>
-              </article>
-
-              <article>
-                <p className="py-2 font-semibold">
-                  Free + Fast Delivery
-                </p>
-                <p className="pb-4">
-                  We’ll ship your medication to your door, in a discreet eco package, for free.
-                </p>
-                <p>
-                  That’s it! You get your medication on time, every time.
-                </p>
-              </article>
+    <section className="font-main">
+      <header className="bg-[#90C387] text-white w-full py-10 pb-20 relative">
+        <div className="max-w-[83rem] mx-auto">
+          <article className="space-y-5">
+            <h2 className="text-[88px] max-w-[380px] leading-[80px] pb-5">
+              online pharmacy
+            </h2>
+            <div className="space-y-5 font-body text-lg max-w-lg">
+              <p>
+                Stop wasting time chasing down refills or waiting in line. Easily access our online pharmacy to get prescriptions filled or renewed and delivered right to your door
+              </p>
+              <strong className="block pb-4">
+                Your pharmacist is just a text away.
+              </strong>
+              <p>
+                With our membership, you'll get:
+              </p>
+              <ul className="list-disc pl-8">
+                <li>Free delivery in Quebec</li>
+                <li>Automatic refills</li>
+                <li>24/7 emergency service</li>
+              </ul>
             </div>
-          </section>
-        </article>
-        <article>
-          <img className="w-full max-w-[500px]" src={OnlinePharmacyImage} alt="Online Pharmacy" />
-        </article>
-      </div>
-
-      <div className="font-body w-full max-w-[58rem] mx-auto py-20">
-        <h2 className="font-semibold text-xl pb-4">Invoice</h2>
-        <ul className="space-y-4">
-          <li className="flex justify-between border-b-[0.75px] border-dark pb-3">
-            <p>Delivery</p>
-            <p>Free</p>
-          </li>
-
-          <li className="flex justify-between border-b-[0.75px] border-dark pb-3">
-            <p>24/7 Emergency Service</p>
-            <p>Free</p>
-          </li>
-
-          <li className="flex justify-between border-b-[0.75px] border-dark pb-3">
-            <p>Personalized Packaging</p>
-            <p>Free</p>
-          </li>
-
-          <li className="flex justify-between border-dark pb-3">
-            <p>Medications</p>
-            <p className="text-right max-w-[150px]">Depends On Your Insurance</p>
-          </li>
-        </ul>
-        <div className="font-main pt-8">
-          <button className={`rounded-full text-xl group hover:ring-1 hover:ring-primary px-12 py-3 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
-            Order Now
-          </button>
+            <button className="rounded-full w-[240px] text-lg flex justify-center py-3 border border-white">
+              Sign Up Today
+            </button>
+          </article>
+          <img className="absolute right-0 top-16" src={OnlinePharmacyBanner} alt="virtual doctor" />
         </div>
-      </div>
+      </header>
+      <section className="bg-white py-14">
+        <h2 className="text-primary text-center text-5xl md:text-7xl pb-10">
+          Get care today for <span className="text-gray-800">antibiotics.</span>
+        </h2>
+        <div className="bg-[#F9F9F9] py-10">
+          <h2 className="text-4xl pb-4 text-center">
+            How It Works
+          </h2>
+          <section className="w-full max-w-4xl space-y-6 mx-auto">
+            {
+              onlinePharmacyHowItWorksData.map((step => (
+                <article key={step.id} className="flex justify-between items-center">
+                  <div className="flex items-center gap-x-8">
+                    <article>
+                      <div className="w-20 h-20 text-3xl font-body font-semibold bg-primary text-white rounded-full flex justify-center items-center">
+                        {step.id}
+                      </div>
+                    </article>
+                    <article className="w-full max-w-[590px] space-y-5">
+                      <p className="font-body text-lg">
+                        {step.text}
+                      </p>
+                    </article>
+                  </div>
+                  <img src={step.image} alt={step.text} />
+                </article>
+              )))
+            }
+          </section>
+        </div>
+      </section>
     </section>
   )
 }
