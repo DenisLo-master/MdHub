6d9fd7a9a217bca5ad979eb78c9cf4c6e1c55436
asfand687 <asfandyar687@gmail.com>
2023-02-24 03:36:06 +0500

    added card element

--- src/sections/Signup/SignupStepThree.jsx
+++ src/sections/Signup/SignupStepThree.jsx
@@ -1,3 +1,4 @@
+import { CardElement } from '@stripe/react-stripe-js'
 import React from 'react'
 import { registerationStore } from '../../store/registerationStore'
 
@@ -162,39 +163,10 @@ const SignupStepThree = () => {
           </div>
         </article>
       </div>
-      <div className="py-16 border space-y-4 px-8 shadow-cardService rounded-[35px] text-primary">
-        <div className="w-full flex">
-          <input
-            onChange={({ target }) => handleRegisterationFormDataChange(target.name, target.value)}
-            className="flex-1 text-center rounded-full text-xl bg-transparent text-primary focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
-            name="creditCardNumber"
-            type="text"
-            value={registerationFormData.creditCardNumber}
-            placeholder="Credit Card Number*"
-            required
-          />
-        </div>
-        <div className="w-full flex flex-wrap gap-y-4 gap-x-6">
-          <input
-            className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
-            name="creditCardExpiry"
-            type="text"
-            placeholder="Expiry*(mm/yy)"
-            value={registerationFormData.creditCardExpiry}
-            onChange={({ target }) => handleRegisterationFormDataChange(target.name, target.value)}
-            required
-          />
-          <input
-            className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
-            name="creditCardCvc"
-            type="text"
-            placeholder="CVC*"
-            value={registerationFormData.creditCardCvc}
-            onChange={({ target }) => handleRegisterationFormDataChange(target.name, target.value)}
-            required
-          />
-        </div>
-      </div>
+      <CardElement
+        id="card-element"
+        className="rounded-full px-4 py-3 ring-1 focus:ring-primary"
+      />
     </section>
   )
 }
