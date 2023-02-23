1ff30cab2a3aaa8a531cf70a759e21d8435871b6
asfand687 <asfandyar687@gmail.com>
2023-02-23 18:32:46 +0500

    completed virtual doctors page

--- src/pages/Services/ServicesVirtualDoctor.jsx
+++ src/pages/Services/ServicesVirtualDoctor.jsx
@@ -1,115 +1,130 @@
 import React from 'react'
-import DescriptionGrid from '../../components/DescriptionGrid'
-import { VirusTestingAndVaccination } from '../../assets'
+import {
+  VirtualDoctorBanner,
+  HowItWorksMobile01
+} from '../../assets'
+
+import { medicalTreatmentData, virtualDoctorHowItWorksData } from '../../constants'
+
 
 const ServicesVirtualDoctor = () => {
   return (
-    <section className="font-main">
-      <DescriptionGrid title="Virtual Doctor" imageSrc={VirusTestingAndVaccination}>
-        <p>
-          Unlimited 24/7 access to Medical advice, diagnosis, and treatment (including prescriptions, lab work, referrals to specialists, and more). Referral to clinics, when necessary.
-        </p>
-      </DescriptionGrid>
-      <section className="py-10 text-gray-800">
-        <h2 className="text-4xl py-10 text-center">
-          How It Works
+    <section className="font-main py-10">
+      <header className="bg-[#A293FF] text-white w-full h-[530px] py-10 relative">
+        <div className="max-w-[83rem] mx-auto">
+          <article className="space-y-5">
+            <h2 className="text-[88px]">
+              virtual doctor
+            </h2>
+            <div className="space-y-5 font-body text-lg max-w-lg">
+              <p>
+                Treatment and diagnosis for 70% of medical issues can be done virtually.
+              </p>
+              <p>
+                Connect with a virtual doctor in minutes, 24/7. Or, book an appointment with a specialist – no referral needed.
+              </p>
+            </div>
+            <button className="rounded-full w-[240px] text-lg flex justify-center py-3 border border-white">
+              Sign Up Today
+            </button>
+          </article>
+          <img className="absolute right-0 top-0 h-full" src={VirtualDoctorBanner} alt="virtual doctor" />
+        </div>
+      </header>
+
+      <section className="bg-white py-14">
+        <h2 className="text-primary text-center text-5xl md:text-7xl">
+          Get care today for <span className="text-gray-800">birth control pills.</span>
         </h2>
-        <section className="flex justify-center space-x-8">
-          <article className="flex space-x-4">
-            <div className="w-12 h-12 flex justify-center items-center space-x-2 rounded-full bg-primary text-white font-body font-semibold">
-              1
-            </div>
-            <div className="max-w-[220px] space-y-2">
-              <h2 className="text-xl leading-5">Describe your symptoms through our app</h2>
-              <div className="font-body font-light">
-                Open the app and click the “Get care” button. One of our healthcare providers will review your request and respond in minutes.
-              </div>
-            </div>
-            <div>
-            </div>
+        <h3 className="text-center text-4xl md:text-5xl py-20">
+          Don't just take it from us
+        </h3>
+        <div className="w-full max-w-6xl mx-auto flex item-center justify-between text-center">
+          <article className="space-y-6 text-center">
+            <h2 className="text-primary text-7xl">4.9/5</h2>
+            <p className="font-body max-w-[170px]">Average user satisfaction rating</p>
           </article>
-
-          <article className="flex space-x-4">
-            <div className="w-12 h-12 flex justify-center items-center rounded-full bg-primary text-white font-body  font-semibold">
-              2
-            </div>
-            <div className="max-w-[220px] space-y-2">
-              <h2 className="text-xl leading-5">Chat with a doctor or nurse practitioner</h2>
-              <div className="font-body font-light">
-                Next, you’ll be connected to the healthcare provider. Online doctor consultations usually take place over text, but the doctor may request video, or audio chat if needed.
-              </div>
-            </div>
+          <article className="space-y-6 text-center">
+            <h2 className="text-primary text-7xl">91%</h2>
+            <p className="font-body max-w-[220px]">Of medical issues resolved in the first interaction</p>
           </article>
-
-          <article className="flex space-x-4">
-            <div className="w-12 h-12 flex justify-center items-center rounded-full bg-primary text-white font-body  font-semibold">
-              3
-            </div>
-            <div className="max-w-[220px] space-y-2">
-              <h2 className="text-xl leading-5">Get a diagnosis or prescription</h2>
-              <div className="font-body font-light">
-                Our online doctors in Canada can diagnose and provide medical prescriptions as necessary. Prescription delivery is available at no additional cost.
-              </div>
-            </div>
+          <article className="space-y-6 text-center">
+            <h2 className="text-primary text-7xl">65%</h2>
+            <p className="font-body max-w-[200px]">Of visits prevent a workplace absence</p>
           </article>
-        </section>
-      </section>
-
-      <section className="py-10 text-gray-800">
-        <h2 className="text-4xl py-10 text-center">
-          Numbers Don't Lie
-        </h2>
-        <div className="flex justify-center md:space-x-24">
-          <article className="font-main text-primary text-7xl text-center max-w-[160px]">
-            <h2>4.9/5</h2>
-            <p className="text-lg text-gray-800">
-              Average user satisfaction rating
-            </p>
-          </article>
-
-          <article className="font-main text-primary text-7xl text-center max-w-[200px]">
-            <h2>91%</h2>
-            <p className="text-lg text-gray-800">
-              Of medical issues resolved in the first interaction
-            </p>
-          </article>
-
-          <article className="font-main text-primary text-7xl text-center max-w-[160px]">
-            <h2>61%</h2>
-            <p className="text-lg text-gray-800">
-              Of visits prevent a workplace absense
-            </p>
-          </article>
-
-          <article className="font-main text-primary text-7xl text-center max-w-[200px]">
-            <h2>3.6</h2>
-            <p className="text-lg text-gray-800">
-              Hours saves per consultation on average
-            </p>
+          <article className="space-y-6 text-center">
+            <h2 className="text-primary text-7xl">3.6</h2>
+            <p className="font-body max-w-[170px]">Hours saves per consultation on average</p>
           </article>
         </div>
       </section>
 
-      <section className="py-10">
-        <h2 className="text-4xl py-10 text-center">
-          Get An Online Doctor Consultation In Minutes
-        </h2>
-        <div className="flex justify-center gap-x-12 pb-6 text-xl text-center">
-          <div className="w-full max-w-[370px]">
-            <article className="w-full text-center bg-primary text-white  py-3 rounded-3xl">
+      <section className="bg-[#F9F9F9] py-14">
+        <div className="w-full max-w-[38rem] mx-auto text-center space-y-4">
+          <h3 className="text-4xl md:text-5xl">
+            Get medical treatment online
+          </h3>
+          <p className="font-body text-light text-dark">
+            Our online doctors can treat your health conditions and concerns without you ever having to leave home. Book an appointment with specialists, like dermatologists, endocrinologists, mental health therapists, gynecologists, and more.
+          </p>
+        </div>
+        <div className="py-10 max-w-[83rem] mx-auto grid grid-cols-4 gap-x-4 gap-y-10">
+          {
+            medicalTreatmentData.map(item => (
+              <article
+                key={item.id}
+                className="max-w-[280px] shadow-cardService rounded-t"
+              >
+                <img className="w-full" src={item.image} alt="Diagnosis" />
+                <div className="bg-white flex flex-col items-center py-4 px-6">
+                  <h2 className="text-lg text-center pb-2">{item.title}</h2>
+                  <p className="font-body">
+                    {item.text}
+                  </p>
+                </div>
+              </article>
+            ))
+          }
+          <article className="max-w-[280px] rounded-t">
+            <div className="bg-primary h-[260px] flex justify-center items-center text-white mb-5">
+              <h2 className="text-3xl max-w-[100px] text-center">Sign Up Today</h2>
+            </div>
+            <button className="w-full border text-2xl border-dark h-36 flex justify-center items-center">
               See A Doctor
-            </article>
-            <p className="font-body font-light text-sm pt-2 text-gray-800">
-              Current members
-            </p>
-          </div>
-          <div className="w-full max-w-[370px]">
-            <article className="w-full text-center bg-primary text-white  py-3 rounded-3xl">
-              Sign Up
-            </article>
-            <p className="font-body font-light text-sm pt-2 text-gray-800">
-              If you are not a member already
-            </p>
+            </button>
+          </article>
+        </div>
+      </section>
+
+      <section className="py-10 bg-white text-gray-800">
+        <div className="w-full max-w-[83rem] mx-auto">
+          <h2 className="text-4xl pb-4 text-center">
+            How It Works
+          </h2>
+          <section>
+            {
+              virtualDoctorHowItWorksData.map(step => (
+                <article key={step.id} className="flex justify-between items-center">
+                  <div className="flex gap-x-8">
+                    <article className="w-20 h-20 text-3xl font-body font-semibold bg-primary text-white rounded-full flex justify-center items-center">
+                      {step.id}
+                    </article>
+                    <article className="w-full max-w-[590px] space-y-5">
+                      <h2 className="text-4xl">{step.title}</h2>
+                      <p className="font-body text-lg">
+                        {step.description}
+                      </p>
+                    </article>
+                  </div>
+                  <img src={step.image} alt={step.title} />
+                </article>
+              ))
+            }
+          </section>
+          <div className="md:pl-28">
+            <button className="rounded-full text-center w-44 py-1 border border-primary">
+              Sign Up Today
+            </button>
           </div>
         </div>
       </section>
