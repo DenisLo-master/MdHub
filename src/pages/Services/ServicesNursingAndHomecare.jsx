eea99434be92b2b2d095afecc05db9be64251c07
asfand687 <asfandyar687@gmail.com>
2023-02-23 23:22:59 +0500

    removed padding from bottom

--- src/pages/Services/ServicesNursingAndHomecare.jsx
+++ src/pages/Services/ServicesNursingAndHomecare.jsx
@@ -26,7 +26,7 @@ const ServicesNursingAndHomecare = () => {
           <img className="absolute right-0 top-0 h-full" src={NursingAndHomecareBanner} alt="Nursing and Homecare" />
         </div>
       </header>
-      <section className="bg-white py-14">
+      <section className="bg-white pt-14">
         <h2 className="text-primary text-center text-5xl md:text-7xl pb-10">
           Get care today for <span className="text-gray-800">palliative care.</span>
         </h2>
@@ -64,28 +64,30 @@ const ServicesNursingAndHomecare = () => {
           </section>
         </section>
       </section>
-      <div className="w-full max-w-[1200px] mx-auto pt-12">
-        <h2 className="md:pl-14 font-main text-[30px] text-center md:text-left text-black pb-4">
-          Services suitable for all your needs
-        </h2>
-        <section className="flex justify-center flex-wrap gap-8 text-center">
-          {
-            nursingAndHomecareServices.map((item, index) => (
-              <article key={index} className={`shadow-cardService w-[127px] h-[120px] text-xs text-center flex justify-center items-center border-[0.25px] border-[#cfc5c5] font-main ${index === 0 ? "bg-primary text-white" : 'bg-white text-gray-800'}`}>
-                <h2 className="max-w-[100px]">{item}</h2>
-              </article>
-            ))
-          }
-        </section>
-        <div className="text-sm font-body md:pl-14 text-gray-800">
-          <h2 className="py-6 font-semibold">
-            Require a different service? Send us an email here.
+      <section className="bg-white">
+        <div className="w-full max-w-[1200px] mx-auto pt-12">
+          <h2 className="md:pl-14 font-main text-[30px] text-center md:text-left text-black pb-4">
+            Services suitable for all your needs
           </h2>
-          <p className="max-w-[1100px]">
-            <strong className="font-bold">NOTE:</strong> Above pricing does not include laboratory fees or additional materials such as vitamin drips or the cost of medication. Our team will confirm all appointments by email or phone and advice of any additional fees. Homecare services require a minimum booking of 3 hours for Montreal and 4 hours for outside of Montreal.
-          </p>
+          <section className="flex justify-center flex-wrap gap-8 text-center">
+            {
+              nursingAndHomecareServices.map((item, index) => (
+                <article key={index} className={`shadow-cardService w-[127px] h-[120px] text-xs text-center flex justify-center items-center border-[0.25px] border-[#cfc5c5] font-main ${index === 0 ? "bg-primary text-white" : 'bg-white text-gray-800'}`}>
+                  <h2 className="max-w-[100px]">{item}</h2>
+                </article>
+              ))
+            }
+          </section>
+          <div className="text-sm font-body md:pl-14 text-gray-800">
+            <h2 className="py-6 font-semibold">
+              Require a different service? Send us an email here.
+            </h2>
+            <p className="max-w-[1100px]">
+              <strong className="font-bold">NOTE:</strong> Above pricing does not include laboratory fees or additional materials such as vitamin drips or the cost of medication. Our team will confirm all appointments by email or phone and advice of any additional fees. Homecare services require a minimum booking of 3 hours for Montreal and 4 hours for outside of Montreal.
+            </p>
+          </div>
         </div>
-      </div>
+      </section>
     </section>
   )
 }
