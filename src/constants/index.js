859dc11d9f8700af83fcc61b90bafa17de1e549f
asfand687 <asfandyar687@gmail.com>
2023-02-23 18:32:22 +0500

    added new assets

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,13 +1,14 @@
 import {
-  ForYouNav01,
-  ForYouNav02,
-  ForYouNav03,
-  ForYouNav04,
-  ForYouNav05,
-  HowItWorks01,
-  HowItWorks02,
-  HowItWorks03,
-  HowItWorks04,
+  HowItWorksMobile01,
+  HowItWorksMobile02,
+  HowItWorksMobile03,
+  MedicalTreatment01,
+  MedicalTreatment02,
+  MedicalTreatment03,
+  MedicalTreatment04,
+  MedicalTreatment05,
+  MedicalTreatment06,
+  MedicalTreatment07,
 }
   from '../assets'
 
@@ -91,4 +92,71 @@ export const membershipBenefits = [
     id: 7,
     title: "Headspace app"
   },
+]
+
+export const medicalTreatmentData = [
+  {
+    id: 1,
+    title: "Diagnosis",
+    image: MedicalTreatment01,
+    text: "Lorem ipsum dolor sit amet consectetur. Vulputate elementum eleifend sed morbi montes tristique."
+  },
+  {
+    id: 2,
+    title: "Prescriptions & renewals",
+    image: MedicalTreatment02,
+    text: "Lorem ipsum dolor sit amet consectetur. Vulputate elementum eleifend sed morbi montes tristique."
+  },
+  {
+    id: 3,
+    title: "Doctors notes",
+    image: MedicalTreatment03,
+    text: "Lorem ipsum dolor sit amet consectetur. Vulputate elementum eleifend sed morbi montes tristique."
+  },
+  {
+    id: 4,
+    title: "Lab Requisitions",
+    image: MedicalTreatment04,
+    text: "Lorem ipsum dolor sit amet consectetur. Vulputate elementum eleifend sed morbi montes tristique."
+  },
+  {
+    id: 5,
+    title: "Referrals to specialists",
+    image: MedicalTreatment05,
+    text: "Lorem ipsum dolor sit amet consectetur. Vulputate elementum eleifend sed morbi montes tristique."
+  },
+  {
+    id: 6,
+    title: "Mental health",
+    image: MedicalTreatment06,
+    text: "Lorem ipsum dolor sit amet consectetur. Vulputate elementum eleifend sed morbi montes tristique."
+  },
+  {
+    id: 7,
+    title: "General health advice",
+    image: MedicalTreatment07,
+    text: "Lorem ipsum dolor sit amet consectetur. Vulputate elementum eleifend sed morbi montes tristique."
+  },
+]
+
+export const virtualDoctorHowItWorksData = [
+  {
+    id: 1,
+    title: "Describe your symptoms using our app",
+    description: "Open the app and click the “Get care” button. Describe your symptoms, and one of our healthcare providers will review your request in minutes.",
+    image: HowItWorksMobile01
+  },
+  {
+    id: 2,
+    title: "Chat with a doctor or nurse practitioner",
+    description: "Typically, you’ll be connected with a healthcare provider over text, but they may request a video or audio chat if necessary.",
+    image: HowItWorksMobile02
+  },
+  {
+    id: 3,
+    title: "Get a diagnosis or prescription",
+    description: "Our virtual doctors can diagnose and provide prescriptions, and prescription delivery is available at no extra cost.",
+    image: HowItWorksMobile03
+  },
+
 ]
