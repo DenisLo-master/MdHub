9c28de5997ff0ca94c707b3a1a10ffc3c0c4699e
asfand687 <asfandyar687@gmail.com>
2023-02-23 22:42:44 +0500

    completed services page one

--- src/pages/Services/ServicesMentalHealth.jsx
+++ src/pages/Services/ServicesMentalHealth.jsx
@@ -60,7 +60,7 @@ const ServicesMentalHealth = () => {
               <div>
                 <h2 className="text-3xl">See a mental health physician</h2>
                 <p className="pt-10 pb-14 max-w-[650px]">Log into your MDHUB account and select “Mental Health Physician” from the menu of specialists. Next, choose a date and time that works for you. Quickly speak with a mental health physician for an initial assessment or follow-up. 30-minute initial appointments are $315, and 30-minute follow-up appointments are $230.</p>
-                <button className="rounded-full text-center w-44 py-1 border border-dark">
+                <button className="rounded-full text-center w-44 py-1 border border-primary">
                   Sign Up Today
                 </button>
               </div>
@@ -71,7 +71,7 @@ const ServicesMentalHealth = () => {
               <div className="text-right">
                 <h2 className="text-3xl">See a mental health therapist</h2>
                 <p className="pt-10 pb-14 max-w-[650px]">See a mental health therapist, with no appointment necessary. All you need to do is choose the topic you’d like to discuss with a therapist and book an appointment. Sessions are 50 minutes in length. The fee for a session ranges from $120-$160, depending on your region. If you have insurance, this fee is often covered by health benefits.</p>
-                <button className="rounded-full text-center w-44 py-1 border border-dark">
+                <button className="rounded-full text-center w-44 py-1 border border-primary">
                   Sign Up Today
                 </button>
               </div>
