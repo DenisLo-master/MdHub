ce671557f3de59af5965547a9b78bf531c2a83bb
asfand687 <asfandyar687@gmail.com>
2023-02-24 12:25:09 +0500

    changed the signup link to production

--- src/sections/Signup/RegistrationCheckoutForm.jsx
+++ src/sections/Signup/RegistrationCheckoutForm.jsx
@@ -79,7 +79,7 @@ const RegistrationCheckoutForm = () => {
         card: elements.getElement(CardElement),
       });
       if (!error) {
-        const response = await fetch('http://localhost:8080/api/v1/auth/register', {
+        const response = await fetch('https://mdhub-backend.onrender.com/api/v1/auth/register', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
