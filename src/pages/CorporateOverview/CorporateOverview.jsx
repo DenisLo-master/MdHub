95c2c983d31328635e723e01dfb596b946236c0a
asfand687 <asfandyar687@gmail.com>
2023-02-25 01:26:13 +0500

    changed content for corporate overview

--- src/pages/CorporateOverview/CorporateOverview.jsx
+++ src/pages/CorporateOverview/CorporateOverview.jsx
@@ -40,7 +40,7 @@ const CorporateOverview = () => {
           </div>
           <div>
             <p className="font-body text-xl text-center py-8 font-light">
-              Lorem ipsum dolor sit amet consectetur. Duis nibh a lobortis nunc tortor fermentum nisi ac hendrerit.
+              We offer holistic corporate plans to suit any size of business.
             </p>
             <div className="flex justify-center gap-x-12">
               <button className={`rounded-full text-xl group hover:ring-1 hover:ring-primary px-10 py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
@@ -57,14 +57,14 @@ const CorporateOverview = () => {
         </article>
 
         <article className="text-center pt-10 space-y-6">
-          <h2 className="text-4xl">Lorem ipsum dolor sit.</h2>
+          <h2 className="text-4xl">The ultimate benefits solution that your teams will actually use and love.</h2>
           <div className="flex justify-between">
             <article className="flex flex-col items-center justify-center space-y-1">
               <img className="w-24" src={CorporateBenefits01} alt="Medical Care" />
               <div className="space-y-2">
-                <h2 className="text-xl">Lorem ipsum dolor sit.</h2>
+                <h2 className="text-xl">Whole-person care anytime, anywhere</h2>
                 <p className="font-body font-thin text-sm w-full max-w-[360px] leading-[1.4rem]">
-                  Lorem ipsum dolor sit amet consectetur. Rhoncus sed imperdiet mauris facilisi at imperdiet hendrerit enim. Sed fames pharetra sagittis pellentesque ullamcorper amet dolor pellentesque.
+                  Support every person on your team with access to quality healthcare available 24/7, like virtual appointments and prescription delivery.
                 </p>
                 <button className={`mx-auto rounded-full text-lg group hover:ring-1 hover:ring-primary px-8 py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
                   Learn More
@@ -74,9 +74,9 @@ const CorporateOverview = () => {
             <article className="flex flex-col items-center justify-center space-y-1">
               <img className="w-24" src={CorporateBenefits02} alt="Medical Care" />
               <div className="space-y-2">
-                <h2 className="text-xl">Lorem ipsum dolor sit.</h2>
-                <p className="font-body font-thin text-sm w-full max-w-[360px] leading-[1.4rem]">
-                  Lorem ipsum dolor sit amet consectetur. Rhoncus sed imperdiet mauris facilisi at imperdiet hendrerit enim. Sed fames pharetra sagittis pellentesque ullamcorper amet dolor pellentesque.
+                <h2 className="text-xl">Family & pediatric care</h2>
+                <p className="font-body font-thin text-sm w-full max-w-[380px] leading-[1.4rem]">
+                  With MDHUB corporate plans, your employee’s family and children can receive healthcare, too. Designed specifically for busy families, your employees’ families can get care fast. 
                 </p>
                 <button className={`mx-auto rounded-full text-lg group hover:ring-1 hover:ring-primary px-8 py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
                   Learn More
@@ -86,9 +86,9 @@ const CorporateOverview = () => {
             <article className="flex flex-col items-center justify-center space-y-1">
               <img className="w-24" src={CorporateBenefits03} alt="Medical Care" />
               <div className="space-y-2">
-                <h2 className="text-xl">Lorem ipsum dolor sit.</h2>
-                <p className="font-body font-thin text-sm w-full max-w-[360px] leading-[1.4rem]">
-                  Lorem ipsum dolor sit amet consectetur. Rhoncus sed imperdiet mauris facilisi at imperdiet hendrerit enim. Sed fames pharetra sagittis pellentesque ullamcorper amet dolor pellentesque.
+                <h2 className="text-xl">Increased ROI & employee satisfaction</h2>
+                <p className="font-body font-thin text-sm w-full max-w-[380px] leading-[1.4rem]">
+                  Your corporate membership is tax-deductible, but it also means less out-of-pocket expenses for your people. Plus, you can feel good knowing they're well taken care of.
                 </p>
                 <button className={`mx-auto rounded-full text-lg group hover:ring-1 hover:ring-primary px-8 py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
                   Learn More
@@ -102,39 +102,27 @@ const CorporateOverview = () => {
           <div className="flex flex-col items-center space-y-2">
             <img src={CorporateOverview02} alt="Healthcare Benefits" />
             <p className="font-body font-thin text-sm w-full max-w-[360px] leading-[1.4rem] px-6">
-              Lorem ipsum dolor sit amet consectetur. Rhoncus sed imperdiet mauris facilisi at imperdiet hendrerit enim. Sed fames pharetra sagittis pellentesque ullamcorper amet dolor pellentesque.
+              Healthy people, happier organizations
             </p>
-            <button className={`mx-auto rounded-full text-lg group hover:ring-1 hover:ring-primary px-8 py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
-              Learn More
-            </button>
           </div>
           <div className="flex flex-col items-center space-y-2">
             <img src={CorporateOverview03} alt="Healthcare Benefits" />
             <p className="font-body font-thin text-sm w-full max-w-[360px] leading-[1.4rem] px-6">
-              Lorem ipsum dolor sit amet consectetur. Rhoncus sed imperdiet mauris facilisi at imperdiet hendrerit enim. Sed fames pharetra sagittis pellentesque ullamcorper amet dolor pellentesque.
+              Mental health & well-being
             </p>
-            <button className={`mx-auto rounded-full text-lg group hover:ring-1 hover:ring-primary px-8 py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
-              Learn More
-            </button>
           </div>
           <div className="flex flex-col items-center space-y-2">
             <img src={CorporateOverview04} alt="Healthcare Benefits" />
             <p className="font-body font-thin text-sm w-full max-w-[360px] leading-[1.4rem] px-6">
-              Lorem ipsum dolor sit amet consectetur. Rhoncus sed imperdiet mauris facilisi at imperdiet hendrerit enim. Sed fames pharetra sagittis pellentesque ullamcorper amet dolor pellentesque.
+              Chat with specialists
             </p>
-            <button className={`mx-auto rounded-full text-lg group hover:ring-1 hover:ring-primary px-8 py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
-              Learn More
-            </button>
           </div>
         </article>
 
         <article className="text-center px-5">
-          <h2 className="text-[2.5rem]">Lorem ipsum dolor sit.</h2>
+          <h2 className="text-[2.5rem]">Improving community wellness </h2>
           <p className="font-body font-light py-4 text-sm">
-            Lorem ipsum dolor sit amet consectetur. Venenatis eu hendrerit faucibus leo ultrices mi enim dui. Lobortis lectus enim nulla in. Vel neque sem massa ullamcorper in. Nunc velit turpis id dignissim diam arcu libero leo. Id porttitor fermentum amet blandit facilisis at. Orci bibendum et mattis laoreet leo tristique at. Non sollicitudin bibendum viverra ac. Purus tristique risus adipiscing quam pharetra. Velit quam viverra quis sit mi. Purus elit dignissim fermentum fermentum fusce. Viverra nulla rhoncus vitae integer euismod. Sagittis accumsan.
-          </p>
-          <p className="font-body font-light text-sm">
-            Molestie lectus at lacus commodo. Sed risus id eget quisque gravida in faucibus. Nec condimentum risus nunc dolor nullam vel leo quis dictum. Morbi quis vel amet massa. Tincidunt quis viverra est nullam diam sed. Fusce pretium felis amet aliquam lobortis amet morbi. Justo id nunc velit sit porta dictumst tempus. Tortor quam risus massa mi cursus cras. Mattis ut purus scelerisque adipiscing varius at aenean feugiat nunc. Diam facilisis malesuada platea a. Senectus eget cursus neque justo et natoque netus sed mauris.
+            A corporate membership, to us, is more like a partnership. When individuals and their families are taken care of and valued, it creates a ripple effect. Organizations become happier, healthier and more productive. When organizations become healthier, so do entire communities, and that’s something you can be proud of.
           </p>
         </article>
       </div>
