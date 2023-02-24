4f976cc8f5c40367cd668ae77c44a76aebb0f714
asfand687 <asfandyar687@gmail.com>
2023-02-25 01:14:45 +0500

    updated text

--- src/pages/ForYou/ForYou.jsx
+++ src/pages/ForYou/ForYou.jsx
@@ -8,27 +8,24 @@ const ForYou = () => {
       <ForYouHeaderSection />
       <section className="w-full max-w-[1130px] mx-auto py-10 font-main">
         <div className="space-y-6">
-          <h2 className="text-4xl">Lorem ipsum dolor.</h2>
-          <p className="font-body font-light">Lorem ipsum dolor sit amet consectetur. Rhoncus sed imperdiet mauris facilisi at imperdiet hendrerit enim. Sed fames pharetra sagittis pellentesque ullamcorper amet dolor pellentesque. Amet enim dictum ut et eu nulla vitae cras varius. Consectetur aliquet cras sit aliquam a faucibus at. Tortor nunc amet vel ipsum congue. Amet in tristique vitae ultrices mi tempus egestas vitae ac. Eget euismod vel etiam elementum laoreet etiam amet viverra. Blandit scelerisque elementum euismod nullam massa ullamcorper tellus donec amet. Pretium tincidunt hendrerit morbi id pellentesque ac facilisis. Ut nec nulla dignissim convallis sit sit donec. Et nullam sed porttito.</p>
+          <h2 className="text-4xl">A new, personalized standard of healthcare</h2>
+          <p className="font-body font-light">Whether it’s getting a prescription for a common illness or accessing a medical laboratory, MDHUB makes it quick and easy to get the care you need. With over 150 doctors registered on our platform, including a variety of specialists, you can save time and the headache of the usual trip to the doctor’s office.
+          </p>
           <button className={`rounded-full text-lg shadow-cardService group hover:ring-1 hover:ring-primary px-10 py-3 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
-            Sign Up Today
+            Register Today
           </button>
         </div>
 
         <div className="flex justify-center py-16">
           <article className="p-12 px-14 bg-[#f9f9f9] shadow-cardService rounded-tl-3xl rounded-bl-3xl">
-            <h2 className="text-4xl pb-6">Lorem ipsum dolor.</h2>
+            <h2 className="text-4xl pb-6">Your health, in your hands. </h2>
             <div className="font-body text-sm max-w-[350px] space-y-6">
               <p>
-                Lorem ipsum dolor sit amet consectetur. Rhoncus sed imperdiet mauris facilisi at imperdiet hendrerit enim. Sed fames pharetra sagittis pellentesque ullamcorper amet dolor pellentesque. Amet enim dictum ut et eu nulla vitae cras varius. Consectetur aliquet cras sit aliquam a faucibus at. Tortor nunc amet vel ipsum congue. Amet in tristique vitae ultrices mi tempus egestas vitae ac. Eget euismod vel etiam elementum laoreet etiam amet viverra.
+                Your life is full – and you don’t have time to wait three or more weeks to see a doctor. Our memberships allow you to receive highly-personalized and compassionate care fast for both your mental and physical health.
+                So you never have to wait to see a doctor again. Learn more about our plans, so you never have to wait to see a doctor again.
               </p>
-
-              <p>
-                Blandit scelerisque elementum euismod nullam massa ullamcorper tellus donec amet. Pretium tincidunt hendrerit morbi id pellentesque ac facilisis. Ut nec nulla dignissim convallis sit sit donec. Et nullam sed porttito.  Eget euismod vel etiam elementum laoreet etiam amet viverra. Blandit scelerisque elementum.
-              </p>
-
               <button className={`rounded-full text-xl group hover:ring-1 hover:ring-primary px-12 py-3 border flex justify-center items-center space-x-2  border-primary bg-transparent font-main`}>
-                Learn More
+                Explore Our Services
               </button>
 
             </div>
@@ -38,35 +35,14 @@ const ForYou = () => {
           </article>
         </div>
 
-        <div className="py-14 flex justify-center gap-x-8 text-white text-2xl font-main">
-          <article className="relative">
-            <img src={ForYouImage02} alt="For You" />
-            <div className="absolute flex space-x-2 items-center left-4 bottom-4">
-              <span>Learn More</span>
-              <ArrowIcon />
-            </div>
-          </article>
-          <article className="relative">
-            <img src={ForYouImage03} alt="For You" />
-            <div className="absolute flex space-x-2 items-center left-4 bottom-4">
-              <span>Learn More</span>
-              <ArrowIcon />
-            </div>
-          </article>
-          <article className="relative">
-            <img src={ForYouImage04} alt="For You" />
-            <div className="absolute flex space-x-2 items-center left-4 bottom-4">
-              <span>Learn More</span>
-              <ArrowIcon />
-            </div>
-          </article>
-        </div>
-
         <div className="py-10 space-y-6">
-          <h2 className="text-4xl">Lorem ipsum dolor.</h2>
+          <h2 className="text-4xl">You and your loved ones deserve more. </h2>
           <p className="font-body font-light text-sm">
-            Lorem ipsum dolor sit amet consectetur. Rhoncus sed imperdiet mauris facilisi at imperdiet hendrerit enim. Sed fames pharetra sagittis pellentesque ullamcorper amet dolor pellentesque. Amet enim dictum ut et eu nulla vitae cras varius. Consectetur aliquet cras sit aliquam a faucibus at. Tortor nunc amet vel ipsum congue. Amet in tristique vitae ultrices mi tempus egestas vitae ac. Eget euismod vel etiam elementum laoreet etiam amet viverra.
+            MDHUB was developed with you and your family in mind. MDHUB is the ultimate solution for entire families and is a great way to keep children and teens healthy.
           </p>
+          <button className={`rounded-full text-xl group hover:ring-1 hover:ring-primary px-12 py-3 border flex justify-center items-center space-x-2  border-primary bg-transparent font-main`}>
+            Explore Our Services
+          </button>
         </div>
       </section>
     </section>
