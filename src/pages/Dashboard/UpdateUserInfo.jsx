b06d5b615a9db747abeff83e20f61a9d055750e7
asfand687 <asfandyar687@gmail.com>
2023-02-24 23:12:31 +0500

    added update card info

--- src/pages/Dashboard/UpdateUserInfo.jsx
+++ src/pages/Dashboard/UpdateUserInfo.jsx
@@ -4,19 +4,30 @@ import { toast } from "react-hot-toast"
 import { SVGLoaderCircles } from '../../assets'
 import { useNavigate } from 'react-router-dom'
 import jwtDecode from 'jwt-decode'
+import { BsPencilSquare } from "react-icons/bs"
+import { Elements } from '@stripe/react-stripe-js';
+import { UserPaymentForm } from '../../sections'
+import { loadStripe } from '@stripe/stripe-js'
+
+const key = import.meta.env.VITE_STRIPE
+const stripePromise = loadStripe(key);
 
 const UpdateUserInfo = () => {
   const [isLoading, setIsLoading] = useState(false)
   const navigate = useNavigate()
   const userInfo = registerationStore(state => state.userInfo)
   const [userInfoFormData, setUserInfoFormData] = useState({ ...userInfo, password: "" })
+  const [paymentInfo, setPaymentInfo] = useState("")
+  const [showPaymentElement, setShowPaymentElement] = useState(false)
+
+  const token = localStorage.getItem("jwtToken")
+  const tokenInfo = token ? jwtDecode(token) : {}
+
   const handleChange = (e) => {
     e.preventDefault()
     setUserInfoFormData({ ...userInfoFormData, [e.target.name]: e.target.value })
   }
   const handleSubmit = async (e) => {
-    const token = localStorage.getItem("jwtToken")
-    const tokenInfo = token ? jwtDecode(token) : {}
     e.preventDefault()
     setIsLoading(true)
     try {
@@ -43,8 +54,25 @@ const UpdateUserInfo = () => {
   }
 
   useEffect(() => {
+    const getUser = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/api/v1/users/${tokenInfo?.id}`, {
+          method: "GET",
+          headers: {
+            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${token}`
+          }
+        })
+        const { paymentInfo } = await response.json()
+        setPaymentInfo(paymentInfo)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getUser()
     window.scrollTo(0, 0)
   }, [])
+
   return (
     <section className="min-h-screen py-20">
       <div className="max-w-xl mx-auto p-8 rounded-[35px] shadow-cardService border-[0.25px]">
@@ -52,66 +80,21 @@ const UpdateUserInfo = () => {
           <h2 className="text-4xl">Edit Info</h2>
           <h2 className="text-sm">{userInfo.isChildUser ? "Child Account" : "Account Admin"}</h2>
         </div>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="w-full flex">
-            <input
-              className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
-              name="firstName"
-              type="text"
-              placeholder={userInfo?.firstName}
-              value={userInfoFormData?.firstName}
-              onChange={(e) => handleChange(e)}
-              required
-            />
-          </div>
-          <div className="w-full flex">
-            <input
-              className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
-              name="lastName"
-              type="text"
-              placeholder={userInfo?.lastName}
-              value={userInfoFormData?.lastName}
-              onChange={(e) => handleChange(e)}
-              required
-            />
-          </div>
-          <div className="w-full flex">
-            <input
-              className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
-              name="email"
-              type="email"
-              placeholder={userInfo?.email}
-              value={userInfoFormData?.email}
-              onChange={(e) => handleChange(e)}
-              required
-            />
-          </div>
-          <div className="w-full flex">
-            <input
-              className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
-              name="password"
-              type="password"
-              placeholder="Enter New Password"
-              value={userInfoFormData?.password}
-              onChange={(e) => handleChange(e)}
-            />
-          </div>
-          <div className="w-full flex">
-            <input
-              className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
-              name="address"
-              type="text"
-              placeholder={userInfo?.address}
-              value={userInfoFormData?.address}
-              onChange={(e) => handleChange(e)}
-              required
-            />
-          </div>
-          <button type="submit" className={`rounded-full w-44 font-main text-xl group hover:ring-1 hover:ring-primary  py-3 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
-            <span>Update</span>
-            {isLoading && <SVGLoaderCircles className="text-primary w-4 h-4" />}
+        <div className="flex items-center gap-x-4">
+          <h2>
+            {`Card Info: **** **** **** ${paymentInfo}`}
+          </h2>
+          <button onClick={() => setShowPaymentElement(true)}>
+            <BsPencilSquare />
           </button>
-        </form>
+        </div>
+        {
+          showPaymentElement && (
+            <Elements stripe={stripePromise}>
+              <UserPaymentForm />
+            </Elements>
+          )
+        }
       </div>
     </section>
   )
