4c0f2dbc2372301dd625bf7cc84b6a2279c49871
asfand687 <asfandyar687@gmail.com>
2023-02-26 22:54:47 +0500

    added diagnostics forms

--- src/sections/Dashboard/DiagnosticAppointmentStepTwo.jsx
+++ src/sections/Dashboard/DiagnosticAppointmentStepTwo.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
 
-const DiagnosticAppointmentStepTwo = () => {
+const DiagnosticAppointmentStepTwo = ({ time, address, selectedDate, updateFields }) => {
   return (
     <div className='space-y-3'>
       <div className="flex space-x-8">
-        <input className="px-4 py-2 flex-1 border rounded-lg bg-[#f9f9f9] outline-none focus:ring ring-primary" type="text" placeholder="Preferred Date (DD-MM-YYY)" />
-        <input className="px-4 py-2 flex-1 border rounded-lg bg-[#f9f9f9] outline-none focus:ring ring-primary" type="text" placeholder="Preferred Time" />
+        <input
+          value={selectedDate}
+          onChange={({ target }) => updateFields({ selectedDate: target.value })}
+          className="px-4 py-2 flex-1 border rounded-lg bg-[#f9f9f9] outline-none focus:ring ring-primary"
+          type="text"
+          placeholder="Preferred Date (DD-MM-YYY)"
+          required
+        />
+        <input
+          value={time}
+          onChange={({ target }) => updateFields({ time: target.value })}
+          className="px-4 py-2 flex-1 border rounded-lg bg-[#f9f9f9] outline-none focus:ring ring-primary"
+          type="text"
+          placeholder="Preferred Time"
+          required
+        />
       </div>
 
       <div className="flex space-x-8">
-        <input className="px-4 py-2 flex-1 border rounded-lg bg-[#f9f9f9] outline-none focus:ring ring-primary" type="text" placeholder="Address" />
+        <input
+          value={address}
+          onChange={({ target }) => updateFields({ address: target.value })}
+          className="px-4 py-2 flex-1 border rounded-lg bg-[#f9f9f9] outline-none focus:ring ring-primary"
+          type="text"
+          placeholder="Address"
+          required
+        />
       </div>
     </div>
   )
