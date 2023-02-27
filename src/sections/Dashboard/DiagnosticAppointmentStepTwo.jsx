1765edce44526207267a2e9b4913e8c1d646622e
asfand687 <asfandyar687@gmail.com>
2023-02-28 00:13:12 +0500

    added postal code

--- src/sections/Dashboard/DiagnosticAppointmentStepTwo.jsx
+++ src/sections/Dashboard/DiagnosticAppointmentStepTwo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import DatePicker from 'react-date-picker'
 import { BiCaretDown } from 'react-icons/bi'
 
-const DiagnosticAppointmentStepTwo = ({ time, address, selectedDate, updateFields, city, province }) => {
+const DiagnosticAppointmentStepTwo = ({ time, address, selectedDate, updateFields, city, province, postalCode }) => {
   const generateOptions = () => {
     const options = [];
 
@@ -48,11 +48,11 @@ const DiagnosticAppointmentStepTwo = ({ time, address, selectedDate, updateField
       </div>
       <div className="w-full flex gap-x-3">
         <input
-          value={address}
-          onChange={(e) => updateFields({ address: e.target.value })}
+          value={province}
+          onChange={() => updateFields({ province: "Quebec" })}
           className="px-4 py-2 flex-1 border rounded-lg bg-[#f9f9f9] outline-none focus:ring ring-primary"
           required
-          placeholder="Address"
+          placeholder="City"
           type="text"
         />
         <input
@@ -63,16 +63,40 @@ const DiagnosticAppointmentStepTwo = ({ time, address, selectedDate, updateField
           placeholder="City"
           type="text"
         />
-      </div>
-      <div className="w-full flex gap-x-3">
         <input
-          value={province}
+          value={postalCode}
+          onChange={(e) => updateFields({ postalCode: e.target.value })}
           className="px-4 py-2 flex-1 border rounded-lg bg-[#f9f9f9] outline-none focus:ring ring-primary"
           required
-          placeholder="City"
+          placeholder="Postal Code"
           type="text"
         />
       </div>
+      <div className="w-full flex">
+        <input
+          value={address}
+          onChange={(e) => updateFields({ address: e.target.value })}
+          className="px-4 py-2 flex-1 border rounded-lg bg-[#f9f9f9] outline-none focus:ring ring-primary"
+          required
+          placeholder="Address"
+          type="text"
+        />
+      </div>
+      {/* <div>
+        <label
+          htmlFor='files'
+          className="rounded-full appearance-none group hover:ring-1 hover:ring-primary  py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent px-8 flex-1"
+        >
+          Upload Requisition File
+        </label>
+        <input
+          required
+          onChange={({ target }) => updateFields({ file: target.files[0] })}
+          id="files"
+          className="hidden"
+          type="file"
+        />
+      </div> */}
     </div>
   )
 }
