6b9d1efada267cfbe5110d9745b9e1845e6bae3d
asfand687 <asfandyar687@gmail.com>
2023-03-01 04:52:57 +0500

    added faq component

---
+++ src/components/FAQ.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import { ArrowRight } from '../assets'
+
+const FAQ = ({ question, children }) => {
+  const [showAnswer, setShowAnswer] = useState(false)
+  return (
+    <div className="border-b border-dark pb-3">
+      <div onClick={() => setShowAnswer(!showAnswer)} className='flex justify-between'>
+        <article className="text-lg">
+          {question}
+        </article>
+        <ArrowRight className={`text-primary transform ${showAnswer ? "-rotate-90" : "rotate-90"}`} />
+      </div>
+      {
+        showAnswer && (
+          <div className="pt-3">
+            {children}
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+export default FAQ
