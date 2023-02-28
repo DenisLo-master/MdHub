865dd72c18cf1e3dab4a2255d8e415d13b091b1e
asfand687 <asfandyar687@gmail.com>
2023-02-28 17:11:00 +0500

    added date of birth and connected it with age

--- src/sections/Signup/ChildAccountForms.jsx
+++ src/sections/Signup/ChildAccountForms.jsx
@@ -1,15 +1,40 @@
 import React, { useState } from 'react'
+import DatePicker from 'react-date-picker'
 import { BsPlusCircle } from 'react-icons/bs'
 import { MdClose } from 'react-icons/md'
 import { registerationStore } from '../../store/registerationStore'
+import dayjs from "dayjs"
+import isBetween from 'dayjs/plugin/isBetween'
 
 const ChildAccountForms = () => {
-  const [isFourteenPlus, setIsFourteenPlus] = useState(true)
+  const [isOlderThanFourteen, setIsOlderThanFourteen] = useState(true)
   const handleChildAccountInputChange = registerationStore(state => state.handleChildAccountInputChange)
   const handleRemoveChildForms = registerationStore(state => state.handleRemoveChildForms)
   const childForms = registerationStore(state => state.childForms)
   const addChildAccount = registerationStore(state => state.addChildAccount)
   const selectedAccountType = registerationStore(state => state.selectedAccountType)
+
+  const handleDateChange = (date, index) => {
+    // Get current date
+    const currentDate = dayjs();
+
+    // Get date 14 years before current date
+    const minDate = currentDate.subtract(14, 'year');
+
+    // Get date 14 years after current date
+    const maxDate = currentDate.add(14, 'year');
+
+    // Convert selected date to dayjs object
+    const selectedDate = dayjs(date);
+
+    // Check if selected date is within 14 years range
+    if (selectedDate.isAfter(minDate) && selectedDate.isBefore(maxDate)) {
+      setIsOlderThanFourteen(false)
+    } else {
+      setIsOlderThanFourteen(true)
+    }
+    handleChildAccountInputChange({ target: { name: "dateOfBirth", value: date } }, index)
+  }
   return (
     <div>
       {
@@ -59,26 +84,71 @@ const ChildAccountForms = () => {
                 required
               />
             </div>
-            <div className="flex gap-x-4 items-center">
-              <input
-                type="checkbox"
-                value={isFourteenPlus}
-                checked={isFourteenPlus}
-                onChange={() => setIsFourteenPlus(!isFourteenPlus)}
-              />
-              <p>I am over fourteen years of age</p>
-            </div>
             <div className="w-full flex">
               <input
+                disabled={!isOlderThanFourteen}
                 className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
                 name="password"
                 type="password"
                 placeholder="Password*"
-                disabled={!isFourteenPlus}
                 value={childForms[index].password}
                 onChange={(event) => handleChildAccountInputChange(event, index)}
                 required
               />
+            </div>
+            <div className="text-xl flex items-center w-full gap-x-6">
+              <div className="flex-1">
+                <p className="pb-2">Gender</p>
+                <div className="w-full flex flex-wrap justify-around gap-x-4">
+                  <article className="flex gap-x-2 items-center">
+                    <input
+                      checked={childForms[index].gender === "male"}
+                      id="male"
+                      className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
+                      name="gender"
+                      type="radio"
+                      value={"male"}
+                      onChange={(event) => handleChildAccountInputChange(event, index)}
+                      required
+                    />
+                    <label htmlFor="male">Male</label>
+                  </article>
+                  <article className="flex items-center gap-x-2">
+                    <input
+                      checked={childForms[index].gender === "female"}
+                      id="female"
+                      className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
+                      name="gender"
+                      type="radio"
+                      value={"female"}
+                      onChange={(event) => handleChildAccountInputChange(event, index)}
+                      required
+                    />
+                    <label htmlFor='female'>Female</label>
+                  </article>
+                  <article className="flex items-center gap-x-2">
+                    <input
+                      id="preferNotToSay"
+                      className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
+                      name="gender"
+                      type="radio"
+                      value={"prefer not to say"}
+                      onChange={(event) => handleChildAccountInputChange(event, index)}
+                      required
+                    />
+                    <label htmlFor='preferNotToSay'>Prefer Not To Say</label>
+                  </article>
+                </div>
+              </div>
+              <div className="flex-1 flex">
+                <DatePicker
+                  className="flex-1 py-2 bg-[#f9f9f9] appearance-none px-4 border rounded-lg outline-none focus:ring ring-dark"
+                  value={childForms[index].dateOfBirth}
+                  onChange={(date) => handleDateChange(date, index)}
+                  format="dd-MM-y"
+                  placeholderText="Select a date"
+                />
+              </div>
             </div>
             {
               selectedAccountType === "family" ? (
@@ -87,7 +157,7 @@ const ChildAccountForms = () => {
                   <p className='text-xl'>add family member</p>
                 </button>
               ) : (
-                <button type="button" disabled={childForms.length > 9} onClick={addChildAccount} className="absolute bottom-4 left-8 flex text-gray-600 gap-x-2 font-body items-center text-3xl cursor-pointer">
+                  <button type="button" disabled={childForms.length > 8} onClick={addChildAccount} className="absolute bottom-4 left-8 flex text-gray-600 gap-x-2 font-body items-center text-3xl cursor-pointer">
                   <BsPlusCircle />
                   <p className='text-xl'>add employee</p>
                 </button>
