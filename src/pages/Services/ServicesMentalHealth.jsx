3aa934b2f2cc3744bec4a266c0b8dcf89e2e65eb
asfand687 <asfandyar687@gmail.com>
2023-03-02 00:26:43 +0500

    added new content to services mental health

--- src/pages/Services/ServicesMentalHealth.jsx
+++ src/pages/Services/ServicesMentalHealth.jsx
@@ -5,14 +5,13 @@ import {
   MeditationImage,
   MentalHealthBanner,
   MentalHealthImg,
-  ListIconImage,
-  CalenderIcon
+  HeadspaceLogo
 } from '../../assets'
 
 
 const ServicesMentalHealth = () => {
   return (
-    <section className="font-main">
+    <section className="font-main bg-white">
       <header className="bg-[#F6BA21] text-white w-full h-[560px] py-10">
         <div className="max-w-[86rem] mx-auto flex justify-between">
           <article className="space-y-5">
@@ -55,33 +54,50 @@ const ServicesMentalHealth = () => {
         </div>
       </section>
 
-      <section className="bg-[#F9F9F9] py-10">
-        <div className="w-full max-w-[85rem] mx-auto">
-          <h2 className="md:text-5xl text-center pb-12">How It Works</h2>
-          <div className="space-y-16">
-            <article className="flex justify-between items-center">
-              <div>
-                <h2 className="text-3xl">See a mental health physician</h2>
-                <p className="pt-10 pb-14 max-w-[650px]">Log into your MDHUB account and select “Mental Health Physician” from the menu of specialists. Next, choose a date and time that works for you. Quickly speak with a mental health physician for an initial assessment or follow-up. 30-minute initial appointments are $315, and 30-minute follow-up appointments are $230.</p>
-                <Link to="/register">
-                  <button className="rounded-full text-center w-44 py-1 border border-primary">
-                    Sign Up Today
-                  </button>
-                </Link>
-              </div>
-              <img src={ListIconImage} alt="List Icon" />
-            </article>
-            <article className="flex justify-between items-center">
-              <img src={CalenderIcon} alt="Calender" />
-              <div className="text-right">
-                <h2 className="text-3xl">See a mental health therapist</h2>
-                <p className="pt-10 pb-14 max-w-[650px]">See a mental health therapist, with no appointment necessary. All you need to do is choose the topic you’d like to discuss with a therapist and book an appointment. Sessions are 50 minutes in length. The fee for a session ranges from $120-$160, depending on your region. If you have insurance, this fee is often covered by health benefits.</p>
-                <button className="rounded-full text-center w-44 py-1 border border-primary">
-                  Sign Up Today
-                </button>
-              </div>
-            </article>
-          </div>
+      <section className="bg-[#F9F9F9] flex justify-center items-center gap-x-12 py-20">
+        <div>
+          <img src={HeadspaceLogo} alt="Headspace" />
+        </div>
+        <div className="w-full max-w-[530px] space-y-4">
+          <h2 className="text-4xl">What is Headspace?</h2>
+          <p className="font-body">
+            Headspace is an app that teaches you how to meditate.
+            Meditation has been shown to help people stress less, focus more and sleep better. Headspace is meditation made simple, teaching you life-changing mindfulness skills in just a few minutes a day.
+          </p>
+        </div>
+      </section>
+      <section className="py-20">
+        <h2 className="text-4xl text-center pb-16">What is Headspace?</h2>
+        <div className="w-full max-w-6xl mx-auto flex justify-between">
+          <article className="w-full max-w-[430px]">
+            <h2 className="text-3xl">See a mental health professional</h2>
+            <p className="font-body py-7">
+              Log into your MDHUB account and select “Mental Health Physician” from the menu of specialists. Next, choose a date and time that works for you. Quickly speak with a mental health physician for an initial assessment or follow-up. 30-minute initial appointments are $315, and 30-minute follow-up appointments are $230.
+            </p>
+            <Link to="/register">
+              <button
+                type="button"
+                className={`rounded-full w-48 font-main group hover:ring-1 hover:ring-primary py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent px-4`}
+              >
+                Sign Up Today
+              </button>
+            </Link>
+          </article>
+          <article className="w-full max-w-[430px]">
+            <h2 className="text-3xl">See a mental health specialist</h2>
+            <p className="font-body py-7">
+              See a mental health therapist, with no appointment necessary. All you need to do is choose the topic you’d like to discuss with a therapist and book an appointment. Sessions are 50 minutes in length. The fee for a session ranges from $120-$160, depending on your region. If you have insurance, this fee is often covered by health benefits.
+            </p>
+            <Link to="/register">
+              <button
+                type="button"
+                className={`rounded-full w-48 font-main group hover:ring-1 hover:ring-primary py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent px-4`}
+              >
+                Sign Up Today
+              </button>
+            </Link>
+          </article>
+
         </div>
       </section>
     </section>
