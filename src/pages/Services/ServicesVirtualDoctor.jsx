259c4f327ab9190dab681d47db46507b04b7815e
asfand687 <asfandyar687@gmail.com>
2023-03-02 02:25:35 +0500

    added new content for virtual doctor

--- src/pages/Services/ServicesVirtualDoctor.jsx
+++ src/pages/Services/ServicesVirtualDoctor.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import {
   VirtualDoctorBanner,
+  MedicalTreatmentAcne,
+  MedicalTreatmentED,
+  MedicalTreatmentUTI,
+  MedicalTreatmentBC,
+  ArrowGradient,
+  OkIconWhite,
+  HomeHowItWorks01,
+  HomeHowItWorks02,
+  HomeHowItWorks03,
+  HomeHowItWorks04
 } from '../../assets'
 
-import { medicalTreatmentData, virtualDoctorHowItWorksData } from '../../constants'
+import { virtualDoctorHowItWorksData } from '../../constants'
 
 const ServicesVirtualDoctor = () => {
   return (
@@ -23,7 +33,7 @@ const ServicesVirtualDoctor = () => {
                 Connect with a virtual doctor in minutes, 24/7. Or, book an appointment with a specialist – no referral needed.
               </p>
             </div>
-            <Link to="/register">
+            <Link className="block" to="/register">
               <button className="rounded-full w-[240px] text-lg flex justify-center py-3 border border-white">
                 Sign Up Today
               </button>
@@ -60,73 +70,190 @@ const ServicesVirtualDoctor = () => {
         </div>
       </section>
 
-      <section className="bg-[#F9F9F9] py-14">
-        <div className="w-full max-w-[38rem] mx-auto text-center space-y-4">
+      <section className="bg-primary text-white py-14">
+        <div className="w-full max-w-[52rem] mx-auto text-center space-y-4">
           <h3 className="text-4xl md:text-5xl">
             Get medical treatment online
           </h3>
-          <p className="font-body text-light text-dark">
+          <p className="font-body text-ligh">
             Our online doctors can treat your health conditions and concerns without you ever having to leave home. Book an appointment with specialists, like dermatologists, endocrinologists, mental health therapists, gynecologists, and more.
           </p>
         </div>
-        <div className="py-10 max-w-[83rem] mx-auto grid grid-cols-4 gap-x-4 gap-y-10">
-          {
-            medicalTreatmentData.map(item => (
-              <article
-                key={item.id}
-                className="max-w-[280px] shadow-cardService rounded-t"
-              >
-                <img className="w-full" src={item.image} alt="Diagnosis" />
-                <div className="bg-white flex flex-col items-center py-4 px-6">
-                  <h2 className="text-lg text-center pb-2">{item.title}</h2>
-                  <p className="font-body">
-                    {item.text}
-                  </p>
-                </div>
-              </article>
-            ))
-          }
-          <article className="max-w-[280px] rounded-t">
-            <div className="bg-primary h-[260px] flex justify-center items-center text-white mb-5">
-              <h2 className="text-3xl max-w-[100px] text-center">Sign Up Today</h2>
-            </div>
-            <Link to="/register">
-              <button className="w-full border text-2xl border-dark h-36 flex justify-center items-center">
-                See A Doctor
-              </button>
-            </Link>
-          </article>
-        </div>
+
+        <section className="py-10 max-w-[70rem] mx-auto">
+          <div className=" grid grid-cols-4 gap-8 text-lg">
+            <article>
+              <img className="w-full" src={MedicalTreatmentBC} alt="Birth Control" />
+              <div className="py-4 text-center bg-white text-dark">
+                Birth Control
+              </div>
+            </article>
+            <article>
+              <img className="w-full" src={MedicalTreatmentED} alt="Erectile Dysfunction" />
+              <div className="py-4 text-center bg-white text-dark">
+                Erectile Dysfunction
+              </div>
+            </article>
+            <article>
+              <img className="w-full" src={MedicalTreatmentAcne} alt="Acne" />
+              <div className="py-4 text-center bg-white text-dark">
+                Acne
+              </div>
+            </article>
+            <article>
+              <img className="w-full" src={MedicalTreatmentUTI} alt="Urinary Tract Infections" />
+              <div className="py-4 text-center bg-white text-dark">
+                Urinary tract infections
+              </div>
+            </article>
+
+            <article className="relative p-4 bg-white text-center text-dark rounded-md">
+              <p>Dermatology</p>
+              <ArrowGradient className="absolute top-[1.45rem] right-6" />
+            </article>
+
+            <article className="relative p-4 bg-white text-center text-dark rounded-md">
+              <p>Mental Health Therapy</p>
+              <ArrowGradient className="absolute top-[1.45rem] right-6" />
+            </article>
+
+            <article className="relative p-4 bg-white text-center text-dark rounded-md">
+              <p>Endocrinology</p>
+              <ArrowGradient className="absolute top-[1.45rem] right-6" />
+            </article>
+
+            <article className="relative p-4 bg-white text-center text-dark rounded-md">
+              <p>Naturopathy</p>
+              <ArrowGradient className="absolute top-[1.45rem] right-6" />
+            </article>
+
+            <article>
+              <Link TO="/register">
+                <button className="rounded-full w-full text-lg flex justify-center py-3 text-primary bg-white border border-white hover:bg-primary hover:text-white transition-colors duration-300 ease-out">
+                  Sign Up Today
+                </button>
+              </Link>
+            </article>
+
+            <article>
+              <Link to="/services/virtual-doctor">
+                <button className="rounded-full w-full text-lg flex justify-center py-3 text-white bg-transparent border border-white hover:bg-white hover:text-primary transition-colors duration-300 ease-out">
+                  See A Doctor
+                </button>
+              </Link>
+            </article>
+
+            <article className="col-span-2">
+              <Link to="/services">
+                <button className="rounded-full w-full text-lg flex justify-center py-3 text-white bg-transparent border border-white hover:bg-white hover:text-primary transition-colors duration-300 ease-out">
+                  View All Specialties and Treatments
+                </button>
+              </Link>
+            </article>
+          </div>
+
+          <div className="flex justify-around flex-wrap py-10 gap-x-4 gap-y-8">
+            <article className="flex items-center text-3xl gap-x-3 text-white">
+              <OkIconWhite />
+              <h2>diagnosis</h2>
+            </article>
+
+            <article className="flex items-center text-3xl gap-x-3 text-white">
+              <OkIconWhite />
+              <h2>doctors notes</h2>
+            </article>
+
+            <article className="flex items-center text-3xl gap-x-3 text-white">
+              <OkIconWhite />
+              <h2>lab requisitions</h2>
+            </article>
+
+            <article className="flex items-center text-3xl gap-x-3 text-white">
+              <OkIconWhite />
+              <h2>mental health</h2>
+            </article>
+
+            <article className="flex items-center text-3xl gap-x-3 text-white pr-8">
+              <OkIconWhite />
+              <h2>referrals to specialists</h2>
+            </article>
+
+            <article className="flex items-center text-3xl gap-x-3 text-white">
+              <OkIconWhite />
+              <h2>general health advice</h2>
+            </article>
+
+            <article className="flex items-center text-3xl gap-x-3 text-white pl-8">
+              <OkIconWhite />
+              <h2>prescriptions & renewals</h2>
+            </article>
+          </div>
+        </section>
       </section>
 
       <section className="py-10 bg-white text-gray-800">
-        <div className="w-full max-w-[83rem] mx-auto">
-          <h2 className="text-4xl pb-4 text-center">
-            How It Works
-          </h2>
-          <section>
-            {
-              virtualDoctorHowItWorksData.map(step => (
-                <article key={step.id} className="flex justify-between items-center">
-                  <div className="flex gap-x-8">
-                    <article className="w-20 h-20 text-3xl font-body font-semibold bg-primary text-white rounded-full flex justify-center items-center">
-                      {step.id}
-                    </article>
-                    <article className="w-full max-w-[590px] space-y-5">
-                      <h2 className="text-4xl">{step.title}</h2>
-                      <p className="font-body text-lg">
-                        {step.description}
-                      </p>
-                    </article>
-                  </div>
-                  <img src={step.image} alt={step.title} />
-                </article>
-              ))
-            }
+        <h2 className="text-4xl text-center pb-12">How it works</h2>
+        <div className="w-full max-w-[70rem] mx-auto">
+          <section className="flex justify-between">
+            <article className="max-w-[230px]">
+              <div className="bg-gradient-primary text-white rounded-full w-20 h-20 flex justify-center items-center font-body text-3xl">
+                1
+              </div>
+              <h2 className="text-lg pr-4 py-3">Describe your symptoms using our app </h2>
+              <p className="max-w-[215px] font-body">
+                Open the app or browser and click the “Get care” button. Describe your symptoms, and one of our healthcare providers will review your request in minutes.
+              </p>
+              <div className="pt-[4.5rem]">
+                <img src={HomeHowItWorks01} alt="How It Works" />
+              </div>
+            </article>
+
+            <article className="max-w-[230px]">
+              <div className="bg-gradient-primary text-white rounded-full w-20 h-20 flex justify-center items-center font-body text-3xl">
+                2
+              </div>
+              <h2 className="text-lg pr-4 py-3">Chat with a doctor</h2>
+              <p className="max-w-[215px] font-body pt-7">
+                Typically, you’ll be connected with a healthcare provider over text, but they may request a video or audio chat if necessary.
+              </p>
+              <div className="pt-24">
+                <img src={HomeHowItWorks02} alt="How It Works" />
+              </div>
+            </article>
+
+            <article className="max-w-[230px]">
+              <div className="bg-gradient-primary text-white rounded-full w-20 h-20 flex justify-center items-center font-body text-3xl">
+                3
+              </div>
+              <h2 className="text-lg text-dark pr-4 pt-3 leading-5 pb-1">Get a diagnosis or prescription, and or lab requisition or doctors note</h2>
+              <p className="max-w-[215px] font-body">
+                Our doctors will take the proper for each patient. They can diagnose, provide prescriptions and or lab requisitions when needed. Patients may also requests doctors not and or request a referral to specialists.
+              </p>
+              <div className="pt-6">
+                <img src={HomeHowItWorks03} alt="How It Works" />
+              </div>
+            </article>
+
+            <article className="max-w-[230px]">
+              <div className="bg-gradient-primary text-white rounded-full w-20 h-20 flex justify-center items-center font-body text-3xl">
+                4
+              </div>
+              <h2 className="text-lg pr-4 pt-3 pb-1 leading-5">Return to MDHUB DASHBOARD to access any of your additional membership benefits:</h2>
+              <p className="max-w-[215px] font-body">
+                Pharmacy, nursing, lab diagnostics, and mental health.
+              </p>
+              <div className="pt-32">
+                <img src={HomeHowItWorks04} alt="How It Works" />
+              </div>
+            </article>
           </section>
-          <div className="md:pl-28">
+
+          <div className="flex justify-center py-10">
             <Link to="/register">
-              <button className="rounded-full text-center w-44 py-1 border border-primary">
+              <button
+                type="button"
+                className={`rounded-full w-48 font-main group hover:ring-1 hover:ring-primary py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent px-4`}
+              >
                 Sign Up Today
               </button>
             </Link>
