603366769d9cd5135fcd4180d36579ba64120de3
asfand687 <asfandyar687@gmail.com>
2023-03-01 17:30:37 +0500

    completed lab requisition form

--- src/components/LabRequisitionForm.jsx
+++ src/components/LabRequisitionForm.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-
+import React, { useRef, useState } from 'react'
+import toast from "react-hot-toast"
 
 const INITIAL_DATA = {
   firstName: "",
@@ -12,7 +12,15 @@ const INITIAL_DATA = {
 
 const LabRequisitionForm = ({ hideForm }) => {
   const [file, setFile] = useState(null)
+  const fileRef = useRef(null)
   const [data, setData] = useState(INITIAL_DATA)
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   const handleFileInputChange = (event) => {
     setFile(event.target.files[0]);
   }
@@ -21,29 +29,28 @@ const LabRequisitionForm = ({ hideForm }) => {
     setData(prev => ({ ...prev, [e.target.name]: e.target.value }))
   }
 
-  console.log(file)
+  const submitForm = async (e) => {
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append("file", file)
+    formData.append("firstName", data.firstName)
+    formData.append("lastName", data.lastName)
+    formData.append("emailAddress", data.emailAddress)
+    formData.append("phoneNumber", data.phoneNumber)
+    formData.append("preferredDate", data.preferredDate)
+    formData.append("preferredTime", data.preferredTime)
 
-  const submitForm = async () => {
-    hideForm()
-    const formData = new FormData();
-    formData.append("reqform", file);
     try {
       const response = await fetch("http://localhost:8080/sendmail", {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          firstName: data.firstName,
-          lastName: data.lastName,
-          email: data.email,
-          phone: data.phoneNumber,
-          preferredData: data.preferredDate,
-          preferredTime: data.preferredTime,
-          ...formData
-        })
+        body: formData
       })
-      await response.json()
+      if (response.ok) {
+        toast.success("Your form has been uploaded, you can continue booking", {
+          id: "success booking"
+        })
+        hideForm()
+      }
     } catch (error) {
       console.log(error)
     }
@@ -54,9 +61,10 @@ const LabRequisitionForm = ({ hideForm }) => {
       <h2 className="text-4xl font-main">
         Laboratory Request Form
       </h2>
-      <form className="w-full py-10 space-y-2">
+      <div className="w-full py-10 space-y-2">
         <div className="flex space-x-8">
           <input
+            required
             value={data.firstName}
             onChange={handleChange}
             className="px-4 py-2 flex-1 border rounded-lg outline-none focus:ring ring-primary"
@@ -65,6 +73,7 @@ const LabRequisitionForm = ({ hideForm }) => {
             placeholder="First Name"
           />
           <input
+            required
             value={data.phoneNumber}
             onChange={handleChange}
             className="px-4 py-2 flex-1 border rounded-lg outline-none focus:ring ring-primary"
@@ -76,6 +85,7 @@ const LabRequisitionForm = ({ hideForm }) => {
 
         <div className="flex space-x-8">
           <input
+            required
             value={data.lastName}
             onChange={handleChange}
             className="px-4 py-2 flex-1 border rounded-lg outline-none focus:ring ring-primary"
@@ -84,6 +94,7 @@ const LabRequisitionForm = ({ hideForm }) => {
             placeholder="Last Name"
           />
           <input
+            required
             value={data.emailAddress}
             onChange={handleChange}
             className="px-4 py-2 flex-1 border rounded-lg outline-none focus:ring ring-primary"
@@ -95,6 +106,7 @@ const LabRequisitionForm = ({ hideForm }) => {
 
         <div className="flex space-x-8">
           <input
+            required
             value={data.preferredDate}
             onChange={handleChange}
             className="px-4 py-2 flex-1 border rounded-lg outline-none focus:ring ring-primary"
@@ -103,6 +115,7 @@ const LabRequisitionForm = ({ hideForm }) => {
             placeholder="Preferred Date (DD/MM/YYY)"
           />
           <input
+            required
             value={data.preferredTime}
             onChange={handleChange}
             className="px-4 py-2 flex-1 border rounded-lg outline-none focus:ring ring-primary"
@@ -112,15 +125,55 @@ const LabRequisitionForm = ({ hideForm }) => {
           />
         </div>
 
+        <div className="flex items-center pt-2">
+          <label className="mr-4 font-medium text-gray-700">Do you already have private insurance provider?</label>
+          <div className="inline-flex items-center">
+            <input
+              required
+              type="radio"
+              id="yes"
+              name="private_insurance"
+              value="yes"
+              className="hidden"
+              onChange={handleOptionChange}
+            />
+            <label
+              htmlFor="yes"
+              className={`inline-block w-4 h-4 rounded-full border-2 border-primary mr-2 ${selectedOption === "yes" ? "bg-primary border-primary" : ""
+                }`}
+            />
+            <span className="text-gray-700 mr-4">Yes</span>
+          </div>
+          <div className="inline-flex items-center">
+            <input
+              required
+              type="radio"
+              id="no"
+              name="private_insurance"
+              value="no"
+              className="hidden"
+              onChange={handleOptionChange}
+            />
+            <label
+              htmlFor="no"
+              className={`inline-block w-4 h-4 rounded-full border-2 border-primary mr-2 ${selectedOption === "no" ? "bg-primary border-primary" : ""
+                }`}
+            />
+            <span className="text-gray-700">No</span>
+          </div>
+        </div>
+
         <div className="flex justify-around flex-wrap space-x-8 pt-4">
           <div className="flex-1 flex space-x-4 text-base font-main">
             <label htmlFor='uploadForm' className={`rounded-full group hover:ring-1 hover:ring-primary  py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent px-4`}>
               Upload Requisition File
             </label>
             <input
+              required
+              name="file"
+              className='hidden'
               id="uploadForm"
               type="file"
-              className="hidden"
               onChange={handleFileInputChange}
             />
             <button
@@ -132,7 +185,7 @@ const LabRequisitionForm = ({ hideForm }) => {
             </button>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   )
 }
