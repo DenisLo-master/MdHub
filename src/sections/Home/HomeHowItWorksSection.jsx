398af82e080685458a111e386c392977fbaf9ccb
asfand687 <asfandyar687@gmail.com>
2023-03-04 00:07:51 +0500

    removed placeholder text

--- src/sections/Home/HomeHowItWorksSection.jsx
+++ src/sections/Home/HomeHowItWorksSection.jsx
@@ -6,9 +6,6 @@ const HomeHowItWorksSection = () => {
     <section>
       <article className="w-full max-w-[90rem] mx-auto pt-10 pb-20">
         <h2 className="text-4xl font-main text-center">How It Works</h2>
-        <p className="max-w-[480px] mx-auto text-center pt-3">
-          Lorem ipsum dolor sit amet consectetur. Ac varius eleifend auctor laoreet tempor fringilla dolor viverra. Sagittis imperdiet ut ut urna pharetra malesuada fames elementum. Gravida nunc erat eu pharetra sit.
-        </p>
         <Slider />
       </article>
     </section>
