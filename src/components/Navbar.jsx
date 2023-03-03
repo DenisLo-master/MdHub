aa0cf7b182cd7e6c1f4a93afb409b26414c6636c
asfand687 <asfandyar687@gmail.com>
2023-03-03 19:32:23 +0500

    increased spacing in sign out label

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -84,7 +84,7 @@ const Navbar = () => {
                 <button
                   onClick={handleSignout}
                   className="rounded-full px-9 py-1 border border-primary text-white bg-primary hover:bg-transparent hover:text-primary transition-all ease-in-out duration-300">
-                  Signout
+                  Sign Out
                 </button>
               </div>
             ):
