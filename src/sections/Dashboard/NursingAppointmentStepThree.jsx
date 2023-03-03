2d80f0e033428d25b209ccdf1e04a04b81efd0e8
asfand687 <asfandyar687@gmail.com>
2023-03-04 03:21:04 +0500

    removed redundant data

--- src/sections/Dashboard/NursingAppointmentStepThree.jsx
+++ src/sections/Dashboard/NursingAppointmentStepThree.jsx
@@ -5,31 +5,23 @@ import { registerationStore } from '../../store/registerationStore'
 const NursingAppointmentStepThree = () => {
   const selectedHomecareOptions = registerationStore(state => state.selectedHomecareOptions)
   const selectedNursingOptions = registerationStore(state => state.selectedNursingOptions)
+  const nursingAppointmentBill = registerationStore(state => state.nursingAppointmentBill)
 
-  const getAmount = () => {
-    if (selectedHomecareOptions.length && selectedNursingOptions.length) {
-      return (99)
-    } else if (selectedHomecareOptions.length && !selectedNursingOptions.length) {
-      return 59.99
-    } else {
-      return 99
-    }
-  }
   return (
     <div>
       <div className="flex justify-between pb-5">
         <article className="space-y-4">
           <h2 className="text-2xl">Your Order</h2>
           <h3 className="font-body">
-            1 hour minimum nursing fee: ${getAmount()}
+            1 hour minimum nursing fee: ${nursingAppointmentBill}
           </h3>
         </article>
         <article className="space-y-4 text-right">
           <h2 className="text-2xl">Subtotal</h2>
           <div className="font-body">
-            <h3>${getAmount()}</h3>
-            <h3>Tax: $47.78</h3>
-            <h2 className="text-3xl">Total: <strong>${Math.floor(getAmount() + 47.78)}</strong></h2>
+            <h3>${nursingAppointmentBill}</h3>
+            <h3>Tax: ${(nursingAppointmentBill * 0.09975).toFixed(2)}</h3>
+            <h2 className="text-3xl">Total: <strong>${(nursingAppointmentBill + (nursingAppointmentBill * 0.09975)).toFixed(2)}</strong></h2>
           </div>
         </article>
       </div>
