cb1f7abf58e3cf52ec9b8b20c6155fc0e4c81f24
asfand687 <asfandyar687@gmail.com>
2023-03-06 23:05:51 +0500

    added tax

--- src/sections/Dashboard/DiagnosticAppointmentStepThree.jsx
+++ src/sections/Dashboard/DiagnosticAppointmentStepThree.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import { CardElement } from '@stripe/react-stripe-js'
+import { registerationStore } from '../../store/registerationStore'
 
 const DiagnosticAppointmentStepThree = () => {
+  const diagnosticsAppointmentBill = registerationStore(state => state.diagnosticsAppointmentBill)
   return (
     <div>
       <div className="flex justify-between pb-5">
         <article className="space-y-4">
           <h2 className="text-2xl">Your Order</h2>
           <h3 className="font-body">
-            1 hour minimum nursing fee: $99
+            1 hour minimum nursing fee: ${diagnosticsAppointmentBill}
           </h3>
         </article>
         <article className="space-y-4 text-right">
           <h2 className="text-2xl">Subtotal</h2>
           <div className="font-body">
-            <h3>$99</h3>
-            <h3>Tax: $47.78</h3>
-            <h2 className="text-3xl">Total: <strong>${Math.floor(99 + 47.78)}</strong></h2>
+            <h3>${diagnosticsAppointmentBill}</h3>
+            <h3>Tax: ${(diagnosticsAppointmentBill * 0.14975).toFixed(2)}</h3>
+            <h2 className="text-3xl">Total: <strong>${(diagnosticsAppointmentBill + (diagnosticsAppointmentBill * 0.014975)).toFixed(2)}</strong></h2>
           </div>
         </article>
       </div>
