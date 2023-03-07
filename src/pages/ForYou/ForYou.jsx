4a102f74a0ff14da2af06b782bd65283173e3d89
asfand687 <asfandyar687@gmail.com>
2023-03-07 14:53:06 +0500

    changed the layout for foryou page

--- src/pages/ForYou/ForYou.jsx
+++ src/pages/ForYou/ForYou.jsx
@@ -1,51 +1,101 @@
 import React from 'react'
 import { ForYouHeaderSection } from '../../sections'
 import { Link } from 'react-router-dom'
-import { ForYouImage01, ForYouImage02, ForYouImage03, ForYouImage04, ArrowIcon } from '../../assets'
+import { ForYouFamilyPlan, OkIconWhite } from '../../assets'
 
 const ForYou = () => {
   return (
     <section>
       <ForYouHeaderSection />
+      <div className="py-10 text-primary font-main">
+        <h2 className="flex text-center justify-center gap-x-3 items-center text-5xl md:text-6xl">
+          Get help with
+          <span className="text-dark">
+            seasonal depression
+          </span>
+        </h2>
+      </div>
       <section className="w-full max-w-[1130px] mx-auto py-10 font-main">
-        <div className="space-y-6">
-          <h2 className="text-4xl">A new, personalized standard of healthcare</h2>
-          <p className="font-body font-light">Whether it’s getting a prescription for a common illness or accessing a medical laboratory, MDHUB makes it quick and easy to get the care you need. With over 150 doctors registered on our platform, including a variety of specialists, you can save time and the headache of the usual trip to the doctor’s office.
-          </p>
-          <Link to="/register" className={`rounded-full text-lg shadow-cardService group hover:ring-1 hover:ring-primary px-10 py-3 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
-            Register Today
-          </Link>
-        </div>
-
-        <div className="flex justify-center py-16">
-          <article className="p-12 px-14 bg-[#f9f9f9] shadow-cardService rounded-tl-3xl rounded-bl-3xl">
-            <h2 className="text-4xl pb-6">Your health, in your hands. </h2>
-            <div className="font-body text-sm max-w-[350px] space-y-6">
-              <p>
-                Your life is full – and you don’t have time to wait three or more weeks to see a doctor. Our memberships allow you to receive highly-personalized and compassionate care fast for both your mental and physical health.
-                So you never have to wait to see a doctor again. Learn more about our plans, so you never have to wait to see a doctor again.
+        <article className="w-full bg-linearGradientPrimary rounded-lg py-10 pb-32 text-white space-y-6">
+          <h2 className="text-center text-4xl">What you get with your membership</h2>
+          <ul className="flex justify-center flex-wrap font-body text-white gap-x-16 gap-y-8 relative">
+            <li className="flex w-[380px] gap-x-2">
+              <OkIconWhite className="w-6 h-6" />
+              <p className="w-full">
+                Instant access to a doctor for free, anytime, from anywhere
               </p>
-              <Link to="/services" className={`rounded-full text-xl group hover:ring-1 hover:ring-primary px-12 py-3 border flex justify-center items-center space-x-2  border-primary bg-transparent font-main`}>
-                Explore Our Services
+            </li>
+            <li className="flex w-[380px] gap-x-2">
+              <OkIconWhite className="w-6 h-6" />
+              <p className="w-full">
+                Most of your medical needs in one package
+              </p>
+            </li>
+            <li className="flex w-[380px] gap-x-2">
+              <OkIconWhite className="w-6 h-6" />
+              <p className="w-full">
+                Prescriptions, lab work, specialist, vaccines and mental health in one place
+              </p>
+            </li>
+            <li className="flex w-[380px] gap-x-2">
+              <OkIconWhite className="w-6 h-6" />
+              <p className="w-full">
+                No travel, traffic or parking or wait time required
+              </p>
+            </li>
+            <li className="flex w-[380px] gap-x-2">
+              <OkIconWhite className="w-6 h-6" />
+              <p className="w-full">
+                Control your medical outlook, be proactive and solve health issues
+              </p>
+            </li>
+            <li className="flex w-[380px] gap-x-2">
+              <OkIconWhite className="w-6 h-6" />
+              <p className="w-full">
+                Coverage for your whole family
+              </p>
+            </li>
+            <li className="absolute -bottom-20 left-36">
+              <Link className="block" to="/register">
+                <button className="rounded-full w-[240px] text-lg flex justify-center py-3 border border-white">
+                  Sign Up Today
+                </button>
               </Link>
+            </li>
+          </ul>
+        </article>
 
-            </div>
-          </article>
-          <article>
-            <img className="max-w-[650px] h-full" src={ForYouImage01} alt="For You" />
-          </article>
-        </div>
+        <article className="flex gap-x-8 py-16 text-dark">
+          <div className="flex-1 space-y-4 font-body">
+            <h2 className="text-5xl font-main max-w-[500px]">
+              The whole family in one affordable plan
+            </h2>
+            <p>
+              A hand-picked network of online doctors in Canada ready to help you.
+            </p>
+            <p className="pb-2">
+              Looking for a doctor who’s accepting consultations from new patients? Our physicians, nurse practitioners, and allied healthcare professionals are licensed and practice in their respective fields right here in Canada. See a healthcare provider in minutes or book an online doctor’s appointment with a specialist.
+            </p>
+            <Link className="block" to="/register">
+              <button className="rounded-full w-[240px] text-lg flex justify-center py-2 border border-primary">
+                Sign Up Today
+              </button>
+            </Link>
+          </div>
+          <div className="flex-1 space-y-4 font-body">
+            <img className="w-full object-cover" src={ForYouFamilyPlan} alt="For you family plan" />
+          </div>
+        </article>
+      </section>
 
-        <div className="py-10 space-y-6">
-          <h2 className="text-4xl">You and your loved ones deserve more. </h2>
-          <p className="font-body font-light text-sm pb-3">
-            MDHUB was developed with you and your family in mind. MDHUB is the ultimate solution for entire families and is a great way to keep children and teens healthy.
-          </p>
-          <Link to="/services">
-            <button className={`rounded-full text-xl group hover:ring-1 hover:ring-primary px-12 py-3 border flex justify-center items-center space-x-2  border-primary bg-transparent font-main`}>
-              Explore Our Services
-            </button>
-          </Link>
+      <section className="bg-forYouBottom font-main bg-cover bg-no-repeat bg-center">
+        <div className="w-full max-w-[1160px] text-3xl mx-auto h-[390px] flex items-center text-white">
+          <div className="space-y-6">
+            <h2 className="max-w-[450px] pb-">
+              “Just registered for a membership, chose a doctor & made a same-day appointment at faster than I buy my morning coffee.”
+            </h2>
+            <h2>— MDHUB Member in Montreal</h2>
+          </div>
         </div>
       </section>
     </section>
