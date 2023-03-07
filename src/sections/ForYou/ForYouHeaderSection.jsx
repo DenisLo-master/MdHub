76bf06325518fa0ff191eb0ac5f843ec705283f9
asfand687 <asfandyar687@gmail.com>
2023-03-07 14:55:02 +0500

    added new header section

--- src/sections/ForYou/ForYouHeaderSection.jsx
+++ src/sections/ForYou/ForYouHeaderSection.jsx
@@ -5,8 +5,11 @@ import ForYouNavCard from '../../components/ForYouNavCard'
 const ForYouHeaderSection = () => {
   return (
     <section className="bg-forYouHero font-main bg-cover bg-no-repeat bg-center">
-      <h2 className="py-10 text-4xl text-center">For You</h2>
-
+      <div className="w-full max-w-7xl mx-auto h-[390px] flex items-center">
+        <h2 className="text-[88px] max-w-[450px] leading-[80px] pb-5 text-white">
+          healthcare, without the headaches
+        </h2>
+      </div>
     </section>
   )
 }
