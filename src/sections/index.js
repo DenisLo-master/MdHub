6bcaab38551b4056dc82f3ddaba4786fa29b26b1
asfand687 <asfandyar687@gmail.com>
2023-03-08 15:39:58 +0500

    added separate components to index

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,42 +1,48 @@
-import HomeHeroSection from "./Home/HomeHeroSection";
-import HomeExploreSolutionsSection from "./Home/HomeExploreSolutionsSection";
-import HomeHeadspaceSection from "./Home/HomeHeadspaceSection";
-import HomeBrandsSection from "./Home/HomeBrandsSection";
-import HomeHowItWorksSection from "./Home/HomeHowItWorksSection";
-import HomeMembershipCostCtaSection from "./Home/HomeMembershipCostCtaSection";
-import HomeMembershipBenefitsSection from "./Home/HomeMembershipBenefitsSection";
-import ServicesHeaderSection from "./Services/ServicesHeaderSection";
+import HomeHeroSection from "./Home/HomeHeroSection"
+import HomeExploreSolutionsSection from "./Home/HomeExploreSolutionsSection"
+import HomeHeadspaceSection from "./Home/HomeHeadspaceSection"
+import HomeBrandsSection from "./Home/HomeBrandsSection"
+import HomeHowItWorksSection from "./Home/HomeHowItWorksSection"
+import HomeMembershipCostCtaSection from "./Home/HomeMembershipCostCtaSection"
+import HomeMembershipBenefitsSection from "./Home/HomeMembershipBenefitsSection"
+import ServicesHeaderSection from "./Services/ServicesHeaderSection"
 import ForYouHeaderSection from './ForYou/ForYouHeaderSection'
-import SignupStepOne from "./Signup/SignupStepOne";
-import SignupStepTwo from "./Signup/SignupStepTwo";
-import SignupStepThree from "./Signup/SignupStepThree";
-import FirstFamilyMemberForm from "./Signup/FirstFamilyMemberForm";
-import SecondFamilyMemberForm from "./Signup/SecondFamilyMemberForm";
-import ThirdFamilyMemberForm from "./Signup/ThirdFamilyMemberForm";
-import FourthFamilyMemberForm from "./Signup/FourthFamilyMemberForm";
-import ChildAccountForms from "./Signup/ChildAccountForms";
-import ParentAccountForm from "./Signup/ParentAccountForm";
-import SignupAccountsTypeNav from "./Signup/SignupAccountsTypeNav";
-import DashboardUserInfo from "./Dashboard/DashboardUserInfo";
-import NursingModal from "./Dashboard/NursingModal";
-import LabTestingModal from "./Dashboard/LabTestingModal";
-import OnlinePharmacyModal from "./Dashboard/OnlinePharmacyModal";
-import UserPaymentForm from "./Dashboard/UserPaymentForm";
-import NursingAppointmentForm from "./Dashboard/NursingAppointmentForm";
-import NursingAppointmentStepOne from "./Dashboard/NursingAppointmentStepOne";
-import NursingAppointmentStepTwo from "./Dashboard/NursingAppointmentStepTwo";
-import NursingAppointmentStepThree from "./Dashboard/NursingAppointmentStepThree";
-import DiagnosticAppointmentStepOne from "./Dashboard/DiagnosticAppointmentStepOne";
-import DiagnosticAppointmentStepTwo from "./Dashboard/DiagnosticAppointmentStepTwo";
-import UpdateBillingSection from "./Dashboard/UpdateBillingSection";
-import DiagnosticAppointmentStepThree from "./Dashboard/DiagnosticAppointmentStepThree";
-import DiagnosticsAppointmentForm from "./Dashboard/DiagnosticsAppointmentForm";
-import HomeUserSatisfactionSection from "./Home/HomeUserSatisfactionSection";
+import SignupStepOne from "./Signup/SignupStepOne"
+import SignupStepTwo from "./Signup/SignupStepTwo"
+import SignupStepThree from "./Signup/SignupStepThree"
+import FirstFamilyMemberForm from "./Signup/FirstFamilyMemberForm"
+import SecondFamilyMemberForm from "./Signup/SecondFamilyMemberForm"
+import ThirdFamilyMemberForm from "./Signup/ThirdFamilyMemberForm"
+import FourthFamilyMemberForm from "./Signup/FourthFamilyMemberForm"
+import ChildAccountForms from "./Signup/ChildAccountForms"
+import ParentAccountForm from "./Signup/ParentAccountForm"
+import SignupAccountsTypeNav from "./Signup/SignupAccountsTypeNav"
+import DashboardUserInfo from "./Dashboard/DashboardUserInfo"
+import NursingModal from "./Dashboard/NursingModal"
+import LabTestingModal from "./Dashboard/LabTestingModal"
+import OnlinePharmacyModal from "./Dashboard/OnlinePharmacyModal"
+import UserPaymentForm from "./Dashboard/UserPaymentForm"
+import NursingAppointmentForm from "./Dashboard/NursingAppointmentForm"
+import NursingAppointmentStepOne from "./Dashboard/NursingAppointmentStepOne"
+import NursingAppointmentStepTwo from "./Dashboard/NursingAppointmentStepTwo"
+import NursingAppointmentStepThree from "./Dashboard/NursingAppointmentStepThree"
+import DiagnosticAppointmentStepOne from "./Dashboard/DiagnosticAppointmentStepOne"
+import DiagnosticAppointmentStepTwo from "./Dashboard/DiagnosticAppointmentStepTwo"
+import UpdateBillingSection from "./Dashboard/UpdateBillingSection"
+import DiagnosticAppointmentStepThree from "./Dashboard/DiagnosticAppointmentStepThree"
+import DiagnosticsAppointmentForm from "./Dashboard/DiagnosticsAppointmentForm"
+import HomeUserSatisfactionSection from "./Home/HomeUserSatisfactionSection"
 import RegistrationCheckoutForm from "./Signup/RegistrationCheckoutForm.jsx"
-
-
+import PricingSlider from "./Pricing/PricingSlider"
+import OnDemandCTA from './Pricing/OnDemandCTA'
+import PricingHeader from './Pricing/PricingHeader'
+import HeadspaceSection from './Pricing/HeadspaceSection'
 
 export {
+  HeadspaceSection,
+  PricingHeader,
+  OnDemandCTA,
+  PricingSlider,
   UpdateBillingSection,
   DiagnosticAppointmentStepThree,
   DiagnosticsAppointmentForm,
