9ff60043116edff3a894b0a22d314eeb4aa46f9c
asfand687 <asfandyar687@gmail.com>
2023-03-10 18:30:31 +0500

    made responsive

--- src/pages/Services/ServicesDiagnostics.jsx
+++ src/pages/Services/ServicesDiagnostics.jsx
@@ -7,13 +7,13 @@ const WORDS = ['virus', 'urine', 'blood', 'covid']
 const ServicesDiagnostics = () => {
   return (
     <section className="font-main text-dark py-10">
-      <header className="bg-diagnostics diagnostics-bg text-white w-full py-20 relative">
+      <header className="bg-diagnostics lg:diagnostics-bg text-white w-full py-20 relative">
         <div className="max-w-[83rem] mx-auto">
-          <article className="space-y-5">
-            <h2 className="text-[88px] max-w-[450px] leading-[80px] pb-5">
+          <article className="space-y-5 px-2">
+            <h2 className="text-[65px] lg:text-[88px] max-w-[450px] leading-[80px] pb-5">
               lab testing & diagnostics
             </h2>
-            <div className="space-y-5 font-body text-lg max-w-lg pb-8 leading-5">
+            <div className="space-y-5 font-body text-lg max-w-lg pb-8 leading-5 px-2">
               <p>
                 Avoid the wait at hospitals and clinics with safe, fast, and professional lab testing from the comfort of your home or office.
               </p>
@@ -22,7 +22,7 @@ const ServicesDiagnostics = () => {
                 Our certified partner labs can process over 300 different screenings to provide you with a comprehensive health assessment – and get you answers fast. 
               </p>
             </div>
-            <Link to="register">
+            <Link className="block px-2" to="register">
               <button className="rounded-full w-[240px] text-lg flex justify-center py-3 border bg-white text-primary border-white hover:opacity-80 transition-opacity ease-in-out duration-300">
                 Sign Up Today
               </button>
@@ -30,8 +30,8 @@ const ServicesDiagnostics = () => {
           </article>
         </div>
       </header>
-      <section className="bg-white py-14">
-        <div className="relative virtual-health text-center flex justify-center flip-words">
+      <section className="bg-white lg:py-14">
+        <div className="hidden relative virtual-health text-center lg:flex justify-center flip-words">
           <h2 className="text-primary text-center text-5xl md:text-6xl">
             Get help for
           </h2>
@@ -49,11 +49,11 @@ const ServicesDiagnostics = () => {
         </div>
       </section>
       <section className="bg-white py-10 pb-24">
-        <h2 className="text-4xl pb-4 text-center">
+        <h2 className="text-4xl pb-10 text-center">
           How It Works
         </h2>
         <section className="w-full max-w-6xl space-y-6 mx-auto">
-          <div className="flex justify-between">
+          <div className="flex flex-col md:flex-row justify-between">
             {
               diagnosisHowItWorksData.map((step => (
                 <article key={step.id} className="flex flex-col justify-between items-center">
@@ -85,7 +85,7 @@ const ServicesDiagnostics = () => {
                       }
                     </article>
                   </div>
-                  <img src={step.image} alt={step.text} />
+                  <img className="w-full max-w-[288px] pt-4" src={step.image} alt={step.text} />
                 </article>
               )))
             }
