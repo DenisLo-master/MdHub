68d69e24d9d4033488c6078cbae716b39f09c5d9
asfand687 <asfandyar687@gmail.com>
2023-03-10 18:31:28 +0500

    made responsive

--- src/sections/Pricing/MembershipBenefits.jsx
+++ src/sections/Pricing/MembershipBenefits.jsx
@@ -5,13 +5,15 @@ import { OkIcon } from '../../assets'
 const MembershipBenefits = () => {
   return (
     <section className="flex justify-center py-10">
-      <div className="w-full max-w-[1000px] rounded-lg bg-gradient-primary text-white py-10 px-16">
+      <div className="w-full max-w-[1000px] rounded-lg bg-gradient-primary text-white py-10 px-8 lg:px-16">
         <h2 className="text-4xl text-center pb-7">
           Members Benefit From:
         </h2>
-        <ul className="grid grid-cols-2 gap-x-16 gap-y-8">
+        <ul className="grid grid-cols-1 lg:grid-cols-2 gap-x-16 gap-y-8">
           <li className="flex gap-x-4 items-center">
-            <OkIcon className="w-8 h-8" />
+            <div>
+              <OkIcon className="w-8 h-8" />
+            </div>
             <p className="text-2xl">
               Free 24/7 unlimited access to doctors
             </p>
@@ -35,7 +37,9 @@ const MembershipBenefits = () => {
             </p>
           </li>
           <li className="flex gap-x-4 items-center">
-            <OkIcon className="w-8 h-8" />
+            <div>
+              <OkIcon className="w-8 h-8" />
+            </div>
             <p className="text-2xl">
               Mental health platform and support
             </p>
