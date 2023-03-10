d7a257be2a6f79f9221e7893bc12aa56ef23d7b0
asfand687 <asfandyar687@gmail.com>
2023-03-10 18:30:59 +0500

    made responsive

--- src/sections/ForYou/ForYouHeaderSection.jsx
+++ src/sections/ForYou/ForYouHeaderSection.jsx
@@ -3,10 +3,10 @@ import { ForYouHeaderBg } from '../../assets'
 
 const ForYouHeaderSection = () => {
   return (
-    <section className="relative font-main bg-cover bg-no-repeat bg-center">
-      <img className="absolute object-cover w-full h-[400px]" src={ForYouHeaderBg} alt="For You" />
+    <section className="relative bg-[#DB8E96] font-main bg-cover bg-no-repeat bg-center px-4">
+      <img className="hidden lg:block absolute object-cover w-full h-[400px]" src={ForYouHeaderBg} alt="For You" />
       <div className="relative z-10 w-full max-w-7xl mx-auto h-[390px] flex items-center">
-        <h2 className="text-[88px] max-w-[450px] leading-[80px] pb-5 text-white">
+        <h2 className="text-[70px] lg:text-[88px] max-w-[450px] leading-[80px] pb-5 text-white">
           healthcare, without the headaches
         </h2>
       </div>
