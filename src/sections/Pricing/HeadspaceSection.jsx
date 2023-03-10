1fb79b8e5ddb296fb0c7dddc0c36a6c8338a5cdf
asfand687 <asfandyar687@gmail.com>
2023-03-10 18:31:24 +0500

    made responsive

--- src/sections/Pricing/HeadspaceSection.jsx
+++ src/sections/Pricing/HeadspaceSection.jsx
@@ -4,8 +4,8 @@ import { HeadspaceLogo } from '../../assets'
 const HeadspaceSection = () => {
   return (
     <div className="py-10 flex justify-center font-main">
-      <article className="max-w-[1200px] flex justify-between items-center">
-        <h3 className="text-3xl max-w-[600px]">
+      <article className="max-w-[1200px] flex flex-col-reverse lg:flex-row justify-between items-center">
+        <h3 className="text-3xl max-w-[600px] text-center lg:text-left">
           Free 3 months of Headspace membership included in all our plans. ($36 value)
         </h3>
         <img className="w-full max-w-[500px]" src={HeadspaceLogo} alt="Headspace" />
