aa759ec303a22dd28fe76c2c03cf3924f45dc47f
asfand687 <asfandyar687@gmail.com>
2023-03-13 18:43:48 +0500

    changed api link to production

--- src/sections/Signup/RegistrationCheckoutForm.jsx
+++ src/sections/Signup/RegistrationCheckoutForm.jsx
@@ -113,7 +113,7 @@ const RegistrationCheckoutForm = () => {
       });
       console.log(paymentMethod)
       if (!error) {
-        const response = await fetch('http://localhost:8080/api/v1/auth/register', {
+        const response = await fetch('https://mdhub-server.onrender.com/api/v1/auth/register', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
