f73486ca92abaa5ac02d1d2c4c027f91528640ff
asfand687 <asfandyar687@gmail.com>
2023-03-15 02:28:06 +0500

    added different layouts for admin

--- src/App.jsx
+++ src/App.jsx
@@ -39,36 +39,57 @@ function App() {
     AOS.init()
   }, [])
 
+  const AdminLayout = ({ children }) => {
+    return (
+      <div className="overflow-x-hidden text-gray-800">
+        <main className="w-full bg-white min-h-[calc(100vh-73px)]">
+          {children}
+        </main>
+      </div>
+    );
+  }
+
+  const DefaultLayout = ({ children }) => {
+    return (
+      <div className="overflow-x-hidden text-gray-800">
+        <Navbar />
+        <main className="w-full bg-white min-h-[calc(100vh-73px)]">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    );
+  }
+
   return (
     <div className="overflow-x-hidden text-gray-800">
       <BrowserRouter>
-      <Navbar/>
         <Toaster />
         <main className="w-full bg-white min-h-[calc(100vh-73px)]">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="services" element={<Services />}>
+            <Route path="/" element={<DefaultLayout><Home /></DefaultLayout>} />
+            <Route path="services" element={<DefaultLayout><Services /></DefaultLayout>}>
               <Route index element={<ServicesVirtualDoctor />} />
               <Route path="mental-health" element={<ServicesMentalHealth />} />
               <Route path="diagnostics" element={<ServicesDiagnostics />} />
               <Route path="nursing-homecare" element={<ServicesNursingAndHomecare />} />
               <Route path="online-pharmacy" element={<ServicesOnlinePharmacy />} />
             </Route>
-            <Route path="/for-you" element={<ForYou />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="/for-you" element={<DefaultLayout><ForYou /></DefaultLayout>} />
+            <Route path="/about" element={<DefaultLayout><About /></DefaultLayout>} />
+            <Route path="/contact" element={<DefaultLayout><Contact /></DefaultLayout>} />
+            <Route path="/dashboard" element={<DefaultLayout><Dashboard /></DefaultLayout>}>
               <Route index element={<DashboardHome />} />
               <Route path="book-appointment" element={<BookAppointment />} />
               <Route path="update-user" element={<UpdateUserInfo />} />
               <Route path="update-billing" element={<UpdateBillingInfo />} />
             </Route>
-            <Route path="/rates" element={<Pricing />} />
-            <Route path="/for-corporate" element={<CorporateOverview />} />
-            <Route path="/for-family" element={<ForYou />} />
-            <Route path="/register" element={<Signup />}/>
-            <Route path="/login" element={<Login />}/>
-            <Route path="/admin" element={<Admin />}>
+            <Route path="/rates" element={<DefaultLayout><Pricing /></DefaultLayout>} />
+            <Route path="/for-corporate" element={<DefaultLayout><CorporateOverview /></DefaultLayout>} />
+            <Route path="/for-family" element={<DefaultLayout><ForYou /></DefaultLayout>} />
+            <Route path="/register" element={<DefaultLayout><Signup /></DefaultLayout>} />
+            <Route path="/login" element={<DefaultLayout><Login /></DefaultLayout>} />
+            <Route path="/admin" element={<AdminLayout><Admin /></AdminLayout>}>
               <Route index element={<AdminDashboard />} />
               <Route path="all_users" element={<AllUsers />} />
               <Route path="add_user" element={<AddUser />} />
@@ -76,7 +97,6 @@ function App() {
             </Route>
           </Routes>
         </main>
-      <Footer/>
       </BrowserRouter>
     </div>
   )
