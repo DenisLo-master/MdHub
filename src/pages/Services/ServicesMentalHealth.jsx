b81205e73cea018aa08f4d6e8468a86c65015ee4
asfand687 <asfandyar687@gmail.com>
2023-03-17 02:56:09 +0500

    added translations to mental health

--- src/pages/Services/ServicesMentalHealth.jsx
+++ src/pages/Services/ServicesMentalHealth.jsx
@@ -7,25 +7,27 @@ import {
   MentalHealthImg,
   HeadspaceLogo
 } from '../../assets'
+import { useTranslation } from 'react-i18next'
 
 
 const WORDS = ['anxiety', 'mood changes', 'depression', 'seasonal depression', "insomnia", "stress"]
 
 const ServicesMentalHealth = () => {
+  const { t } = useTranslation()
   return (
     <section className="font-main bg-white py-10">
       <header className="bg-mentalHealth text-white w-full lg:h-[560px] py-10">
         <div className="max-w-[86rem] mx-auto flex justify-between px-4">
           <article className="space-y-5 flex flex-col items-center lg:items-start">
             <h2 className="w-full text-center lg:text-left text-[44px] lg:text-[88px] leading-[90px] max-w-[280px]">
-              mental health
+              {t('mental health')}
             </h2>
-            <p className="text-center font-body text-lg max-w-lg py-3">
-              In today’s fast-paced, modern and ultra-connected world, your mental health is more important than ever. With MDHUB, you can easily get mental health support when you need it most.
+            <p className="text-center lg:text-left font-body text-lg max-w-lg py-3">
+              {t('in-todays-fast-paced-modern-and-ultra-connected-world-your-mental-health-is-more-important-than-ever-with-mdhub-you-can-easily-get-mental-health-support-when-you-need-it-most')}
             </p>
             <Link to="/register">
               <button className="rounded-full w-[240px] text-lg flex justify-center py-2 border bg-white text-mentalHealth border-white hover:opacity-80 transition-opacity ease-in-out duration-300">
-                Sign Up Today
+                {t('sign-up-today')}
               </button>
             </Link>
           </article>
@@ -36,14 +38,14 @@ const ServicesMentalHealth = () => {
       <section className="bg-white py-8 lg:py-14">
         <div className="hidden relative virtual-health text-center lg:flex justify-center flip-words">
           <h2 className="text-primary text-center text-5xl md:text-6xl">
-            Get care today for
+            {t('get-help-for')}
           </h2>
           <div className="w-full absolute left-0 top-16">
             <div className="w-full h-16 overflow-y-hidden flex justify-center">
               {
                 WORDS.map(word => (
                   <span className="md:text-6xl">
-                    {word}
+                    {t(`${word}`)}
                   </span>
                 ))
               }
@@ -51,20 +53,20 @@ const ServicesMentalHealth = () => {
           </div>
         </div>
         <h3 className="text-center text-4xl md:text-5xl lg:pt-32 py-10 lg:py-20">
-          MDHUB members can benefit from:
+          {t('mdhub-members-can-benefit-from')}
         </h3>
         <div className="w-full max-w-4xl mx-auto flex flex-col lg:flex-row item-center justify-between text-center">
           <article className="space-y-6 flex flex-col items-center">
             <img src={MentalHealthImg} alt="mental health" />
-            <p className="font-body">Mental health frontline</p>
+            <p className="font-body">{t('mental-health-frontline')}</p>
           </article>
           <article className="space-y-6 pt-6 flex flex-col items-center">
             <img src={HearBrainImage} alt="heart and brain" />
-            <p className="md:pt-2 font-body">Mental health specialists</p>
+            <p className="md:pt-2 font-body">{t('mental-health-specialists')}</p>
           </article>
           <article className="space-y-6 flex flex-col items-center md:-mt-3">
             <img src={MeditationImage} alt="meditation" />
-            <p className="max-w-[180px] font-body">Headspace wellness & meditation app.</p>
+            <p className="max-w-[180px] font-body">{t('headspace-wellness-and-meditation-app')}</p>
           </article>
         </div>
       </section>
@@ -74,43 +76,40 @@ const ServicesMentalHealth = () => {
           <img src={HeadspaceLogo} alt="Headspace" />
         </div>
         <div className="w-full max-w-[530px] space-y-4">
-          <h2 className="text-4xl">What is Headspace?</h2>
+          <h2 className="text-4xl">{t('what-is-headspace')}?</h2>
           <p className="font-body">
-            Headspace is an app that teaches you how to meditate.
-            Meditation has been shown to help people stress less, focus more and sleep better. Headspace is meditation made simple, teaching you life-changing mindfulness skills in just a few minutes a day.
+            {t('headspace-is-an-app-that-teaches-you-how-to-meditate-meditation-has-been-shown-to-help-people-stress-less-focus-more-and-sleep-better-headspace-is-meditation-made-simple-teaching-you-life-changing-mindfulness-skills-in-just-a-few-minutes-a-day')}
           </p>
         </div>
       </section>
       <section className="py-10 lg:py-20">
-        <h2 className="text-4xl text-center pb-16">How it works</h2>
+        <h2 className="text-4xl text-center pb-16">{t('how-it-works')}</h2>
         <div className="w-full max-w-6xl mx-auto flex flex-col lg:flex-row gap-y-5 justify-between">
           <article className="w-full max-w-[464px] shadow-glow py-5 px-8">
-            <h2 className="text-3xl">See a mental health physician</h2>
+            <h2 className="text-3xl">{t('see-a-mental-health-physician')}</h2>
             <p className="font-body py-7">
-              Log into your MDHUB account and select “Mental Health Physician” from the menu of specialists.
-              Next, choose a date and time that works for you.
-              Quickly speak with a mental health physician for an initial assessment or follow-up. 30-minute initial appointments are $315, and 30-minute follow-up appointments are $230.
+              {t('log-into-your-mdhub-account-and-select-mental-health-physician-from-the-menu-of-specialists-next-choose-a-date-and-time-that-works-for-you-quickly-speak-with-a-mental-health-physician-for-an-initial-assessment-or-follow-up-30-minute-initial-appointments-are-315-and-30-minute-follow-up-appointments-are-230')}
             </p>
             <Link to="/register">
               <button
                 type="button"
                 className={`rounded-full w-56 font-main group hover:ring-1 hover:ring-primary py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent px-4`}
               >
-                Sign Up Today
+                {t('sign-up-today')}
               </button>
             </Link>
           </article>
           <article className="w-full max-w-[464px] shadow-glow py-5 px-8">
-            <h2 className="text-3xl">See a mental health therapist</h2>
+            <h2 className="text-3xl">{t('see-a-mental-health-therapist')}</h2>
             <p className="font-body py-7">
-              See a mental health therapist, with no appointment necessary. All you need to do is choose the topic you’d like to discuss with a therapist and book an appointment. Sessions are 50 minutes in length. The fee for a session ranges from $120-$160, depending on your region. If you have insurance, this fee is often covered by health benefits.
+              {t('see-a-mental-health-therapist-with-no-appointment-necessary-all-you-need-to-do-is-choose-the-topic-youd-like-to-discuss-with-a-therapist-and-book-an-appointment-sessions-are-50-minutes-in-length-the-fee-for-a-session-ranges-from-120-160-depending-on-your-region-if-you-have-insurance-this-fee-is-often-covered-by-health-benefits')}
             </p>
             <Link to="/register">
               <button
                 type="button"
                 className={`rounded-full w-56 font-main group hover:ring-1 hover:ring-primary py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent px-4`}
               >
-                Sign Up Today
+                {t('sign-up-today')}
               </button>
             </Link>
           </article>
