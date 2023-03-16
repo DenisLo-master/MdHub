7be78bef4a0ad1e0edf42c40847a266566bd59e0
asfand687 <asfandyar687@gmail.com>
2023-03-17 00:14:33 +0500

    added translations to home membership benefits

--- src/sections/Home/HomeMembershipBenefitsSection.jsx
+++ src/sections/Home/HomeMembershipBenefitsSection.jsx
@@ -4,9 +4,11 @@ import {
   OkIconPrimary,
   Benefit
 } from '../../assets'
+import { useTranslation } from 'react-i18next'
 
 
 const HomeMembershipBenefitsSection = () => {
+  const { t, i18n } = useTranslation()
   return (
     <section className="relative w-full font-main bg-white py-8 lg:py-20">
       <div className="w-full flex flex-col-reverse lg:flex-row justify-between">
@@ -16,10 +18,10 @@ const HomeMembershipBenefitsSection = () => {
             <article className="px-4 md:px-0 w-full">
               <div className="space-y-2">
                 <h2 className="w-full font-main text-4xl md:text-5xl">
-                  Membership Benefits
+                  {t('Membership Benefits')}
                 </h2>
                 <h3 className='w-full font-body text-base pb-8'>
-                  Your health, all in one place.
+                  {t('Your health, all in one place.')}
                 </h3>
               </div>
             </article>
@@ -28,49 +30,49 @@ const HomeMembershipBenefitsSection = () => {
                 <div>
                   <OkIconPrimary className="w-7 h-7" />
                 </div>
-                <p>Free unlimited access to doctors 24/7</p>
+                <p>{t('Free unlimited access to doctors 24/7')}</p>
               </li>
 
               <li className='flex items-center gap-x-3'>
                 <div>
                   <OkIconPrimary className="w-7 h-7" />
                 </div>
-                <p>Free online pharmacy and delivery</p>
+                <p>{t('Free online pharmacy and delivery')}</p>
               </li>
 
               <li className='flex items-center gap-x-3'>
                 <div>
                   <OkIconPrimary className="w-7 h-7" />
                 </div>
-                <p>Free Headspace membership</p>
+                <p>{t('Free Headspace membership')}</p>
               </li>
 
               <li className='flex items-center gap-x-3'>
                 <div>
                   <OkIconPrimary className="w-7 h-7" />
                 </div>
-                <p>Access to private labs with a 25% discount</p>
+                <p>{t('Access to private labs with a 25% discount')}</p>
               </li>
 
               <li className='flex items-center gap-x-3'>
                 <div>
                   <OkIconPrimary className="w-7 h-7" />
                 </div>
-                <p>Mobile nursing and homecare with a 25% discount</p>
+                <p>{t('Mobile nursing and homecare with a 25% discount')}</p>
               </li>
 
               <li className='flex items-center gap-x-3'>
                 <div>
                   <OkIconPrimary className="w-7 h-7" />
                 </div>
-                <p>Virus testing with a 25% discount + free rush service</p>
+                <p>{t('Virus testing with a 25% discount + free rush service')}</p>
               </li>
 
               <li className='flex items-center gap-x-3'>
                 <div>
                   <OkIconPrimary className="w-7 h-7" />
                 </div>
-                <p>Access to specialists and mental health professionals with a 15% discount</p>
+                <p>{t('Access to specialists and mental health professionals with a 15% discount')}</p>
               </li>
               <li>
                 <Link className="block" to="/rates">
@@ -78,7 +80,7 @@ const HomeMembershipBenefitsSection = () => {
                     type="button"
                     className={`rounded-full w-48 font-main group hover:ring-1 hover:ring-primary py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent px-4`}
                   >
-                    Learn More
+                    {t('Learn More')}
                   </button>
                 </Link>
               </li>
