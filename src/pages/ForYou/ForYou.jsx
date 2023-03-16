7598206614b38427be50010c3a69fab65d79919f
asfand687 <asfandyar687@gmail.com>
2023-03-17 02:00:27 +0500

    added translations to for you

--- src/pages/ForYou/ForYou.jsx
+++ src/pages/ForYou/ForYou.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import { ForYouHeaderSection } from '../../sections'
 import { Link } from 'react-router-dom'
 import { ForYouFamilyPlan, OkIconWhite, ForYouBottom } from '../../assets'
+import { useTranslation } from 'react-i18next' 
 
 const ForYou = () => {
+  const { t } = useTranslation()
   return (
     <section>
       <ForYouHeaderSection />
       <div className="py-10 text-primary font-main px-4">
         <h2 className="flex flex-col lg:flex-row text-center justify-center gap-x-3 items-center text-5xl md:text-6xl">
-          Get help with
+          {t('Get help with')}
           <span className="text-dark">
-            seasonal depression
+            {t('seasonal depression')}
           </span>
         </h2>
       </div>
@@ -22,43 +24,43 @@ const ForYou = () => {
             <li className="flex w-[380px] gap-x-2">
               <OkIconWhite className="w-6 h-6" />
               <p className="w-full">
-                Instant access to a doctor for free, anytime, from anywhere
+                {t('instant-access-to-a-doctor-for-free-anytime-from-anywhere')}
               </p>
             </li>
             <li className="flex w-[380px] gap-x-2">
               <OkIconWhite className="w-6 h-6" />
               <p className="w-full">
-                Most of your medical needs in one package
+                {t('most-of-your-medical-needs-in-one-package')}
               </p>
             </li>
             <li className="flex w-[380px] gap-x-2">
               <OkIconWhite className="w-6 h-6" />
               <p className="w-full">
-                Prescriptions, lab work, specialist, vaccines and mental health in one place
+                {t('prescriptions-lab-work-specialist-vaccines-and-mental-health-in-one-place')}
               </p>
             </li>
             <li className="flex w-[380px] gap-x-2">
               <OkIconWhite className="w-6 h-6" />
               <p className="w-full">
-                No travel, traffic or parking or wait time required
+                {t('no-travel-traffic-or-parking-or-wait-time-required')}
               </p>
             </li>
             <li className="flex w-[380px] gap-x-2">
               <OkIconWhite className="w-6 h-6" />
               <p className="w-full">
-                Control your medical outlook, be proactive and solve health issues
+                {t('control-your-medical-outlook-be-proactive-and-solve-health-issues')}
               </p>
             </li>
             <li className="flex w-[380px] gap-x-2">
               <OkIconWhite className="w-6 h-6" />
               <p className="w-full">
-                Coverage for your whole family
+                {t('coverage-for-your-whole-family')}
               </p>
             </li>
             <li className="absolute -bottom-20 left-4 lg:left-36">
               <Link className="block" to="/register">
                 <button className="rounded-full w-[240px] text-lg flex justify-center py-3 border border-white">
-                  Sign Up Today
+                  {t('sign-up-today')}
                 </button>
               </Link>
             </li>
@@ -68,17 +70,17 @@ const ForYou = () => {
         <article className="flex flex-col-reverse lg:flex-row gap-x-8 py-16 text-dark">
           <div className="flex-1 space-y-4 font-body px-4">
             <h2 className="text-4xl lg:text-5xl font-main max-w-[500px] py-4">
-              The whole family in one affordable plan
+              {t('the-whole-family-in-one-affordable-plan')}
             </h2>
             <p>
-              A hand-picked network of online doctors in Canada ready to help you.
+              {t('a-hand-picked-network-of-online-doctors-in-canada-ready-to-help-you')}
             </p>
             <p className="pb-2">
-              Looking for a doctor who’s accepting consultations from new patients? Our physicians, nurse practitioners, and allied healthcare professionals are licensed and practice in their respective fields right here in Canada. See a healthcare provider in minutes or book an online doctor’s appointment with a specialist.
+              {t('looking-for-a-doctor-whos-accepting-consultations-from-new-patients-our-physicians-nurse-practitioners-and-allied-healthcare-professionals-are-licensed-and-practice-in-their-respective-fields-right-here-in-canada-see-a-healthcare-provider-in-minutes-or-book-an-online-doctors-appointment-with-a-specialist')}
             </p>
             <Link className="block" to="/register">
               <button className="rounded-full w-[240px] text-lg flex justify-center py-2 border border-primary">
-                Sign Up Today
+                {t('sign-up-today')}
               </button>
             </Link>
           </div>
@@ -93,9 +95,9 @@ const ForYou = () => {
         <div className="relative z-10 w-full max-w-[1160px] text-3xl mx-auto h-[400px] object-cover flex items-center text-white px-4">
           <div className="space-y-6">
             <h2 className="max-w-[450px] pb-">
-              “Just registered for a membership, chose a doctor & made a same-day appointment at faster than I buy my morning coffee.”
+              “{t('just-registered-for-a-membership-chose-a-doctor-and-made-a-same-day-appointment-at-faster-than-i-buy-my-morning-coffee')}”
             </h2>
-            <h2>— MDHUB Member in Montreal</h2>
+            <h2>— {t('mdhub-member-in-montreal')}</h2>
           </div>
         </div>
       </section>
