5d2a9835712b1f308fe021420fc20adb8613e8b1
asfand687 <asfandyar687@gmail.com>
2023-03-17 00:37:20 +0500

    added translation to how it works

--- src/sections/Home/HomeHowItWorksSection.jsx
+++ src/sections/Home/HomeHowItWorksSection.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Slider from '../../components/Slider'
+import { useTranslation } from 'react-i18next'
 
 const HomeHowItWorksSection = () => {
+  const { t } = useTranslation()
   return (
     <section>
       <article className="hidden lg:block w-full max-w-[90rem] mx-auto pt-10">
-        <h2 className="text-4xl font-main text-center">How It Works</h2>
+        <h2 className="text-4xl font-main text-center">{t('How It Works')}</h2>
         <Slider />
       </article>
     </section>
