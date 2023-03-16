e7f6de4f9d675cf9de427ff0640fa05ca214cb3f
asfand687 <asfandyar687@gmail.com>
2023-03-17 03:04:20 +0500

    added translations to services header

--- src/sections/Services/ServicesHeaderSection.jsx
+++ src/sections/Services/ServicesHeaderSection.jsx
@@ -9,29 +9,31 @@ import {
   MentalHealthIcon,
   StethoscopeIconWhite
 } from '../../assets'
+import { useTranslation } from 'react-i18next'
 
 
 const ServicesHeaderSection = () => {
+  const { t } = useTranslation()
   const location = useLocation()
   return (
     <section className="mb-10">
-      <h2 className="font-main py-10 text-4xl text-center">Services</h2>
+      <h2 className="font-main py-10 text-4xl text-center">{t('Services')}</h2>
       <div className="w-full max-w-[1100px] mx-auto rounded-b-full lg:rounded-full shadow-xl flex flex-col lg:flex-row justify-center items-center pb-24 lg:pb-3 py-3 gap-x-6 text-[#3F3F3F] font-main">
         <Link to="/services">
-          <ServicesCard title={"virtual doctor"} bgPrimary={location.pathname === "/services"}>
+          <ServicesCard title={t('virtual-doctor')} bgPrimary={location.pathname === "/services"}>
             <VirtualDoctorIcon className={`${location.pathname === "/services" ? "text-white" : "text-primary"}`} />
           </ServicesCard>
         </Link>
 
         <Link to="/services/mental-health">
-          <ServicesCard title={"mental health"} bgPrimary={location.pathname === "/services/mental-health"}>
+          <ServicesCard title={t('mental health')} bgPrimary={location.pathname === "/services/mental-health"}>
             <MentalHealthIcon className={`${location.pathname === "/services/mental-health" ? "text-white" : "text-primary"}`} />
           </ServicesCard>
         </Link>
 
         <Link to="/services/online-pharmacy">
           <ServicesCard
-            title={"online pharmacy"}
+            title={t('online-pharmacy')}
             bgPrimary={location.pathname === "/services/online-pharmacy"}
           >
             <OnlinePharmacyIcon
@@ -41,7 +43,7 @@ const ServicesHeaderSection = () => {
         </Link>
 
         <Link to="/services/diagnostics">
-          <ServicesCard title={"diagnostics"} bgPrimary={location.pathname === "/services/diagnostics"}>
+          <ServicesCard title={t('diagnostics')} bgPrimary={location.pathname === "/services/diagnostics"}>
             {
               location.pathname === "/services/diagnostics" ?
                 <StethoscopeIconWhite /> : <StethoscopeIcon />
@@ -50,7 +52,7 @@ const ServicesHeaderSection = () => {
         </Link>
 
         <Link to="/services/nursing-homecare">
-          <ServicesCard title={"nursing & homecare"} bgPrimary={location.pathname === "/services/nursing-homecare"}>
+          <ServicesCard title={t('nursing-and-homecare')} bgPrimary={location.pathname === "/services/nursing-homecare"}>
             <NursingHomecareIcon
              className={`${location.pathname === "/services/nursing-homecare" ? "text-white" : "text-primary"}`}
             />
