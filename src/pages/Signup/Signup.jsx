b9e26f9555625f0ffc61abac3e4479f8e0296e4b
asfand687 <asfandyar687@gmail.com>
2023-03-17 15:05:09 +0500

    added translations to signup

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { registerationStore } from '../../store/registerationStore'
 import {
   SignupAccountsTypeNav,
   RegistrationCheckoutForm
 }
   from '../../sections'
-
 import { loadStripe } from "@stripe/stripe-js"
-import { Elements } from '@stripe/react-stripe-js';
+import { Elements } from '@stripe/react-stripe-js'
 const key = import.meta.env.VITE_STRIPE
-const stripePromise = loadStripe(key);
+const stripePromise = loadStripe(key)
+import { useTranslation } from 'react-i18next'
 
 const Signup = () => {
   const navigate = useNavigate()
+  const { t } = useTranslation()
 
   useEffect(() => {
     const token = localStorage.getItem("jwtToken")
@@ -23,11 +23,12 @@ const Signup = () => {
   return (
     <section className="w-full max-w-[42rem] flex flex-col items-center mx-auto py-8 md:py-20 font-main">
       <article className="w-full space-y-4 flex flex-col items-center">
-        <h2 className="text-4xl text-center">Choose Account Type</h2>
+        <h2 className="text-4xl text-center">{t('choose-account-type')}</h2>
         <div className="text-center space-y-1 font-body pb-4">
-          <h2>We make it easier to get the care you need for yourself, your family or your corporation.
+          <h2>
+            {t('we-make-it-easier-to-get-the-care-you-need-for-yourself-your-family-or-your-corporation')}
           </h2>
-          <h2>Memberships are now as low as $19.99 per month!</h2>
+          <h2>{t('memberships-are-now-as-low-as-19-99-per-month')}</h2>
         </div>
         <SignupAccountsTypeNav />
       </article>
