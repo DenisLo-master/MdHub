d22eb96aadd90669d87bcbc10a3cbcc4a257e920
asfand687 <asfandyar687@gmail.com>
2023-03-18 01:58:05 +0500

    made about responsive

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -11,45 +11,69 @@ const About = () => {
           <img src={AboutHeroImage} alt="About Us" />
           <div className="absolute bottom-16 left-32 space-y-4 max-w-[1000px]">
             <h2 className="text-4xl">{t('About Us')}</h2>
-            <p className="font-body">
+            <p className="hidden lg:block font-body">
               {t('were-a-group-of-passionate-individuals-that-believe-canadians-deserve-more-when-it-comes-to-their-healthcare-thats-why-we-created-the-ultimate-healthcare-solution-to-put-your-health-back-in-your-hands-while-virtual-healthcare-isnt-new-bridging-the-gap-between-virtual-and-physical-care-is-and-were-proud-to-be-a-disruptor-in-the-virtual-healthcare-industry-our-team-of-doctors-and-nurse-practitioners-is-here-to-help-when-you-need-them-so-you-your-family-and-team-can-access-quality-healthcare-fast-because-a-healthy-life-is-a-happy-life')}
             </p>
           </div>
         </div>
 
-        <div className="flex flex-row-reverse items-center justify-center  py-16">
-          <article className="py-10 pl-16 rounded-lg">
+        <div className="flex flex-col lg:flex-row-reverse items-center justify-center  py-16">
+          <article className="py-10 px-4 lg:pl-16 rounded-lg">
             <h2 className="text-6xl text-dark text-center font-main pb-4">{t('them')}</h2>
             <ul className="font-body space-y-6 font-light text-lg">
               <li className="flex items-center gap-x-5">
-                <CloseVector /> <span>{t('24-7-access-to-real-doctors')} </span>
+                <div>
+                  <CloseVector />
+                </div>
+                <span>{t('24-7-access-to-real-doctors')} </span>
               </li>
               <li className="flex items-center gap-x-5">
-                <CloseVector /> <span>{t('prescription-by-doctors')}</span>
+                <div>
+                  <CloseVector />
+                </div>
+                <span>{t('prescription-by-doctors')}</span>
               </li>
               <li className="flex items-center gap-x-5">
-                <CloseVector /> <span>{t('free-online-pharmacy')}</span>
+                <div>
+                  <CloseVector />
+                </div>
+                <span>{t('free-online-pharmacy')}</span>
               </li>
               <li className="flex items-center gap-x-5">
-                <CloseVector /> <span>{t('individual-family-or-corporate-coverage')}</span>
+                <div>
+                  <CloseVector />
+                </div>
+                <span>{t('individual-family-or-corporate-coverage')}</span>
               </li>
               <li className="flex items-center gap-x-5">
-                <CloseVector /> <span>{t('quick-and-discounted-access-to-medical-specialists')}</span>
+                <div>
+                  <CloseVector />
+                </div>
+                <span>{t('quick-and-discounted-access-to-medical-specialists')}</span>
               </li>
               <li className="flex items-center gap-x-5">
-                <CloseVector /> <span>{t('worldwide-access-to-telemedicine')}</span>
+                <div>
+                  <CloseVector />
+                </div>
+                <span>{t('worldwide-access-to-telemedicine')}</span>
               </li>
               <li className="flex items-center gap-x-5">
-                <CloseVector /> <span>{t('headspace-app')}</span>
+                <div>
+                  <CloseVector />
+                </div>
+                <span>{t('headspace-app')}</span>
               </li>
               <li className="flex items-center gap-x-5">
-                <CloseVector /> <span>{t('centralized-health-records-and-platform')}</span>
+                <div>
+                  <CloseVector />
+                </div>
+                <span>{t('centralized-health-records-and-platform')}</span>
               </li>
             </ul>
           </article>
-          <article className="bg-primary text-white py-10 px-20 rounded-lg bg-linearGradientPrimary shadow-cardService">
+          <article className="bg-primary text-white py-10 px-4 lg:px-20 rounded-lg bg-linearGradientPrimary shadow-cardService">
             <h2 className="text-6xl text-center font-main pb-4">{t('us')}</h2>
-            <ul className="font-body grid grid-cols-2 gap-x-14 gap-y-2 max-w-[550px] space-y-4 font-light text-lg">
+            <ul className="font-body grid grid-cols-1 lg:grid-cols-2 gap-x-14 gap-y-2 max-w-[550px] space-y-4 font-light text-lg">
               <li className="flex items-center gap-x-5">
                 <div>
                   <OkIcon className="w-10 h-10" />
