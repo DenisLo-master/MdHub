eba1191170ecf88babdd8f06b73aff3ffc177aa7
asfand687 <asfandyar687@gmail.com>
2023-03-17 18:35:57 +0500

    added translations to user info

--- src/sections/Dashboard/DashboardUserInfo.jsx
+++ src/sections/Dashboard/DashboardUserInfo.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { TiPencil } from 'react-icons/ti'
 import { Link } from 'react-router-dom'
 import { registerationStore } from '../../store/registerationStore'
+import { useTranslation } from 'react-i18next'
 
 const DashboardUserInfo = () => {
   const userInfo = registerationStore(state => state.userInfo)
+  const { t } = useTranslation()
   return (
     <section className="px-4 flex flex-col md:flex-row gap-y-4 text-center md:text-left gap-x-16 font-body font-light py-10">
 
@@ -13,11 +15,11 @@ const DashboardUserInfo = () => {
           <Link to="/dashboard/update-user">
             <button className="flex items-center gap-x-2">
               <TiPencil className="text-3xl" />
-              <h3 className="hidden lg:block">Edit Profile</h3>
+              <h3 className="hidden lg:block">{t('edit-profile')}</h3>
             </button>
           </Link>
         </div>
-        <h2 className="text-4xl font-main font-normal pb-4">Your Information</h2>
+        <h2 className="text-4xl font-main font-normal pb-4">{t('your-information')}</h2>
         <div className="p-10 rounded-[35px] shadow-cardService space-y-6  border-[0.25px] lg:h-[428px]">
           <article className="bg-white rounded-[48px] shadow-cardService px-8 py-3 border-[0.25px]">
             {userInfo?.firstName}
@@ -38,11 +40,11 @@ const DashboardUserInfo = () => {
       </article>
 
       <article className="flex-1">
-        <h2 className="text-4xl font-main font-normal pb-4">Your Subscription</h2>
+        <h2 className="text-4xl font-main font-normal pb-4">{t('your-subscription')}</h2>
         <div className="p-10 flex flex-col justify-between rounded-[35px] shadow-cardService space-y-6 border-[0.25px] lg:h-[428px]">
           <article className="space-y-10">
             <div>
-              <p className="text-sm">Your current subscription plan</p>
+              <p className="text-sm">{t('your-current-subscription-plan')}</p>
               <h2 className="font-bold text-2xl tracking-widest">{`${userInfo?.accountType} ${userInfo?.paymentMode}`}</h2>
             </div>
             <div>
@@ -59,13 +61,13 @@ const DashboardUserInfo = () => {
                 }
                 /month
               </h2>
-              <p className="text-sm">Unlimited usage</p>
+              <p className="text-sm">{t('unlimited-usage')}</p>
             </div>
           </article>
           <article>
             <Link to="/dashboard/update-billing">
               <button type="button" className={`rounded-full font-main text-lg group hover:ring-1 hover:ring-primary px-12 py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
-                Manage
+                {t('manage')}
               </button>
             </Link>
           </article>
