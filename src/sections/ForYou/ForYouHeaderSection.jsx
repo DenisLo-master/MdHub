75152250f96650a52f4785a4811aa9e9db01db63
asfand687 <asfandyar687@gmail.com>
2023-03-17 18:36:20 +0500

    added translations to for you header

--- src/sections/ForYou/ForYouHeaderSection.jsx
+++ src/sections/ForYou/ForYouHeaderSection.jsx
@@ -7,7 +7,7 @@ const ForYouHeaderSection = () => {
   return (
     <section className="relative bg-[#DB8E96] font-main">
       <img className="hidden lg:block absolute  object-cover w-full h-[400px]" src={ForYouHeaderBg} alt="For You" />
-      <div className="relative z-10 w-full max-w-7xl mx-auto h-[390px] flex items-center">
+      <div className="relative z-10 w-full max-w-7xl mx-auto h-[390px] flex items-center px-4 text-center lg:text-left">
         <h2 className="text-[70px] lg:text-[88px] max-w-[450px] leading-[80px] pb-5 text-white">
           {t('healthcare-without-the-headaches')}
         </h2>
