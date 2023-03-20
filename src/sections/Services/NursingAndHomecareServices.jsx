9b41d33f20626e2d2f5112d46a4c61e30a99eab5
asfand687 <asfandyar687@gmail.com>
2023-03-20 21:42:10 +0500

    added get care now

--- src/sections/Services/NursingAndHomecareServices.jsx
+++ src/sections/Services/NursingAndHomecareServices.jsx
@@ -115,7 +115,7 @@ const NursingAndHomecareServices = () => {
             </Link>
             <Link className="block w-full" to="/dashboard">
               <button className="rounded-full w-full text-lg flex justify-center py-3 text-white bg-transparent border border-white hover:bg-white hover:text-primary transition-colors duration-300 ease-out">
-                {t('See A Doctor')}
+                {t('get-care-now')}
               </button>
             </Link>
           </article>
