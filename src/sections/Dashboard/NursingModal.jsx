1dd1cf180e9a006e0008db4874985abbdcec2364
asfand687 <asfandyar687@gmail.com>
2023-03-23 21:24:27 +0500

    added new translations

--- src/sections/Dashboard/NursingModal.jsx
+++ src/sections/Dashboard/NursingModal.jsx
@@ -5,18 +5,20 @@ import { loadStripe } from '@stripe/stripe-js'
 import { NursingAppointmentForm } from '../../sections'
 import { Elements } from '@stripe/react-stripe-js'
 const key = import.meta.env.VITE_STRIPE
-const stripePromise = loadStripe(key);
+const stripePromise = loadStripe(key)
+import { useTranslation } from 'react-i18next'
 
 
 
 const NursingModal = () => {
   const showNursingModal = registerationStore(state => state.showNursingModal)
   const setShowNursingModal = registerationStore(state => state.setShowNursingModal)
+  const { t } = useTranslation()
 
   return (
     <Modal
       key="nursing and homecare services"
-      title="nursing and homecare services"
+      title={t('nursing-and-homecare-services')}
       showModal={showNursingModal}
       setShowModal={setShowNursingModal}
     >
