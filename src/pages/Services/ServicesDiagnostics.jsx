4c8343ec418adfa42d7a2484ca9ff21b0ed0099e
asfand687 <asfandyar687@gmail.com>
2023-03-23 20:11:59 +0500

    updated translations

--- src/pages/Services/ServicesDiagnostics.jsx
+++ src/pages/Services/ServicesDiagnostics.jsx
@@ -19,11 +19,10 @@ const ServicesDiagnostics = () => {
             </h2>
             <div className="space-y-5 font-body text-lg max-w-lg pb-8 leading-5 px-2">
               <p>
-                {t('avoid-the-wait-at-hospitals-and-clinics-with-safe-fast-and-professional-lab-testing-from-the-comfort-of-your-home-or-office')}
-                <strong>{" "}{t('get-results-fast')}</strong>
+                {t('get-safe-fast-and-professional-lab-testing-choose-from-service-at-our-multiple-labs-across-canada-or-have-our-medical-professionals-conduct-your-testing-the-comfort-of-your-home-or-office')} 
               </p>
               <p>
-                {t('our-certified-partner-labs-can-process-over-300-different-screenings-to-provide-you-with-a-comprehensive-health-assessment-and-get-you-answers-fast')}
+                {t('our-certified-partner-labs-can-process-over-300-different-screenings-for-medical-conditions-like-diabetes-and-heart-disease')}
               </p>
             </div>
             <Link className="flex lg:block justify-center px-2" to="register">
@@ -57,43 +56,50 @@ const ServicesDiagnostics = () => {
       </section>
       <section className="bg-white py-10 pb-24">
         <h2 className="text-4xl pb-10 text-center">
-          How It Works
+          {t('How It Works')}
         </h2>
         <section className="w-full max-w-6xl space-y-6 mx-auto">
           <div className="flex flex-col md:flex-row justify-between">
             {
-              diagnosisHowItWorksData.map((step => (
-                <article key={step.id} className="flex flex-col justify-between items-center">
-                  <div className="flex flex-col gap-x-8">
-                    <article className="pb-2">
-                      <div className="w-14 h-14 text-3xl font-body font-semibold bg-gradient-primary text-white rounded-full flex justify-center items-center">
-                        {step.id}
-                      </div>
-                    </article>
-                    <article className="w-full max-w-[220px] space-y-5 border-b border-dark pb-4 mb-4">
-                      <p className="leading-[1.2em] text-2xl">
-                        {t(`${step.text}`)}
-                        {step.id === 3 && (<span><strong>{" "}{t('here')}</strong>{")"}</span>)}
-                      </p>
-                      {
-                        step.id === 1 && (
-                          <strong className="font-body block">
-                            {t('see-a-doctor-now')}
-                          </strong>
-                        )
-                      }
-                      {
-                        step.id === 3 && (
+              diagnosisHowItWorksData.map((step => {
+                return (
+                  <article key={step.id} className="flex flex-col justify-between items-center">
+                    <div className="flex flex-col gap-x-8">
+                      <article className="pb-2">
+                        <div className="w-14 h-14 text-3xl font-body font-semibold bg-gradient-primary text-white rounded-full flex justify-center items-center">
+                          {step.id}
+                        </div>
+                      </article>
+                      <article className="w-full max-w-[220px] space-y-5 border-b border-dark pb-4 mb-4">
+                        <p className="leading-[1.2em] text-2xl">
+                          {t(`${step.text}`)}
+                        </p>
+                        {step.id === 1 && (
+                          <Link to="/dashboard">
+                            <strong className="font-body block mt-2">
+                              {t('see-a-doctor-now')}
+                            </strong>
+                          </Link>
+                        )}
+                        {step.id === 2 && (
                           <p className="font-light text-sm font-body text-dark">
                             {t('please-note-member-pricing-is-99-for-this-service')}
                           </p>
-                        )
-                      }
-                    </article>
-                  </div>
-                  <img className="w-full max-w-[224px] pt-4" src={step.image} alt={step.text} />
-                </article>
-              )))
+                        )}
+                        {step.id === 4 && (
+                          <p className="font-light text-sm font-body text-dark">
+                            {t('access-your-dashboard')} {" "}
+                            <strong>
+                              <Link className="font-semibold" to="/dashboard">{t('here')}</Link>
+                            </strong>
+                          </p>
+                        )}
+                      </article>
+                    </div>
+                    <img className="w-full max-w-[224px] pt-4" src={step.image} alt={step.text} />
+                  </article>
+                )
+              }))
             }
           </div>
         </section>
