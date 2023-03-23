7c22e50e5eed734715dad775d3d83602148e3b34
asfand687 <asfandyar687@gmail.com>
2023-03-23 21:24:21 +0500

    added new translations

--- src/sections/Dashboard/NursingAppointmentStepTwo.jsx
+++ src/sections/Dashboard/NursingAppointmentStepTwo.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import { BiCaretDown } from 'react-icons/bi'
 import DatePicker from 'react-date-picker'
 import 'react-date-picker/dist/entry.nostyle'
+import { useTranslation } from 'react-i18next'
 
 const NursingAppointmentStepTwo = ({ time, selectedDate, updateFields, address, city, province, postalCode }) => {
+  const { t } = useTranslation()
   const generateOptions = () => {
     const options = [];
 
@@ -24,14 +26,14 @@ const NursingAppointmentStepTwo = ({ time, selectedDate, updateFields, address, 
   };
   return (
     <div className="space-y-5">
-      <p className="text-xl pb-2">Please select date and time.</p>
+      <p className="text-xl pb-2">{t('please-select-date-and-time')}</p>
       <div className="w-full flex">
         <DatePicker
           className="flex-1 bg-transparent appearance-none px-4 py-2 border rounded-lg outline-none focus:ring ring-primary"
           value={selectedDate}
           onChange={(date) => updateFields({ selectedDate: date })}
           format="dd-MM-y"
-          placeholderText="Select a date"
+          placeholderText={t('select-a-date')}
         />
       </div>
       <div className="w-full flex relative">
@@ -39,11 +41,11 @@ const NursingAppointmentStepTwo = ({ time, selectedDate, updateFields, address, 
         <select
           required
           className="flex-1 bg-transparent appearance-none px-4 py-2 border rounded-lg outline-none focus:ring ring-primary"
-          placeholder="select time"
+          placeholder={t('select-time')}
           value={time}
           onChange={(e) => updateFields({ time: e.target.value })}
         >
-          <option value="">Select Time</option>
+          <option value="">{t('select-time')}</option>
           {generateOptions()}
           <option value="19:00">19:00</option>
         </select>
@@ -54,7 +56,7 @@ const NursingAppointmentStepTwo = ({ time, selectedDate, updateFields, address, 
           onChange={(e) => updateFields({ province: e.target.value })}
           className="px-4 py-2 flex-1 border rounded-lg outline-none focus:ring ring-primary"
           required
-          placeholder="Province"
+          placeholder={t('province')}
           type="text"
         />
         <input
@@ -62,7 +64,7 @@ const NursingAppointmentStepTwo = ({ time, selectedDate, updateFields, address, 
           onChange={(e) => updateFields({ city: e.target.value })}
           className="px-4 py-2 flex-1 border rounded-lg outline-none focus:ring ring-primary"
           required
-          placeholder="City"
+          placeholder={t('city-0')}
           type="text"
         />
         <input
@@ -70,7 +72,7 @@ const NursingAppointmentStepTwo = ({ time, selectedDate, updateFields, address, 
           onChange={(e) => updateFields({ postalCode: e.target.value })}
           className="px-4 py-2 flex-1 border rounded-lg outline-none focus:ring ring-primary"
           required
-          placeholder="Postal Code"
+          placeholder={t('postal-code-0')}
           type="text"
         />
       </div>
@@ -80,7 +82,7 @@ const NursingAppointmentStepTwo = ({ time, selectedDate, updateFields, address, 
           onChange={(e) => updateFields({ address: e.target.value })}
           className="px-4 py-2 flex-1 border rounded-lg outline-none focus:ring ring-primary"
           required
-          placeholder="Address"
+          placeholder={t('address-1')}
           type="text"
         />
       </div>
