c41fbb709d32ae574d6dbca7803118118aa164f4
asfand687 <asfandyar687@gmail.com>
2023-03-23 21:24:15 +0500

    added new translations

--- src/sections/Dashboard/NursingAppointmentStepThree.jsx
+++ src/sections/Dashboard/NursingAppointmentStepThree.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import { CardElement } from '@stripe/react-stripe-js'
 import { registerationStore } from '../../store/registerationStore'
+import { useTranslation } from 'react-i18next'
 
 const NursingAppointmentStepThree = () => {
   const nursingAppointmentBill = registerationStore(state => state.nursingAppointmentBill)
+  const { t } = useTranslation()
   return (
     <div>
       <div className="flex justify-between pb-5">
         <article className="space-y-4">
-          <h2 className="text-2xl">Your Order</h2>
+          <h2 className="text-2xl">{t('your-order')}</h2>
           <h3 className="font-body">
-            1 hour minimum nursing fee: ${nursingAppointmentBill > 0 ? nursingAppointmentBill : 99}
+            {t('1-hour-minimum-nursing-fee')}: ${nursingAppointmentBill > 0 ? nursingAppointmentBill : 99}
           </h3>
         </article>
         <article className="space-y-4 text-right">
-          <h2 className="text-2xl">Subtotal</h2>
+          <h2 className="text-2xl">{t('subtotal')}</h2>
           <div className="font-body">
             <h3>${nursingAppointmentBill > 0 ? nursingAppointmentBill : 99}</h3>
-            <h3>Tax: ${nursingAppointmentBill > 0 ? (nursingAppointmentBill * 0.14975).toFixed(2) : (99 * 0.14975).toFixed(2)}</h3>
-            <h2 className="text-3xl">Total: <strong>${nursingAppointmentBill > 0 ? (nursingAppointmentBill + (nursingAppointmentBill * 0.14975)).toFixed(2) : 113.83}</strong></h2>
+            <h3>{t('tax-0')}: ${nursingAppointmentBill > 0 ? (nursingAppointmentBill * 0.14975).toFixed(2) : (99 * 0.14975).toFixed(2)}</h3>
+            <h2 className="text-3xl">{t('total-0')}: <strong>${nursingAppointmentBill > 0 ? (nursingAppointmentBill + (nursingAppointmentBill * 0.14975)).toFixed(2) : 113.83}</strong></h2>
           </div>
         </article>
+      </div>
+      <div className="w-full flex flex-wrap gap-y-4 gap-x-6 pb-4 font-body">
+        <input
+          className="flex-1 rounded-full text-lg focus:ring-1 focus:ring-primary outline-none px-8 py-1 border border-primary"
+          name="address"
+          type="text"
+          placeholder="Coupon Code"
+          required
+        />
       </div>
       <div>
         <CardElement
