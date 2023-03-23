b81101a20fdb5a8e0adc9bc152012c78d3044a25
asfand687 <asfandyar687@gmail.com>
2023-03-23 21:24:08 +0500

    added new translations

--- src/sections/Dashboard/NursingAppointmentStepOne.jsx
+++ src/sections/Dashboard/NursingAppointmentStepOne.jsx
@@ -1,38 +1,33 @@
 import React, { useState } from 'react'
 import Select from 'react-select'
-import { BiCaretDown } from 'react-icons/bi'
 import { nursingServices, homecareServices } from '../../constants'
 import { registerationStore } from '../../store/registerationStore'
 import LabRequisitionForm from '../../components/LabRequisitionForm'
+import { useTranslation } from 'react-i18next'
 
-const options = [
-  {
-    label: 'Nursing Services',
-    options: nursingServices.map(item => (
-      { value: item, label: item }
-    ))
-  },
-  {
-    label: 'Homecare Services',
-    options: homecareServices.map(item => (
-      { value: item, label: item }
-    ))
-  }
-]
-
-const nursingServicesData = nursingServices.map(item => (
-  { value: item, label: item }
-))
-
-const homecareServicesData = homecareServices.map(item => (
-  { value: item, label: item }
-))
 
 const NursingAppointmentStepOne = ({ customNursingService, updateFields }) => {
   const [reqNeeded, setReqNeeded] = useState(false)
   const setNursingAppointmentBill = registerationStore(state => state.setNursingAppointmentBill)
   const selectedNursingHomecareOptions = registerationStore(state => state.selectedNursingHomecareOptions)
   const setSelectedNursingHomecareOptions = registerationStore(state => state.setSelectedNursingHomecareOptions)
+  const { t } = useTranslation()
+
+  const options = [
+    {
+      label: t('nursing-services'),
+      options: nursingServices.map(item => (
+        { value: item, label: t(`${item}`) }
+      ))
+    },
+    {
+      label: t('homecare-services'),
+      options: homecareServices.map(item => (
+        { value: item, label: t(`${item}`) }
+      ))
+    }
+  ]
+
 
   const handleSelect = (selectedOption) => {
     setSelectedNursingHomecareOptions(selectedOption);
@@ -60,20 +55,20 @@ const NursingAppointmentStepOne = ({ customNursingService, updateFields }) => {
   return (
     <div>
       <div className="text-lg">
-        <p className="text-xl pb-4">Please select the services of your need.</p>
+        <p className="text-xl pb-4">{t('please-select-the-services-of-your-need')}</p>
         <div className="space-y-3">
           <Select
             value={selectedNursingHomecareOptions}
             onChange={handleSelect}
             options={options}
             isMulti
-            placeholder="Select services"
+            placeholder={t('select-services')}
           />
         </div>
         <div className="py-4">
           <textarea
             className="w-full h-28 p-4 outline-none focus:ring-2 ring-primary rounded-3xl border-2"
-            placeholder="Can’t find the service you are looking for? Please describe the service in the field."
+            placeholder={t('cant-find-the-service-you-are-looking-for-please-describe-the-service-in-the-field')}
             value={customNursingService}
             onChange={(e) => updateFields({ customNursingService: e.target.value })}
           />
@@ -88,7 +83,7 @@ const NursingAppointmentStepOne = ({ customNursingService, updateFields }) => {
             onChange={() => setReqNeeded(!reqNeeded)}
           />
           <label htmlFor="labReq">
-            OPTIONAL: Upload Requisition if needed
+            {t('optional-upload-requisition-if-needed')}
           </label>
         </div>
       </div>
