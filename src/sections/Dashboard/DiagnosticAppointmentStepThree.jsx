d7ba155461809b3e4034e98fa619944a150d8694
asfand687 <asfandyar687@gmail.com>
2023-03-23 21:23:45 +0500

    added new translations

--- src/sections/Dashboard/DiagnosticAppointmentStepThree.jsx
+++ src/sections/Dashboard/DiagnosticAppointmentStepThree.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import { CardElement } from '@stripe/react-stripe-js'
 import { registerationStore } from '../../store/registerationStore'
+import { useTranslation } from 'react-i18next'
 
 const DiagnosticAppointmentStepThree = () => {
   const diagnosticsAppointmentBill = registerationStore(state => state.diagnosticsAppointmentBill)
+  const { t } = useTranslation()
   return (
     <div>
       <div className="flex justify-between pb-5">
         <article className="space-y-4">
-          <h2 className="text-2xl">Your Order</h2>
+          <h2 className="text-2xl">{t('your-order')}</h2>
           <h3 className="font-body">
-            1 hour minimum nursing fee: ${diagnosticsAppointmentBill}
+            {t('1-hour-minimum-nursing-fee')}: ${diagnosticsAppointmentBill}
           </h3>
         </article>
         <article className="space-y-4 text-right">
-          <h2 className="text-2xl">Subtotal</h2>
+          <h2 className="text-2xl">{t('subtotal')}</h2>
           <div className="font-body">
             <h3>${diagnosticsAppointmentBill}</h3>
-            <h3>Tax: ${(diagnosticsAppointmentBill * 0.14975).toFixed(2)}</h3>
-            <h2 className="text-3xl">Total: <strong>${(diagnosticsAppointmentBill + (diagnosticsAppointmentBill * 0.014975)).toFixed(2)}</strong></h2>
+            <h3>{t('tax-0')}: ${(diagnosticsAppointmentBill * 0.14975).toFixed(2)}</h3>
+            <h2 className="text-3xl">{t('total-0')}: <strong>${(diagnosticsAppointmentBill + (diagnosticsAppointmentBill * 0.014975)).toFixed(2)}</strong></h2>
           </div>
         </article>
       </div>
