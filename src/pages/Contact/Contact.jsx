87c961a715c384697b031db24d28e22a9f94d72c
asfand687 <asfandyar687@gmail.com>
2023-03-26 03:48:29 +0500

    added translations to contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { PhoneIcon, LocationIcon } from '../../assets'
+import { useTranslation } from 'react-i18next'
 
 const Contact = () => {
+  const { t } = useTranslation()
   return (
     <section className="py-20 flex justify-center text-white">
       <article className="flex items-center">
@@ -17,25 +19,25 @@ const Contact = () => {
           </ul>
         </div>
         <div className="py-16 px-12 bg-[#f9f9f9] shadow-cardService rounded-[35px] text-gray-800 font-body">
-          <h3 className="pb-4">Required Field *</h3>
+          <h3 className="pb-4">{t('required-field')} *</h3>
           <form className="space-y-4 w-[500px] text-gray-800">
             <div className="w-full relative">
               <p className="absolute left-0 -top-3">*</p>
-              <input className="w-full px-6 py-3 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" type="text" placeholder="Full Name" />
+              <input className="w-full px-6 py-3 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" type="text" placeholder={t('full-name')} />
             </div>
             <div className="w-full relative">
               <p className="absolute left-0 -top-3">*</p>
-              <input className="w-full px-6 py-3 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" type="email" placeholder="Your Email Address" />
+              <input className="w-full px-6 py-3 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" type="email" placeholder={t('your-email-address')} />
             </div>
             <div className="w-full">
-              <input className="w-full px-6 py-3 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" type="text" placeholder="Organization/Company" />
+              <input className="w-full px-6 py-3 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" type="text" placeholder={t('organization-company')} />
             </div>
             <div className="w-full">
-              <textarea className="w-full h-[300px] p-6 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" placeholder="Your message here..." />
+              <textarea className="w-full h-[300px] p-6 border border-[#48ABC8] focus:shadow-input outline-none rounded-[20px]" placeholder={t('your-message-here')} />
             </div>
             <div className='w-full pt-3'>
               <button type="submit" className={`rounded-full font-main text-xl group hover:ring-1 hover:ring-primary px-12 py-3 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
-                Send Message
+                {t('send-message')}
               </button>
             </div>
           </form>
