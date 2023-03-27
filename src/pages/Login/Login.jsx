3fd4d901b704f27c5195f3072a1af7a48fec1fd1
asfand687 <asfandyar687@gmail.com>
2023-03-27 19:31:41 +0500

    added translation

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,12 +3,14 @@ import { useNavigate } from 'react-router-dom'
 import { registerationStore } from '../../store/registerationStore'
 import toast from 'react-hot-toast'
 import { SVGLoaderCircles } from '../../assets'
+import { useTranslation } from 'react-i18next'
 
 const Login = () => {
   const navigate = useNavigate()
   const loginFormRef = useRef(null)
   const [isLoading, setIsLoading] = useState(false)
   const setUserInfo = registerationStore(state => state.setUserInfo)
+  const { t } = useTranslation()
 
   const handleLogin = async (e) => {
     e.preventDefault()
@@ -50,7 +52,7 @@ const Login = () => {
   }, [])
   return (
     <section className="w-full max-w-[42rem] flex flex-col items-center mx-auto py-8 md:py-20 font-main">
-      <h2 className="text-4xl text-center pb-4">Login</h2>
+      <h2 className="text-4xl text-center pb-4">{t('Login')}</h2>
       <div className="py-16 border space-y-4 px-8 shadow-cardService rounded-[35px] text-primary relative">
         <form ref={loginFormRef} onSubmit={handleLogin} className="space-y-4 w-full lg:w-[500px] text-gray-800">
           <div className="w-full flex">
@@ -58,7 +60,7 @@ const Login = () => {
               className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
               name="email"
               type="email"
-              placeholder="Email*"
+              placeholder={`${t('email-0')}*`}
               required
             />
           </div>
@@ -67,11 +69,11 @@ const Login = () => {
               className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
               name="password"
               type="password"
-              placeholder="Password*"
+              placeholder={`${t('password-0')}*`}
               required
             />
           </div>
-          <p className="cursor-pointer pl-5">Forgot Password?</p>
+          <p className="cursor-pointer pl-5">{t('forgot-password')}?</p>
           <div className='relative w-full pt-3'>
             <button type="submit" className={`rounded-full w-40 font-main text-xl group hover:ring-1 hover:ring-primary  py-3 border flex justify-center items-center space-x-2  border-primary bg-transparent`}>
               <span>Login</span>
