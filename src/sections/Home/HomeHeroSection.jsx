b85f77599ecda0c81f317429f47b2549ea75d576
asfand687 <asfandyar687@gmail.com>
2023-03-30 19:14:53 +0500

    added new hero image

--- src/sections/Home/HomeHeroSection.jsx
+++ src/sections/Home/HomeHeroSection.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { FiArrowUpRight } from 'react-icons/fi'
 import Button from '../../components/Button'
-import { HeroGif, HeroBg } from '../../assets'
+import { HeroGif, HeroBg, HomeHeroImage } from '../../assets'
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 
@@ -54,11 +54,8 @@ const HomeHeroSection = () => {
   }, [])
 
   return (
-    <div className="pt-28 md:pt-20 md:py-20 bg-white">
+    <div className="pt-28 md:pt-20 md:py-20 md:pb-10 bg-white">
       <section className="max-w-[90rem] mx-auto flex flex-col-reverse gap-y-6 md:flex-row items-center relative">
-        <aside className="hidden lg:block absolute -left-[70px] 2xl:-left-[150px] top-[430px] text-black opacity-20 transform -rotate-90">
-          &copy; 2023 MDHUB Inc
-        </aside>
         <article data-aos="fade-up" className="flex-1 px-4 md:pl-20">
           <h2 className="w-full max-w-[750px] font-main text-center md:text-left text-4xl md:text-6xl">
             {t('Accessible, quality healthcare at your fingertips.')}
@@ -68,38 +65,35 @@ const HomeHeroSection = () => {
           </p>
           <div className='flex justify-center md:justify-start space-x-2 md:space-x-6 font-main text-gray-700'>
             <Link to="/login">
-              <Button wide>
-                <span>{t('See A Doctor')}</span> <FiArrowUpRight className="md:text-lg transition-colors ease-out duration-150 group-hover:text-primary" />
+              <Button wide gradient>
+                <span>{t('See A Doctor')}</span> 
               </Button>
             </Link>
             <Link to="/register">
-              <Button wide>
-                <span>{t('Sign Up')}</span> <FiArrowUpRight className="md:text-lg transition-colors ease-out duration-150 group-hover:text-primary" />
+              <Button wide gradient>
+                <span>Sign Up For $29.99</span> 
               </Button>
             </Link>
           </div>
+          <div className="pt-8 max-w-[400px] w-full flex justify-between font-main">
+            <article>
+              <h2 className="text-5xl text-primary">1500+</h2>
+              <p className="font-body text-[#ACACAC] max-w-[50px]">Active Doctors</p>
+            </article>
+            <article>
+              <h2 className="text-5xl text-primary">15K+</h2>
+              <p className="font-body text-[#ACACAC] max-w-[50px]">Active Users</p>
+            </article>
+            <article>
+              <h2 className="text-5xl text-primary">94%</h2>
+              <p className="font-body text-[#ACACAC] max-w-[50px]">Symptoms Treated</p>
+            </article>
+          </div>
         </article>
         <article data-aos="fade-up" className="w-full max-w-[630px] flex justify-center relative">
-          <img className="relative z-10 w-full max-w-[430px]" src={HeroGif} alt="Hero" />
-          <img alt="Hero Bg" src={HeroBg} className="absolute hidden lg:block max-w-[550px] top-0 right-16 z-0" />
+          <img className="relative z-10 w-full max-w-[430px]" src={HomeHeroImage} alt="Hero" />
         </article>
       </section>
-      <div className="hidden relative homepage text-center lg:flex justify-center flip-words font-main">
-        <h2 className="text-primary text-center text-5xl md:text-6xl">
-          {t('Get care today for')}
-        </h2>
-        <div className="w-full absolute left-0 top-[70px]">
-          <div className="w-full h-16 overflow-y-hidden flex justify-center">
-            {
-              selectedValues.map((word, index) => (
-                <span key={`${index}${word}`} className="md:text-6xl">
-                  {t(`${word}`)}
-                </span>
-              ))
-            }
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
