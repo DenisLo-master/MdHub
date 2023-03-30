28a930573affba970b25634cd6e91cc0d38d42bd
asfand687 <asfandyar687@gmail.com>
2023-03-30 19:14:41 +0500

    added new styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ children, wide, fullWidth }) => {
+const Button = ({ children, wide, fullWidth, gradient }) => {
   return (
-    <button className={`rounded-full group hover:ring-1 hover:ring-primary  py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent ${wide ? "w-[160px]" : "px-8"} ${fullWidth ? "flex-1" : ""}`}>
+    <button className={`${gradient ? "bg-gradient-primary text-white" : "border border-primary"} rounded-full group hover:ring-1 hover:ring-primary  py-2  flex justify-center items-center space-x-2  bg-transparent ${wide ? "w-[200px]" : "px-8"} ${fullWidth ? "flex-1" : ""}`}>
       {children}
     </button>
   )
