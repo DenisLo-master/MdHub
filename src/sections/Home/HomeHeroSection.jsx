419bf76b098ac52adf8df140f1ff3cb25bb12348
asfand687 <asfandyar687@gmail.com>
2023-04-01 02:23:42 +0500

    fixed spacing

--- src/sections/Home/HomeHeroSection.jsx
+++ src/sections/Home/HomeHeroSection.jsx
@@ -55,7 +55,7 @@ const HomeHeroSection = () => {
 
   return (
     <div className="md:pt-40 md:py-20 md:pb-10 bg-home-gradient px-4">
-      <section className="max-w-[90rem] mx-auto flex flex-col-reverse gap-y-6 md:flex-row items-center relative">
+      <section className="max-w-[90rem] mx-auto flex flex-col gap-y-6 md:flex-row items-center relative">
         <article data-aos="fade-up" className="flex-1 px-4 md:pl-20">
           <h2 className="w-full max-w-[750px] font-main text-center md:text-left text-3xl md:text-6xl">
             {t('Accessible, quality healthcare at your fingertips.')}
