fc176d05435118382e09c94a80c763a55c0cda91
asfand687 <asfandyar687@gmail.com>
2023-04-01 02:23:50 +0500

    added translations

--- src/sections/Home/HomeMedicalConditionsSection.jsx
+++ src/sections/Home/HomeMedicalConditionsSection.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { HomeMedicalConditionsHero, ArrowIcon, ChevronRight, ForYouBottom } from '../../assets'
-import {Link} from "react-router-dom"
-import Button from '../../components/Button'
+import { Link } from "react-router-dom"
 import { useTranslation } from 'react-i18next'
 import { GoStar } from 'react-icons/go'
 import { AiOutlinePlus } from 'react-icons/ai'
@@ -11,41 +10,41 @@ const HomeMedicalConditionsSection = () => {
 		<section className="font-main bg-home-gradient-reverse">
 			<div className="w-full max-w-7xl mx-auto flex flex-col-reverse gap-y-4 md:flex-row items-start px-4 text-center md:text-left">
       	<article className="flex-1">
-      		<h2 className="text-5xl">Medical Conditions Treated</h2>
+					<h2 className="text-5xl">{t('medical-conditions-treated')}</h2>
       		<p className="w-full max-w-[500px] py-4 font-body">
-      			Our medical team can treat most health conditions online. Our health professionals are available for a consultation and to provide prescriptions for common chronic care conditions.
+						{t('our-medical-team-can-treat-most-health-conditions-online-our-health-professionals-are-available-for-a-consultation-and-to-provide-prescriptions-for-common-chronic-care-conditions')}
       		</p>
       		<div className="max-w-[380px] space-y-4">
       			<article>
-							<h2 className="text-2xl text-primary pb-2">Acute care</h2>
-	      			<p className="font-body text-sm">Our healthcare professionals provide tests and treatment for most common medical problems. 
+							<h2 className="text-2xl text-primary pb-2">{t('acute-care')}</h2>
+							<p className="font-body text-sm">{t('our-healthcare-professionals-provide-tests-and-treatment-for-most-common-medical-problems')}
 	      			</p>
       			</article>
       			<article>
-							<h2 className="text-2xl text-primary pb-2">Chronic care</h2>
+							<h2 className="text-2xl text-primary pb-2">{t('chronic-care')}</h2>
 	      			<p className="font-body text-sm">
-	      				Our healthcare professionals can provide you with a consultation or a prescription for most common chronic illnesses previously diagnosed.
+								{t('our-healthcare-professionals-can-provide-you-with-a-consultation-or-a-prescription-for-most-common-chronic-illnesses-previously-diagnosed-0')}
 	      			</p>
       			</article>
       			<article>
-							<h2 className="text-2xl text-primary pb-2">Mental health</h2>
+							<h2 className="text-2xl text-primary pb-2">{t('mental-health-0')}</h2>
 	      			<p className="font-body text-sm">
-	      				Our healthcare professionals can provide you with a consultation or a prescription for most common chronic illnesses previously diagnosed.
+								{t('our-healthcare-professionals-can-provide-you-with-a-consultation-or-a-prescription-for-most-common-chronic-illnesses-previously-diagnosed')}
 	      			</p>
       			</article>
       			<article>
-							<h2 className="text-2xl text-primary pb-2">Sexual health</h2>
+							<h2 className="text-2xl text-primary pb-2">{t('sexual-health')}</h2>
 	      			<p className="font-body text-sm">
-	      				Our health professionals are available to advise you and prescribe treatment for sexual health problems.
+								{t('our-health-professionals-are-available-to-advise-you-and-prescribe-treatment-for-sexual-health-problems')}
 	      			</p>
       			</article>
 						<article className="text-primary flex items-center gap-x-2">
 							<AiOutlinePlus />
-							<h2 className="text-2xl text-primary">many more</h2>
+							<h2 className="text-2xl text-primary">{t('many-more-0')}</h2>
 						</article>
 						<article>
 							<Link to="/services" className="flex items-center gap-x-1 font-body">
-								Explore all our services and treatments.
+								{t('explore-all-our-services-and-treatments')}
 								<ChevronRight className="text-primary h-3 w-3"/>
 							</Link>
 						</article>
@@ -55,15 +54,15 @@ const HomeMedicalConditionsSection = () => {
       </div>
 			<div className="text-center py-10">
 				<div className="relative">
-					<h2 className="text-4xl md:text-6xl">Get your first year of membership for <span className="line-through">$359</span> $289</h2>
-					{/* <div className="absolute -top-6 right-[23rem] w-[2px] h-28 bg-primary transform rotate-45"></div> */}
+					<h2 className="text-4xl md:text-6xl text-center px-4 pb-7">{t('get-your-first-year-of-membership-for')} <span className="price">$359</span> $289</h2>
+					{/* <div className="absolute -top-6 right-[24%] w-[2px] h-28 bg-primary transform rotate-45"></div> */}
 				</div>
-				<p className="text-lg font-body py-4">Receive immediate personalized and human medical follow-up by registering with MDHub.</p>
+				<p className="text-lg font-body py-4">{t('receive-immediate-personalized-and-human-medical-follow-up-by-registering-with-mdhub')}</p>
 				<div className="flex justify-center">
 					<Link to="/register">
-						<Button wide gradient>
-							<span>Sign Up Today</span>
-						</Button>
+						<button className="border border-primary text-white rounded-full group hover:ring-1 hover:ring-primary  py-2  flex justify-center items-center space-x-2  bg-gradient-primary px-12">
+							<span>{t('sign-up-today')}</span>
+						</button>
 					</Link>
 				</div>
 			</div>
@@ -81,7 +80,7 @@ const HomeMedicalConditionsSection = () => {
 							<GoStar/>
 							<GoStar/>
 						</div>
-						<h2 className="font-main">Reviewing: Lab testing & diagnostics</h2>
+						<h2 className="font-main">{t('reviewing-lab-testing-and-diagnostics')}</h2>
 					</div>
 				</div>
 			</section>
