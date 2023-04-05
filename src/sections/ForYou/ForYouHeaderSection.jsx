5c7dd57b016f5a16843df6e63a41381580c698d4
asfand687 <asfandyar687@gmail.com>
2023-04-06 04:50:25 +0500

    made header reactive for for you and for family

--- src/sections/ForYou/ForYouHeaderSection.jsx
+++ src/sections/ForYou/ForYouHeaderSection.jsx
@@ -3,13 +3,13 @@ import { ForYouHeaderBg } from '../../assets'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
-const ForYouHeaderSection = () => {
+const ForYouHeaderSection = ({ family }) => {
   const { t } = useTranslation()
   return (
-    <section className="relative lg:h-[390px] flex items-center bg-[#FF7455] font-main">
+    <section className={`relative lg:h-[390px] flex items-center font-main ${family ? "bg-[#BE74D0]" : "bg-[#FF7455]"}`}>
       <div className="w-full px-4 text-center md:text-left md:w-[clamp(60rem,80vw,95rem)] mx-auto">
         <h2 className="w-full py-12 max-w-[270px] mx-auto md:mx-0 text-[96px] leading-[90px] text-white">
-          {t('for you')}
+          {family ? t('for family') : t('for you')}
         </h2>
       </div>
     </section>
