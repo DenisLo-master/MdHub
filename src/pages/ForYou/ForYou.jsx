a8a1975ee023598ce6e229970f4dbd4daecad525
asfand687 <asfandyar687@gmail.com>
2023-04-06 04:50:08 +0500

    added translations: For You

--- src/pages/ForYou/ForYou.jsx
+++ src/pages/ForYou/ForYou.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { ForYouHeaderSection } from '../../sections'
 import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { ForYouHeaderSection } from '../../sections'
 import { ForYouFamilyPlan, OkIconWhite, ForYouBottom, ForYouHealthImage } from '../../assets'
-import { useTranslation } from 'react-i18next' 
 
 const ForYou = () => {
   const { t } = useTranslation()
