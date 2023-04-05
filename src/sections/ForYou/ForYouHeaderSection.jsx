085bf253e45df9975ad0230c18db58eee15e6e30
asfand687 <asfandyar687@gmail.com>
2023-04-06 01:12:51 +0500

    added header section: For You

--- src/sections/ForYou/ForYouHeaderSection.jsx
+++ src/sections/ForYou/ForYouHeaderSection.jsx
@@ -6,20 +6,11 @@ import { Link } from 'react-router-dom'
 const ForYouHeaderSection = () => {
   const { t } = useTranslation()
   return (
-    <section className="relative bg-[#EA919B] font-main">
-      <img className="hidden lg:block absolute  object-cover w-full h-[420px] 2xl:h-[450px]" src={ForYouHeaderBg} alt="For You" />
-      <div className="relative z-10 w-full max-w-[900px] md:pl-10 2xl:pl-20 py-10 px-4 text-center lg:text-left space-y-4 flex flex-col items-center md:items-start">
-        <h2 className="text-[70px] lg:text-[88px] leading-[80px] text-white">
-          {t('healthcare-without-the-headaches')}
+    <section className="relative bg-[#FF7455] font-main">
+      <div className="w-full px-4 text-center md:text-left md:w-[clamp(70rem,90vw,100rem)] mx-auto">
+        <h2 className="w-full py-12 max-w-[270px] mx-auto md:mx-0 text-[96px] leading-[90px] text-white">
+          {t('for you')}
         </h2>
-        <p className="font-body text-white">
-          {t('with-mdhub-our-members-can-say-goodbye-to-weeks-of-waiting-for-doctor-appointments-or-long-waits-at-hospitals-forget-driving-and-parking-to-get-a-treatment-our-team-of-over-1-500-canadian-certified-medical-practitioners-is-available-on-demand-from-anywhere-allowing-you-to-take-back-control-of-your-health-members-have-full-control-of-when-and-how-they-access-medical-services-and-can-benefit-from-being-proactive-with-their-care-choices')}
-        </p>
-        <Link className="block" to="/register">
-          <button className="rounded-full text-primary w-[240px] text-lg flex justify-center py-3 border bg-white  border-white hover:opacity-80 transition-opacity ease-in-out duration-300">
-            {t('sign-up-today')}
-          </button>
-        </Link>
       </div>
     </section>
   )
