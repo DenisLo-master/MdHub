ff526d08c5a4226dff7b01f50ce5f4c2b6d28330
asfand687 <asfandyar687@gmail.com>
2023-04-06 00:32:55 +0500

    made images in 2 columns: online pharmacy

--- src/sections/Services/OnlinePharmacyServices.jsx
+++ src/sections/Services/OnlinePharmacyServices.jsx
@@ -28,7 +28,7 @@ const OnlinePharmacyServices = () => {
       </div>
 
       <section className="py-10 max-w-[70rem] mx-auto">
-        <div className=" grid grid-cols-1 lg:grid-cols-4 gap-8 text-lg">
+        <div className=" grid grid-cols-2 lg:grid-cols-4 gap-8 text-lg">
           <article>
             <img className="w-full" src={MedicalTreatmentBC} alt="Birth Control" />
             <div className="py-4 text-center bg-white text-dark">
@@ -55,17 +55,17 @@ const OnlinePharmacyServices = () => {
           </article>
 
           <article className="lg:col-span-2 flex flex-col lg:flex-row gap-4">
-            <h2 className="text-6xl whitespace-nowrap text-center">{t('many-more')}</h2>
+            <h2 className="text-3xl md:text-6xl whitespace-nowrap text-center">{t('many-more')}</h2>
           </article>
 
           <article className="lg:col-span-2 flex flex-col lg:flex-row gap-4">
             <Link className="block w-full" to="/register">
-              <button className="rounded-full w-full text-lg flex justify-center py-3 text-primary bg-white border border-white hover:bg-transparent hover:text-white transition-colors duration-300 ease-out">
+              <button className="rounded-full w-full text-[13px] md:text-lg flex justify-center py-3 text-primary bg-white border border-white hover:bg-transparent hover:text-white transition-colors duration-300 ease-out">
                 {t('sign-up-today')}
               </button>
             </Link>
             <Link className="block w-full" to="/dashboard">
-              <button className="rounded-full w-full text-lg flex justify-center py-3 text-white bg-transparent border border-white hover:bg-white hover:text-primary transition-colors duration-300 ease-out">
+              <button className="rounded-full w-full text-[13px] md:text-lg flex justify-center py-3 text-white bg-transparent border border-white hover:bg-white hover:text-primary transition-colors duration-300 ease-out">
                 {t('get-care-now')}
               </button>
             </Link>
