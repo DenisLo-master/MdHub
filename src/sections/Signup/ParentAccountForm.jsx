f52e66ddddec17272a5fbe7bccabce3cd59fd518
asfand687 <asfandyar687@gmail.com>
2023-04-11 22:47:31 +0500

    added background

--- src/sections/Signup/ParentAccountForm.jsx
+++ src/sections/Signup/ParentAccountForm.jsx
@@ -119,10 +119,10 @@ const ParentAccountForm = () => {
   }
 
   return (
-    <div className="py-8 lg:py-16 border space-y-4 px-4 md:px-8 shadow-cardService rounded-[35px] text-primary relative">
+    <div className="py-8 lg:py-16 border space-y-4 px-4 md:px-8  bg-[#F5F5F5] rounded-[8px] text-primary relative">
       <div className="w-full flex flex-wrap gap-y-4 gap-x-6">
         <input
-          className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
+          className="flex-1 rounded-md text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
           name="firstName"
           type="text"
           placeholder={t('first-name')}
