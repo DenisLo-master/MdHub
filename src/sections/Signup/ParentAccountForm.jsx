12c34861f1fdf0e83a8b61720447e2e3f5ac4e55
asfand687 <asfandyar687@gmail.com>
2023-04-11 23:40:00 +0500

    added show password icon

--- src/sections/Signup/ParentAccountForm.jsx
+++ src/sections/Signup/ParentAccountForm.jsx
@@ -5,6 +5,7 @@ import { BiCaretDown } from 'react-icons/bi'
 import { useTranslation } from 'react-i18next' 
 import { AiFillCloseCircle, AiFillCheckSquare } from 'react-icons/ai'
 import dayjs from 'dayjs'
+import { ShowPasswordIcon } from '../../assets'
 
 
 const ParentAccountForm = () => {
@@ -19,6 +20,7 @@ const ParentAccountForm = () => {
   const [conditionsFulfilled, setConditionsFulfilled] = useState([])
   const [correctDate, setCorrectDate] = useState(false)
   const [previousLength, setPreviousLength] = useState(0);
+  const [showPassword, setShowPassword] = useState(false)
 
   const getStrengthLabel = (strengthScore) => {
     switch (strengthScore) {
@@ -119,38 +121,51 @@ const ParentAccountForm = () => {
   }
 
   return (
-    <div className="py-8 lg:py-16 border space-y-4 px-4 md:px-8  bg-[#F5F5F5] rounded-[8px] text-primary relative">
+    <div className="space-y-6 px-4 md:px-8  rounded-[8px] text-primary relative">
       <div className="w-full flex flex-wrap gap-y-4 gap-x-6">
-        <input
-          className="flex-1 rounded-md text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
-          name="firstName"
-          type="text"
-          placeholder={t('first-name')}
-          value={registerationFormData.firstName}
-          onChange={({ target }) => handleRegisterationFormDataChange(target.name, target.value)}
-          required
-        />
-        <input
-          className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
-          name="lastName"
-          type="text"
-          placeholder={t('last-name')}
-          value={registerationFormData.lastName}
-          onChange={({ target }) => handleRegisterationFormDataChange(target.name, target.value)}
-          required
-        />
+        <article className="flex-1 flex flex-col gap-y-2">
+          <label htmlFor='firstName' className="font-body text-dark font-semibold">
+            First Name
+          </label>
+          <input
+            id="firstName"
+            className="rounded-md text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border border-primary"
+            name="firstName"
+            type="text"
+            value={registerationFormData.firstName}
+            onChange={({ target }) => handleRegisterationFormDataChange(target.name, target.value)}
+            required
+          />
+        </article>
+        <article className="flex-1 flex flex-col gap-y-2">
+          <label htmlFor='lastName' className="font-body text-dark font-semibold">
+            Last Name
+          </label>
+          <input
+            id="lastName"
+            className="rounded-md text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border border-primary"
+            name="lastName"
+            type="text"
+            value={registerationFormData.lastName}
+            onChange={({ target }) => handleRegisterationFormDataChange(target.name, target.value)}
+            required
+          />
+        </article>
       </div>
-      <div className="w-full flex flex-wrap gap-y-4 gap-x-6">
+      <div className="w-full flex flex-col gap-y-2">
+        <label htmlFor='email' className="font-body text-dark font-semibold">
+          Email
+        </label>
         <input
-          className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
+          id="email"
+          className="rounded-md text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border border-primary"
           name="email"
           type="email"
-          placeholder={t('email')}
           value={registerationFormData.email}
           onChange={({ target }) => handleRegisterationFormDataChange(target.name, target.value)}
           required
         />
-        <input
+        {/* <input
           className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
           name="phone"
           type="tel"
@@ -158,54 +173,72 @@ const ParentAccountForm = () => {
           value={registerationFormData.phone}
           onChange={({ target }) => handleRegisterationFormDataChange(target.name, target.value)}
           required
-        />
+        /> */}
       </div>
-      <div className="w-full flex flex-col gap-y-2 relative">
-        <input
-          className="flex-1 rounded-full text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
-          name="password"
-          type="password"
-          pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$"
-          title={t('your-password-must-be-at-least-8-characters-long-and-contain-at-least-one-uppercase-letter-one-lowercase-letter-and-one-number')}
-          placeholder={t('password')}
-          value={registerationFormData.password}
-          onChange={({ target }) => {
-            handleRegisterationFormDataChange(target.name, target.value)
-            checkPasswordStrength(target.value)
-          }}
-          required
-        />
-        {registerationFormData.password && (
-          <div className="pt-6">
-            <ul>
-              {conditionsFulfilled.map((condition, index) => (
-                <li
-                  className="text-green-600 flex items-center gap-x-1 font-body gap-y-2"
-                  key={index}
-                >
-                  <AiFillCheckSquare className="hidden md:block" />
-                  <p>{condition}</p>
-                </li>
-              ))}
-            </ul>
-            <ul>
-              {suggestions.map((suggestion, index) => (
-                <li
-                  className="text-red-600 flex items-center gap-x-1 font-body gap-y-2"
-                  key={index}
-                >
-                  <AiFillCloseCircle className="hidden md:block" />
-                  <p>{suggestion}</p>
-                </li>
-              ))}
-            </ul>
-            <p className="absolute top-[52px] left-0 md:right-0 font-body text-sm md:text-base">
-              {t('password-strength')}: <strong>{strength}</strong>
-            </p>
+      <div className="w-full flex flex-col gap-y-2">
+        <label htmlFor='password' className="font-body text-dark font-semibold">
+          Create a Password
+        </label>
+        <div className="w-full">
+          <div className="relative flex flex-col">
+            <input
+              id="password"
+              className="rounded-md text-xl focus:ring-1 focus:ring-primary outline-none px-8 py-2 border border-primary"
+              name="password"
+              type={`${showPassword ? "text" : "password"}`}
+              pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$"
+              title={t('your-password-must-be-at-least-8-characters-long-and-contain-at-least-one-uppercase-letter-one-lowercase-letter-and-one-number')}
+              value={registerationFormData.password}
+              onChange={({ target }) => {
+                handleRegisterationFormDataChange(target.name, target.value)
+                checkPasswordStrength(target.value)
+              }}
+              required
+            />
+            <div onClick={() => setShowPassword(!showPassword)} className="font-body text-dark absolute flex items-center gap-x-2 right-4 top-[12px] cursor-pointer">
+              <ShowPasswordIcon />
+              <p>Show</p>
+            </div>
           </div>
-        )}
+          {/* {registerationFormData.password && (
+            <div className="pt-6">
+              <ul>
+                {conditionsFulfilled.map((condition, index) => (
+                  <li
+                    className="text-green-600 flex items-center gap-x-1 font-body gap-y-2"
+                    key={index}
+                  >
+                    <AiFillCheckSquare className="hidden md:block" />
+                    <p>{condition}</p>
+                  </li>
+                ))}
+              </ul>
+              <ul>
+                {suggestions.map((suggestion, index) => (
+                  <li
+                    className="text-red-600 flex items-center gap-x-1 font-body gap-y-2"
+                    key={index}
+                  >
+                    <AiFillCloseCircle className="hidden md:block" />
+                    <p>{suggestion}</p>
+                  </li>
+                ))}
+              </ul>
+              <p className="absolute top-[52px] left-0 md:right-0 font-body text-sm md:text-base">
+                {t('password-strength')}: <strong>{strength}</strong>
+              </p>
+            </div>
+          )} */}
+        </div>
+        <ul className="font-body text-dark list-disc pt-4 pl-5">
+          <li>8 or more characters</li>
+          <li>Upper + lowercase letters</li>
+          <li>At least one number</li>
+          <li>No repetitive characters (e.g. 'bbb', 'Aaa')</li>
+          <li>No sequential characters (e.g. 'abc', '123', '321' or 'cba')</li>
+        </ul>
       </div>
-      <div className="text-xl flex flex-col lg:flex-row gap-y-4 items-center w-full gap-x-6">
+      {/* <div className="text-xl flex flex-col lg:flex-row gap-y-4 items-center w-full gap-x-6">
 
         <div className="w-full flex-1 flex relative">
           <BiCaretDown className="text-2xl absolute right-3 pointer-events-none top-2" />
@@ -228,24 +261,24 @@ const ParentAccountForm = () => {
         <div className="w-full flex-1 flex flex-col">
           <input
             className="flex-1 rounded-full text-xl lg:text-lg focus:ring-1 focus:ring-primary outline-none px-8 py-2 border"
-            type="text"            
-            value={registerationFormData.dateOfBirth} 
+            type="text"
+            value={registerationFormData.dateOfBirth}
             onChange={({ target }) => handleDateChange(target.value)}
             placeholder={t('date-of-birth-mm-dd-yyyy')}
           />
         </div>
-      </div>
+      </div> */}
       {selectedAccountType === "family" && (
         <div className="text-center flex lg:block flex-col items-center gap-y-4">
           <button
             disabled={childForms.length > 1}
             onClick={addChildAccount}
-            className="lg:absolute bottom-4 left-8 flex gap-x-2 text-gray-600 font-body items-center text-3xl cursor-pointer"
+            className="lg:absolute -bottom-36 left-8 flex gap-x-2 text-gray-600 font-body items-center text-xl cursor-pointer"
           >
             <BsPlusCircle />
             <p className='text-base md:text-xl'>{t('add-family-member')}</p>
           </button>
-          <div className="lg:absolute bottom-4 right-8 text-gray-600 font-body">
+          <div className="lg:absolute -bottom-36 right-8 text-gray-600 font-body">
             <p className='text-base md:text-xl'>{t('account-admin')}</p>
           </div>
         </div>
@@ -255,7 +288,7 @@ const ParentAccountForm = () => {
           <button
             disabled={childForms.length > 8}
             onClick={addChildAccount}
-            className="lg:absolute bottom-4 left-8 flex gap-x-2 text-gray-600 font-body items-center text-3xl cursor-pointer"
+            className="lg:absolute bottom-4 left-8 flex gap-x-2 text-gray-600 font-body items-center text-xl cursor-pointer"
           >
             <BsPlusCircle />
             <p className='text-base md:text-xl'>{t('add-employee')}</p>
