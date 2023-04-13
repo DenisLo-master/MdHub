aaa41d5b928e6cf0814b6afff61fd848cc4f4a78
asfand687 <asfandyar687@gmail.com>
2023-04-14 03:15:22 +0500

    changed the cards

--- src/sections/Pricing/PricingSlider.jsx
+++ src/sections/Pricing/PricingSlider.jsx
@@ -6,7 +6,8 @@ const PricingSlider = () => {
   const [selectedPackage, setSelectedPackage] = useState("single")
   const { t } = useTranslation()
   return (
-    <section className="lg:py-10 px-4">
+    <section className="lg:py-10 lg:pt-20 px-4">
+      <h2 className="text-center text-4xl">Plan Type</h2>
       <div className="pb-10 pt-3 lg:pb-3 lg:py-3 px-4 lg:my-10 font-main text-lg flex flex-col md:flex-row items-center justify-between gap-x-4 w-full rounded-b-full lg:rounded-full bg-white lg:bg-[#F1F1F1] shadow-sliderNav max-w-[485px] mx-auto">
         <button
           onClick={() => setSelectedPackage("single")}
@@ -27,141 +28,199 @@ const PricingSlider = () => {
         </button>
       </div>
 
-      <section className={`relative transition-all ease-in-out duration-700 ${selectedPackage === "corporate" ? "h-[900px] lg:h-[300px]" : "h-[900px] lg:h-[490px]"}`}>
-        <div className={`w-full font-body flex flex-col lg:flex-row items-center justify-center gap-y-10 gap-x-14 absolute left-0 top-10 transition-opacity ease-in-out duration-700 ${selectedPackage === "single" ? "opacity-100" : "opacity-0"}`}>
-          <article className="p-8 px-12 flex flex-col items-center rounded-lg shadow-glow">
-            <h2 className="text-3xl font-main">{t('monthly')}</h2>
-            <div className="text-primary pt-8">
-              <h2 className="text-[40px] leading-9">$29.99</h2>
-              <p className="text-base text-dark text-center">{t('per-month')}</p>
+      <section className={`relative transition-all ease-in-out duration-700 ${selectedPackage === "corporate" ? "h-[900px] lg:h-[400px]" : "h-[900px] lg:h-[490px]"}`}>
+        <div className={`w-full font-body flex flex-col lg:flex-row items-center lg:items-start justify-center gap-y-10 gap-x-14 absolute left-0 top-10 transition-opacity ease-in-out duration-700 ${selectedPackage === "single" ? "opacity-100" : "opacity-0"}`}>
+          <article className="max-w-[400px] flex flex-col items-center rounded-t-[32px] bg-[#299EBF] text-white">
+            <div className="text-center p-8">
+              <h2 className="text-3xl font-main pb-2">On Demand</h2>
+              <p className="font-body leading-5">An on demand plan for full access to our services with a single payment per consultation for our virtual doctors and mental health.</p>
             </div>
-            <div className="pt-8 space-y-2 text-center">
-              <Link className="relative z-10 rounded-full w-[240px] text-lg flex justify-center py-2 bg-gradient-primary text-white" to="/register">
-                {t('get-started')}
-              </Link>
-              <p className="text-sm text-primary">{t('3-months-minimum')}*</p>
+            <div className="w-full rounded-t-[32px] bg-[#0B637B] pt-8 p-6 flex flex-col items-center">
+              <div className="text-center font-body">
+                <h2 className="text-6xl font-semibold">$0</h2>
+                <p>per month</p>
+                <p className="pt-6 pb-4">$89 per consultation</p>
+              </div>
+              <div>
+                <Link className="relative z-10 rounded-full w-[240px] text-lg flex justify-center py-2 bg-white text-dark font-main" to="/register">
+                  Sign Up Today
+                </Link>
+              </div>
             </div>
           </article>
 
-          <article className="p-8 px-12 flex flex-col items-center rounded-lg shadow-glow relative">
+
+          <article className="max-w-[400px] mt-8 flex flex-col items-center rounded-t-[32px] bg-[#1EBC91] text-white relative">
             <section className="absolute -top-7 left-0 w-full flex justify-center">
-              <div className="absolute flex justify-center items-center top-0 left-1/2 transform translate-x-[-50%] bg-gradient-primary rounded-2xl w-56 h-16 text-white font-main text-center">
+              <div className="absolute flex justify-center items-center top-0 left-1/2 transform translate-x-[-50%] bg-white rounded-2xl w-44 h-16 text-dark font-main text-center">
                 {t('save-25-compared-to-monthly-plan')}
               </div>
             </section>
-            <h2 className="text-5xl font-main pt-12">{t('yearly')}</h2>
-            <div className="text-primary pt-14 py-10">
-              <h2 className="text-[52px] leading-9">$24.99</h2>
-              <p className="text-base text-dark text-center">{t('per-month')}</p>
+            <div className="text-center pt-12 p-8">
+              <h2 className="text-5xl font-main pb-2">Yearly</h2>
+              <p className="font-body leading-5">One annual payment saving 25% to have full access to all of our services.</p>
             </div>
-            <div className="pt-8 space-y-2 text-center">
-              <Link className="block" to="/register">
-                <button className="relative z-10 rounded-full w-[240px] text-lg flex justify-center py-2 bg-gradient-primary text-white">
-                  {t('get-started')}
-                </button>
-              </Link>
-              <p className="text-sm text-primary">{t('12-months-minimum')}*</p>
+            <div className="w-full rounded-t-[32px] bg-[#1B5747] pt-8 p-6 flex flex-col items-center">
+              <div className="text-center font-body pb-14">
+                <h2 className="text-6xl font-semibold">$24.99</h2>
+                <p>per month</p>
+              </div>
+              <div>
+                <Link className="relative z-10 rounded-full w-[240px] text-lg flex justify-center py-2 bg-white text-dark font-main" to="/register">
+                  Sign Up Today
+                </Link>
+                <p className="text-center font-body pt-3">
+                  12 months minimum*
+                </p>
+              </div>
+            </div>
+          </article>
+
+          <article className="max-w-[400px] flex flex-col items-center rounded-t-[32px] bg-[#299EBF] text-white">
+            <div className="text-center p-8">
+              <h2 className="text-3xl font-main pb-2">Monthly</h2>
+              <p className="font-body leading-5">A preferred monthly rate to have full access to all of our services.
+              </p>
+            </div>
+            <div className="w-full rounded-t-[32px] bg-[#0B637B] pt-8 p-6 flex flex-col items-center">
+              <div className="text-center font-body">
+                <h2 className="text-6xl font-semibold">$29.99</h2>
+                <p>per month</p>
+              </div>
+              <div>
+                <Link className="relative z-10 rounded-full w-[240px] text-lg flex justify-center py-2 bg-white text-dark font-main" to="/register">
+                  Sign Up Today
+                </Link>
+                <p className="text-center font-body pt-3">
+                  12 months minimum*
+                </p>
+              </div>
             </div>
           </article>
         </div>
 
-        <div className={`w-full font-body flex flex-col lg:flex-row items-center justify-center gap-y-10 gap-x-14 absolute left-0 top-10 transition-opacity ease-in-out duration-700 ${selectedPackage === "family" ? "opacity-100" : "opacity-0"}`}>
-          <article className="p-8 px-12 flex flex-col items-center rounded-lg shadow-glow">
-            <div className="space-y-2 text-center font-main">
-              <h2 className="text-3xl">{t('monthly')}</h2>
-              <h4 className="text-base">{t('includes-all-family-members-in-the-same-household')}</h4>
+        <div className={`w-full font-body flex flex-col lg:flex-row items-center lg:items-start justify-center gap-y-10 gap-x-14 absolute left-0 top-10 transition-opacity ease-in-out duration-700 ${selectedPackage === "family" ? "opacity-100" : "opacity-0"}`}>
+          <article className="max-w-[400px] flex flex-col items-center rounded-t-[32px] bg-[#299EBF] text-white">
+            <div className="text-center p-8">
+              <h2 className="text-3xl font-main pb-2">On Demand</h2>
+              <p className="font-body leading-5">An on demand plan for full access to our services with a single payment per consultation for our virtual doctors and mental health.</p>
             </div>
-            <div className="text-primary pt-8">
-              <h2 className="text-[40px] leading-9">$49.99</h2>
-              <p className="text-base text-dark text-center">{t('per-month')}</p>
-            </div>
-            <div className="pt-8 space-y-2 text-center">
-              <Link className="block" to="/register">
-                <button className="rounded-full w-[240px] text-lg flex justify-center py-2 bg-gradient-primary text-white">
-                  {t('get-started')}
-                </button>
-              </Link>
-              <p className="text-sm text-primary">{t('3-months-minimum')}*</p>
+            <div className="w-full rounded-t-[32px] bg-[#0B637B] pt-8 p-6 flex flex-col items-center">
+              <div className="text-center font-body">
+                <h2 className="text-6xl font-semibold">$0</h2>
+                <p>per month</p>
+                <p className="pt-6 pb-4">$89 per consultation</p>
+              </div>
+              <div>
+                <Link className="relative z-10 rounded-full w-[240px] text-lg flex justify-center py-2 bg-white text-dark font-main" to="/register">
+                  Sign Up Today
+                </Link>
+              </div>
             </div>
           </article>
 
-          <article className="p-8 px-12 flex flex-col items-center rounded-lg shadow-glow relative">
+
+          <article className="max-w-[400px] mt-8 flex flex-col items-center rounded-t-[32px] bg-[#1EBC91] text-white relative">
             <section className="absolute -top-7 left-0 w-full flex justify-center">
-              <div className="absolute flex justify-center items-center top-0 left-1/2 transform translate-x-[-50%] bg-gradient-primary rounded-2xl w-56 h-16 text-white font-main text-center">
+              <div className="absolute flex justify-center items-center top-0 left-1/2 transform translate-x-[-50%] bg-white rounded-2xl w-44 h-16 text-dark font-main text-center">
                 {t('save-25-compared-to-monthly-plan')}
               </div>
             </section>
-            <div className="font-main text-center">
-              <h2 className="text-5xl pt-12">{t('yearly')}</h2>
-              <h4 className="text-base">{t('includes-all-family-members-in-the-same-household')}</h4>
+            <div className="text-center pt-12 p-8">
+              <h2 className="text-5xl font-main pb-2">Yearly</h2>
+              <p className="font-body leading-5">One annual payment saving 25% to have full access to all of our services.</p>
             </div>
-            <div className="text-primary pt-14 py-10">
-              <h2 className="text-[52px] leading-9">$39.99</h2>
-              <p className="text-base text-dark text-center">{t('per-month')}</p>
+            <div className="w-full rounded-t-[32px] bg-[#1B5747] pt-8 p-6 flex flex-col items-center">
+              <div className="text-center font-body pb-14">
+                <h2 className="text-6xl font-semibold">$39.99</h2>
+                <p>per month</p>
+              </div>
+              <div>
+                <Link className="relative z-10 rounded-full w-[240px] text-lg flex justify-center py-2 bg-white text-dark font-main" to="/register">
+                  Sign Up Today
+                </Link>
+                <p className="text-center font-body pt-3">
+                  12 months minimum*
+                </p>
+              </div>
             </div>
-            <div className="pt-8 space-y-2 text-center">
-              <Link className="block" to="/register">
-                <button className="rounded-full w-[240px] text-lg flex justify-center py-2 bg-gradient-primary text-white">
-                  {t('get-started')}
-                </button>
-              </Link>
-              <p className="text-sm text-primary">12 {t('months-minimum')}*</p>
+          </article>
+
+          <article className="max-w-[400px] flex flex-col items-center rounded-t-[32px] bg-[#299EBF] text-white">
+            <div className="text-center p-8">
+              <h2 className="text-3xl font-main pb-2">Monthly</h2>
+              <p className="font-body leading-5">A preferred monthly rate to have full access to all of our services.
+              </p>
+            </div>
+            <div className="w-full rounded-t-[32px] bg-[#0B637B] pt-8 p-6 flex flex-col items-center">
+              <div className="text-center font-body">
+                <h2 className="text-6xl font-semibold">$49.99</h2>
+                <p>per month</p>
+              </div>
+              <div>
+                <Link className="relative z-10 rounded-full w-[240px] text-lg flex justify-center py-2 bg-white text-dark font-main" to="/register">
+                  Sign Up Today
+                </Link>
+                <p className="text-center font-body pt-3">
+                  12 months minimum*
+                </p>
+              </div>
             </div>
           </article>
         </div>
 
         <div className={`w-full font-body  absolute left-0 top-10 transition-opacity ease-in-out duration-700 ${selectedPackage === "corporate" ? "opacity-100" : "opacity-0"}`}>
-          <div className="flex flex-col lg:flex-row items-center justify-center gap-y-10 gap-x-14">
-            <article className="p-4 px-12 flex flex-col items-center rounded-lg shadow-glow">
-              <div className="text-center font-main">
+          <div className="w-full flex flex-col lg:flex-row items-center justify-center gap-y-10 gap-x-14">
+            <article className="w-full max-w-[400px] flex flex-col rounded-t-[32px] bg-[#299EBF] text-white items-center">
+              <div className="text-center py-8 font-main">
                 <h2 className="text-3xl">10-50 {t('employees')}</h2>
               </div>
-              <div className="text-primary pt-8">
-                <h2 className="text-[40px] leading-9">$19.99</h2>
-                <p className="text-base text-dark text-center">{t('per-month')}</p>
+              <div className="w-full rounded-t-[32px] bg-[#0B637B] pt-8 p-6 flex flex-col items-center">
+                <div className="text-center font-body pb-10">
+                  <h2 className="text-6xl font-semibold">$19.99</h2>
+                  <p>per month</p>
               </div>
-              <div className="pt-4 space-y-2 text-center">
-                <Link className="block" to="/register">
-                  <button className="rounded-full w-[240px] text-lg flex justify-center py-2 bg-gradient-primary text-white">
-                    {t('get-started')}
-                  </button>
+                <div>
+                  <Link className="relative z-10 rounded-full w-[240px] text-lg flex justify-center py-2 bg-white text-dark font-main" to="/register">
+                    Sign Up Today
                 </Link>
+              </div>
               </div>
             </article>
-            <article className="p-4 px-12 flex flex-col items-center rounded-lg shadow-glow">
-              <div className="text-center font-main">
-                <h2 className="text-3xl">51-100 {t('employees-0')}</h2>
+            <article className="w-full max-w-[400px] flex flex-col rounded-t-[32px] bg-[#1EBC91] text-white items-center">
+              <div className="text-center py-8 font-main">
+                <h2 className="text-3xl">51-100 {t('employees')}</h2>
               </div>
-              <div className="text-primary pt-8">
-                <h2 className="text-[40px] leading-9">$17.99</h2>
-                <p className="text-base text-dark text-center">{t('per-month')}</p>
+              <div className="w-full rounded-t-[32px] bg-[#1B5747] pt-8 p-6 flex flex-col items-center">
+                <div className="text-center font-body pb-10">
+                  <h2 className="text-6xl font-semibold">$17.99</h2>
+                  <p>per month</p>
               </div>
-              <div className="pt-4 space-y-2 text-center">
-                <Link className="block" to="/register">
-                  <button className="rounded-full w-[240px] text-lg flex justify-center py-2 bg-gradient-primary text-white">
-                    {t('get-started')}
-                  </button>
+                <div>
+                  <Link className="relative z-10 rounded-full w-[240px] text-lg flex justify-center py-2 bg-white text-dark font-main" to="/register">
+                    Sign Up Today
                 </Link>
+              </div>
               </div>
             </article>
-            <article className="p-4 px-12 flex flex-col items-center rounded-lg shadow-glow">
-              <div className="text-center font-main">
-                <h2 className="text-3xl">101+ {t('employees-0')}</h2>
+            <article className="w-full max-w-[400px] flex flex-col rounded-t-[32px] bg-[#299EBF] text-white items-center">
+              <div className="text-center py-8 font-main">
+                <h2 className="text-3xl">101+ {t('employees')}</h2>
               </div>
-              <div className="text-primary pt-8">
-                <h2 className="text-[40px] leading-9">$14.99</h2>
-                <p className="text-base text-dark text-center">{t('per-month')}</p>
+              <div className="w-full rounded-t-[32px] bg-[#0B637B] pt-8 p-6 flex flex-col items-center">
+                <div className="text-center font-body pb-10">
+                  <h2 className="text-6xl font-semibold">$14.99</h2>
+                  <p>per month</p>
               </div>
-              <div className="pt-4 space-y-2 text-center">
-                <Link className="block" to="/register">
-                  <button className="rounded-full w-[240px] text-lg flex justify-center py-2 bg-gradient-primary text-white">
-                    {t('get-started')}
-                  </button>
+                <div>
+                  <Link className="relative z-10 rounded-full w-[240px] text-lg flex justify-center py-2 bg-white text-dark font-main" to="/register">
+                    Sign Up Today
                 </Link>
+              </div>
               </div>
             </article>
           </div>
-          <p className="text-center text-sm pt-8 text-primary">
+          <p className="text-center text-sm pt-8 text-dark">
             *{t('price-is-per-employee')}, {t('per-month')}, {t('for-a-minimum-term-of-12-months')}
           </p>
         </div>
