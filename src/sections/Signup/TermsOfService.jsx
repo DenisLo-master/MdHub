48fa015dede153e9af26b5a02426d3aa4a166360
asfand687 <asfandyar687@gmail.com>
2023-04-14 01:06:27 +0500

    added white background

--- src/sections/Signup/TermsOfService.jsx
+++ src/sections/Signup/TermsOfService.jsx
@@ -5,12 +5,12 @@ const TermsOfService = () => {
   const [medicalTermsAccepted, setMedicalTermsAccepted] = useState(false)
   const [privacyPolicyAccepted, setPrivacyPolicyAccepted] = useState(false)
   return (
-    <section className="space-y-6">
-      <div className="space-y-4 px-8 text-dark font-body">
+    <section>
+      <div className="space-y-4 px-8 text-dark font-body pt-6">
         <p>I am at least 18 years of age and I have read and accept:</p>
         <div className="flex flex-col gap-y-4">
           <article 
-            className="flex justify-between items-center rounded-md text-xl px-8 py-2 border border-primary">
+            className="flex justify-between items-center rounded-md text-xl px-8 py-2 border border-primary bg-white">
             <p>Membership Terms of Service</p>
             <input 
               checked={membershipTermsAccepted}
@@ -22,7 +22,7 @@ const TermsOfService = () => {
             />
             </article>
             <article 
-              className="flex justify-between items-center rounded-md text-xl px-8 py-2 border border-primary">
+              className="flex justify-between items-center rounded-md text-xl px-8 py-2 border border-primary bg-white">
               <p>Medical Terms of Service</p>
               <input 
                 checked={medicalTermsAccepted}
@@ -34,7 +34,7 @@ const TermsOfService = () => {
               />
             </article>
             <article 
-              className="flex justify-between items-center rounded-md text-xl px-8 py-2 border border-primary">
+              className="flex justify-between items-center rounded-md text-xl px-8 py-2 border border-primary bg-white">
               <p>Privacy Policy</p>
               <input 
                 checked={privacyPolicyAccepted}
