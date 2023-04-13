a95f215c776ceec16fdfb795e67f64fd98cb5ab3
asfand687 <asfandyar687@gmail.com>
2023-04-14 03:15:08 +0500

    changed the layout

--- src/sections/Pricing/HeadspaceSection.jsx
+++ src/sections/Pricing/HeadspaceSection.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { HeadspaceLogo } from '../../assets'
+import { HeadspaceLogoWhite } from '../../assets'
 import { useTranslation } from 'react-i18next'
 
 const HeadspaceSection = () => {
   const { t } = useTranslation()
   return (
-    <div className="py-10 flex justify-center font-main px-4">
-      <article className="max-w-[1200px] flex flex-col-reverse lg:flex-row justify-between items-center">
+    <div className="py-32 text-white bg-headspace font-main px-4">
+      <article className="flex flex-col-reverse lg:flex-row-reverse justify-around items-center">
         <h3 className="text-3xl max-w-[600px] text-center lg:text-left">
           {t('free-3-months-of-headspace-membership-included-in-all-our-plans')}. ($36 {t('value')})
         </h3>
-        <img className="w-full max-w-[500px]" src={HeadspaceLogo} alt="Headspace" />
+        <img className="w-full max-w-[450px]" src={HeadspaceLogoWhite} alt="Headspace" />
       </article>
     </div>
   )
