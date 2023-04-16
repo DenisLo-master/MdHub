ef7554f7300918a37c14f3d8dacb8b5e49740c78
asfand687 <asfandyar687@gmail.com>
2023-04-16 21:57:45 +0500

    added new layout: services

--- src/sections/Services/ServicesHeaderSection.jsx
+++ src/sections/Services/ServicesHeaderSection.jsx
@@ -7,7 +7,7 @@ import {
   StethoscopeIcon,
   NursingHomecareIcon,
   MentalHealthIcon,
-  StethoscopeIconWhite
+  StethoscopeIconBlack
 } from '../../assets'
 import { useTranslation } from 'react-i18next'
 
@@ -20,41 +20,38 @@ const ServicesHeaderSection = () => {
       <h2 className="font-main py-10 text-4xl text-center">{t('Services')}</h2>
       <div className={`w-full max-w-[1170px] mx-auto rounded-b-full lg:rounded-full shadow-xl flex flex-col lg:flex-row justify-center ${i18n.language === "fr" && "lg:justify-between lg:px-6"} items-center pb-24 lg:pb-3 py-3 gap-x-6 text-[#3F3F3F] font-main`}>
         <Link to="/services">
-          <ServicesCard title={t('virtual-doctor')} bgPrimary={location.pathname === "/services"}>
-            <VirtualDoctorIcon className={`${location.pathname === "/services" ? "text-white" : "text-primary"}`} />
+          <ServicesCard bgColor="virtualDoctor" title={t('virtual-doctor')} bgPrimary={location.pathname === "/services"}>
+            <VirtualDoctorIcon className="text-black" />
           </ServicesCard>
         </Link>
 
         <Link to="/services/mental-health">
-          <ServicesCard title={t('mental health')} bgPrimary={location.pathname === "/services/mental-health"}>
-            <MentalHealthIcon className={`${location.pathname === "/services/mental-health" ? "text-white" : "text-primary"}`} />
+          <ServicesCard bgColor="mentalHealth" title={t('mental health')} bgPrimary={location.pathname === "/services/mental-health"}>
+            <MentalHealthIcon className="text-black" />
           </ServicesCard>
         </Link>
 
         <Link to="/services/online-pharmacy">
-          <ServicesCard
+          <ServicesCard bgColor="onlinePharmacy"
             title={t('online-pharmacy')}
             bgPrimary={location.pathname === "/services/online-pharmacy"}
           >
             <OnlinePharmacyIcon
-              className={`${location.pathname === "/services/online-pharmacy" ? "text-white" : "text-primary"}`}
+              className="text-black"
             />
           </ServicesCard>
         </Link>
 
         <Link to="/services/diagnostics">
-          <ServicesCard title={t('diagnostics')} bgPrimary={location.pathname === "/services/diagnostics"}>
-            {
-              location.pathname === "/services/diagnostics" ?
-                <StethoscopeIconWhite /> : <StethoscopeIcon />
-            }
-        </ServicesCard>
+          <ServicesCard bgColor="diagnostics" title={t('diagnostics')} bgPrimary={location.pathname === "/services/diagnostics"}>
+            <StethoscopeIconBlack />
+          </ServicesCard>
         </Link>
 
         <Link to="/services/nursing-homecare">
-          <ServicesCard title={t('nursing-and-homecare')} bgPrimary={location.pathname === "/services/nursing-homecare"}>
+          <ServicesCard bgColor="nursingHomecare" title={t('nursing-and-homecare')} bgPrimary={location.pathname === "/services/nursing-homecare"}>
             <NursingHomecareIcon
-             className={`${location.pathname === "/services/nursing-homecare" ? "text-white" : "text-primary"}`}
+              className="text-black"
             />
           </ServicesCard>
         </Link>
