44e13f6e5877a9702658c453bdd33411fd726eff
asfand687 <asfandyar687@gmail.com>
2023-04-16 21:57:15 +0500

    added new layout mental health

--- src/sections/Services/MentalHealthServices.jsx
+++ src/sections/Services/MentalHealthServices.jsx
@@ -10,80 +10,37 @@ import {
   VirtualCare01,
   VirtualCare02,
   VirtualCare03,
-  VirtualCare04
+  VirtualCare04,
+  Yellow,
 } from '../../assets'
 import { useTranslation } from 'react-i18next'
 
 const MentalHealthServices = () => {
   const { t } = useTranslation()
   return (
-    <section className="bg-primary text-white py-14 px-4">
-      <div className="w-full max-w-[52rem] mx-auto text-center space-y-4">
-        <h3 className="text-4xl md:text-5xl">
-          {t('get-care-for-your-mental-health')}
-        </h3>
-        <p className="font-body max-w-xl mx-auto text-ligh">
-          {t('we-can-all-use-a-little-help-sometimes-get-mental-health-care-and-support-with-mdhubs-suite-of-mental-health-services-so-you-can-feel-your-best')}
-        </p>
+    <section className="bg-[#F6E2B0] text-white w-full lg:h-[530px] py-10 relative">
+      <div className="max-w-[83rem] mx-auto flex flex-col justify-center h-full">
+        <article className="px-4 space-y-5 flex flex-col items-center lg:items-start text-center">
+          <h2 className="text-5xl lg:text-[48px] text_black font-semibold max-w-lg text-left">
+            {t('Get care for your mental health')}
+          </h2>
+          <div className="space-y-5 font-body text-lg max-w-lg">
+            <p className="text-center lg:text-left text_black text-base leading-4">
+              {t('We can all use a little help sometimes. Get mental health care and support with MDHUB’s suite of mental health services, so you can feel your best.')}
+            </p>
+          </div>
+          <Link className="block" to="/register">
+            <button className="rounded-full w-[480px] mt-[48px] text-lg flex justify-center py-3 border bg-[#3F3F3F] border-none hover:opacity-80 transition-opacity ease-in-out duration-300 text-white font-helvetica">
+              {t('sign-up-today')}
+            </button>
+            <button className="rounded-full w-[480px] mt-8 text-lg flex justify-center py-3 border bg-white border-none hover:opacity-80 transition-opacity ease-in-out duration-300 text-[#3F3F3F] font-helvetica">
+              {t('Get Care Now')}
+            </button>
+          </Link>
+        </article>
+        <img className="hidden lg:block absolute right-5 top-5" src={Yellow} alt="virtual doctor" />
       </div>
 
-      <section className="py-10 max-w-[70rem] mx-auto">
-        <div className=" grid grid-cols-2 lg:grid-cols-4 gap-8 text-lg">
-          <article>
-            <img className="w-full" src={MentalHealthServices01} alt="Anxiety" />
-            <div className="py-4 text-center bg-white text-dark">
-              {t('anxiety-0')}
-            </div>
-          </article>
-          <article>
-            <img className="w-full" src={MentalHealthServices02} alt="Depression" />
-            <div className="py-4 text-center bg-white text-dark">
-              {t('depression-0')}
-            </div>
-          </article>
-          <article>
-            <img className="w-full" src={MentalHealthServices03} alt="Disordered eating" />
-            <div className="py-4 text-center bg-white text-dark">
-              {t('disordered-eating')}
-            </div>
-          </article>
-          <article>
-            <img className="w-full" src={MentalHealthServices04} alt="Sleep issues" />
-            <div className="py-4 text-center bg-white text-dark">
-              {t('sleep-issues')}
-            </div>
-          </article>
-
-          <article className="lg:col-span-2 relative p-4 bg-white text-center text-dark rounded-md">
-            <p>
-              {t('loss-of-focus')}
-            </p>
-          </article>
-
-          <article className="lg:col-span-2 relative p-4 bg-white text-center text-dark rounded-md">
-            <p>
-              {t('suicidal-thoughts')}
-            </p>
-          </article>
-
-          <article className="lg:col-span-2 flex flex-col lg:flex-row gap-4">
-            <h2 className="text-3xl md:text-6xl whitespace-nowrap text-center">{t('many-more')}</h2>
-          </article>
-
-          <article className="lg:col-span-2 flex flex-col lg:flex-row gap-4">
-            <Link className="block w-full" to="/register">
-              <button className="rounded-full w-full text-[12px] md:text-lg  flex justify-center py-3 text-primary bg-white border border-white hover:bg-transparent hover:text-white transition-colors duration-300 ease-out">
-                {t('sign-up-today')}
-              </button>
-            </Link>
-            <Link className="block w-full" to="/dashboard">
-              <button className="rounded-full w-full text-[12px] md:text-lg  flex justify-center py-3 text-white bg-transparent border border-white hover:bg-white hover:text-primary transition-colors duration-300 ease-out">
-                {t('get-care-now')}
-              </button>
-            </Link>
-          </article>
-        </div>
-      </section>
     </section>
   )
 }
