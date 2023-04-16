cade83e4c4cdf283bac937325ffce4298e2648ec
asfand687 <asfandyar687@gmail.com>
2023-04-16 21:55:57 +0500

    added bg prop

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-const ServicesCard = ({ title, children, bgPrimary }) => {
+const ServicesCard = ({ title, children, bgPrimary, bgColor }) => {
   const { i18n } = useTranslation()
   return (
-    <article className={`flex justify-center gap-x-2 items-center ${bgPrimary ? 'bg-primary shadow-cardService' : 'bg-transparent'} ${i18n.language === "en" ? "w-[208px]" : "w-auto px-4"} py-3 rounded-full w-[208px]`}>
+    <article className={`flex justify-center gap-x-2 items-center ${bgPrimary ? `bg-${bgColor} shadow-cardService` : 'bg-transparent'} ${i18n.language === "en" ? "w-[208px]" : "w-auto px-4"} py-3 rounded-full w-[208px]`}>
       {
         children
       }
-      <p className={`text-base md:text-lg leading-5 ${bgPrimary ? "text-white" : "text-gray-800"}`}>{title}</p>
+      <p className={`text-base md:text-lg leading-5 text-black`}>{title}</p>
     </article>
   )
 }
