7a8c5d83066ad0b30b0f8bd038e025aea1cbd957
asfand687 <asfandyar687@gmail.com>
2023-04-16 21:55:46 +0500

    added new prop

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 import { ArrowRight } from '../assets'
 
-const FAQ = ({ question, children }) => {
+const FAQ = ({ question, children, bgColor }) => {
   const [showAnswer, setShowAnswer] = useState(false)
   return (
-    <div className="border-b border-dark pb-3">
+    <div className="border-b border-dark pb-4">
       <div onClick={() => setShowAnswer(!showAnswer)} className='flex justify-between'>
         <article className="text-xl">
           {question}
         </article>
         <div>
-          <ArrowRight className={`text-primary transform ${showAnswer ? "-rotate-90" : "rotate-90"}`} />
+          <ArrowRight className={`text-[${bgColor}] transform ${showAnswer ? "-rotate-90" : "rotate-90"}`} />
         </div>
       </div>
       {
