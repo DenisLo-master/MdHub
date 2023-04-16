5dde908ec3c5915d1a418ad68bba32ae6c91e94d
asfand687 <asfandyar687@gmail.com>
2023-04-16 21:56:19 +0500

    added new layout

--- src/pages/Services/ServicesMentalHealth.jsx
+++ src/pages/Services/ServicesMentalHealth.jsx
@@ -8,7 +8,7 @@ import {
   HeadspaceLogo
 } from '../../assets'
 import { useTranslation } from 'react-i18next'
-import {MentalHealthServices} from '../../sections'
+import { MentalHealthServices } from '../../sections'
 import { mentalHealthFAQData } from '../../constants'
 import FAQ from "../../components/FAQ.jsx"
 
@@ -21,7 +21,7 @@ const ServicesMentalHealth = () => {
     <section className="font-main bg-white py-10">
       <header className="bg-mentalHealth text-white w-full py-10">
         <div className="max-w-[86rem] mx-auto flex items-center justify-between px-4">
-          <article className="space-y-5 flex flex-col items-center lg:items-start">
+          <article className="space-y-5 flex flex-col items-center lg:items-start text-center w-full">
             <h2 className="w-full text-center lg:text-left text-[44px] lg:text-[88px] leading-[90px] max-w-[280px]">
               {t('mental health')}
             </h2>
@@ -31,7 +31,7 @@ const ServicesMentalHealth = () => {
               }
             </p>
             <Link to="/register">
-              <button className="rounded-full w-[260px] text-lg flex justify-center py-2 border bg-white text-mentalHealth border-white hover:opacity-80 transition-opacity ease-in-out duration-300">
+              <button className="rounded-full w-[260px] text-lg flex justify-center py-2 border bg-white text-[#3F3F3F] border-white hover:opacity-80 transition-opacity ease-in-out duration-300">
                 {t('sign-up-today')}
               </button>
             </Link>
@@ -41,7 +41,7 @@ const ServicesMentalHealth = () => {
       </header>
 
       <section className="bg-white py-8 lg:py-14">
-        <div className="hidden relative virtual-health text-center lg:flex justify-center flip-words mb-32 px-4">
+        <div className="relative virtual-health text-center flex justify-center flip-words mb-32 px-4">
           <h2 className="text-primary text-center text-5xl md:text-6xl">
             {t('get-help-for')}
           </h2>
@@ -49,7 +49,7 @@ const ServicesMentalHealth = () => {
             <div className="w-full h-16 overflow-y-hidden flex justify-center">
               {
                 WORDS.map(word => (
-                  <span className="md:text-6xl">
+                  <span className="text-5xl md:text-6xl">
                     {t(`${word}`)}
                   </span>
                 ))
@@ -58,28 +58,28 @@ const ServicesMentalHealth = () => {
           </div>
         </div>
 
-        <MentalHealthServices/>
+        <MentalHealthServices />
 
         <h3 className="text-center text-4xl md:text-5xl lg:pt-32 py-10 lg:py-20">
           {t('mdhub-members-can-benefit-from')}
         </h3>
         <div className="w-full max-w-4xl mx-auto flex flex-col lg:flex-row item-center justify-between text-center">
-          <article className="space-y-6 flex flex-col items-center">
+          <article className="space-y-6 lg:w-[151px] flex flex-col items-center">
             <img src={MentalHealthImg} alt="mental health" />
             <p className="font-body">{t('mental-health-physicians')}</p>
           </article>
-          <article className="space-y-6 pt-6 flex flex-col items-center">
+          <article className="space-y-6 lg:w-[151px]  flex flex-col items-center">
             <img src={HearBrainImage} alt="heart and brain" />
             <p className="md:pt-2 font-body">{t('mental-health-therapists')}</p>
           </article>
-          <article className="space-y-6 flex flex-col items-center md:-mt-3">
+          <article className="space-y-6 lg:w-[151px] flex flex-col items-center mt-3">
             <img src={MeditationImage} alt="meditation" />
             <p className="max-w-[180px] font-body">{t('headspace-wellness-and-meditation-app-0')}</p>
           </article>
         </div>
       </section>
 
-      <section className="flex flex-col lg:flex-row justify-center items-center gap-x-12 px-4 text-center lg:text-left py-10 lg:py-20">
+      <section className="flex flex-col md:flex-row-reverse flex-col-reverse justify-center items-center gap-x-12 px-4 text-center lg:text-left py-10 lg:py-20 bg-[#F6E2B0]">
         <div>
           <img src={HeadspaceLogo} alt="Headspace" />
         </div>
@@ -88,54 +88,66 @@ const ServicesMentalHealth = () => {
           <p className="font-body">
             {t('you-can-think-of-headspace-as-your-minds-best-friend-through-science-backed-meditation-and-mindfulness-tools-headspace-helps-you-create-life-changing-habits-to-support-your-mental-health-and-find-a-healthier-happier-you-with-your-mdhub-membership-you-can-enjoy-3-months-free-with-your-mdhub-membership-download-the-app-and-start-feeling-better-in-just-a-few-minutes-a-day')}
           </p>
+          <p className="font-body">
+            {t('Download the app and start feeling better in just a few minutes a day.')}
+          </p>
         </div>
       </section>
       <section className="py-10 lg:py-20">
-        <h2 className="text-4xl text-center pb-16">{t('how-it-works')}</h2>
-        <div className="w-full px-4 max-w-6xl mx-auto flex flex-col lg:flex-row gap-y-5 justify-between">
-          <article className="w-full max-w-[464px] shadow-glow py-5 px-8">
-            <h2 className="text-3xl">{t('see-a-mental-health-physician')}</h2>
+        <div className="w-full px-4 max-w-6xl mx-auto flex flex-col lg:flex-col gap-y-5 justify-between">
+          <div>
+            <h2 className="text-4xl text-left">{t('how-it-works')}</h2>
             <p className="font-body py-7">
-              {t('log-into-your-mdhub-account-and-select-mental-health-physician-from-the-menu-of-specialists-next-choose-a-date-and-time-that-works-for-you-quickly-speak-with-a-mental-health-physician-for-an-initial-assessment-or-follow-up-30-minute-initial-appointments-are-315-and-30-minute-follow-up-appointments-are-230')}
+              {t('Our frontline care can be accessed 24/7 by simply logging into your virtual doctor platform and seeing a general doctor. For a more specialized practitioner, members are encouraged to book an on-demand appointment with a mental health physician or therapist.')}
             </p>
-            <Link to="/register">
-              <button
-                type="button"
-                className={`rounded-full w-72 font-main group hover:ring-1 hover:ring-primary py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent px-4`}
-              >
-                {t('sign-up-today')}
-              </button>
-            </Link>
-          </article>
-          <article className="w-full max-w-[464px] shadow-glow py-5 px-8">
-            <h2 className="text-3xl">{t('see-a-mental-health-therapist')}</h2>
-            <p className="font-body py-7">
-              {t('see-a-mental-health-therapist-with-no-appointment-necessary-all-you-need-to-do-is-choose-the-topic-youd-like-to-discuss-with-a-therapist-and-book-an-appointment-sessions-are-50-minutes-in-length-the-fee-for-a-session-ranges-from-120-160-depending-on-your-region-if-you-have-insurance-this-fee-is-often-covered-by-health-benefits')}
-            </p>
-            <Link to="/register">
-              <button
-                type="button"
-                className={`rounded-full w-72 font-main group hover:ring-1 hover:ring-primary py-2 border flex justify-center items-center space-x-2  border-primary bg-transparent px-4`}
-              >
-                {t('sign-up-today')}
-              </button>
-            </Link>
-          </article>
+          </div>
+          <div className='flex justify-between'>
+            <article className="w-full max-w-[528px] bg-[#ECB118] py-5 px-8 rounded-lg">
+              <h2 className="text-3xl">{t('See a mental health therapist')}</h2>
+              <p className="font-body py-7">
+                {t('Access your MDHUB account and navigate to the specialists menu. Select "Mental Health Physician" from the options provided. Then, pick a suitable date and time. You can discuss your mental health concerns during an initial assessment or follow-up session with a mental health physician.')}
+              </p>
+              <Link to="/register">
+                <button
+                  type="button"
+                  className={`rounded-full font-main group py-2 border-none flex justify-center items-center space-x-2  bg-[#3F3F3F] px-4 flex w-full text-white`}
+                >
+                  {t('sign-up-today')}
+                </button>
+              </Link>
+            </article>
+            <article className="w-full max-w-[528px] bg-[#ECB118] py-5 px-8">
+              <h2 className="text-3xl">{t('see-a-mental-health-therapist')}</h2>
+              <p className="font-body py-7">
+                {t('Talk to a mental health therapist without scheduling an appointment. Simply access your MDHUB account and navigate to the specialists menu. Select “Mental Health Therapist” from the options provided and choose the topic you’d like to discuss, then book a session.')}
+              </p>
+              <Link to="/register">
+                <button
+                  type="button"
+                  className={`rounded-full font-main group  py-2 border-none flex justify-center items-center space-x-2  bg-[#3F3F3F] px-4 flex w-full text-white mt-1`}
+                >
+                  {t('sign-up-today')}
+                </button>
+              </Link>
+            </article>
+          </div>
+
         </div>
       </section>
       <section className="py-10 font-body w-full max-w-[72rem] mx-auto flex flex-col gap-y-5 px-4">
         {
           mentalHealthFAQData.map(faqItem => (
-            <FAQ 
+            <FAQ
               key={faqItem.id}
               question={t(`${faqItem.question}`)}
+              bgColor={"#ECB118"}
             >
-                <p>
+              <p>
                 {t(`${faqItem.answer}`)}
-                </p>
+              </p>
             </FAQ>
           ))
-        }  
+        }
       </section>
     </section>
   )
