0919245413b33fd2f65056a863c5458a47b94caf
asfand687 <asfandyar687@gmail.com>
2023-04-16 19:54:48 +0500

    changed home slider

--- src/components/HomepageSlider.jsx
+++ src/components/HomepageSlider.jsx
@@ -52,11 +52,117 @@ const HomepageSlider = () => {
     return <ul className="custom-dots">{customDots}</ul>;
   };
   return (
-    <section>
-      <div className="hidden slick-lg lg:block mx-auto font-main h-[612px] px-24 text-primary w-full max-w-[1500px]">
+    <section className="pb-6">
+      <div className="font-main px-4 md:px-8 w-full max-w-7xl mx-auto">
         <h2 className="text-7xl font-main text-center text-dark pb-10">{t('essential-medical-services')}</h2>
-        <Slider {...settings}>
-          <article className="flex justify-center">
+        <div className="flex justify-center gap-x-8">
+          <article className="w-full max-w-[250px]">
+            <div className="bg-[#ECB118] text-white p-6  rounded-[8px] flex flex-col justify-between relative md:h-[490px]">
+              <div className="flex justify-end">
+                <Link to="/services/mental-health">
+                  <img src={RedirectIcon} alt="redirect" />
+                </Link>
+              </div>
+              <div>
+                <div className="w-[79px] h-[54px] flex justify-center my-12 mx-auto">
+                  <SliderMentalHealth />
+                </div>
+                <h2 className={`${i18n.language === "en" ? "text-3xl" : "text-[26px]"} pt-8 pb-4 font-semibold`}>{t('mental-health')}</h2>
+                <p className="font-body pb-4 text-sm">
+                  {t('take-control-of-your-mental-well-being-easily-get-mental-health-support-from-physicians-and-therapists')}
+                </p>
+              </div>
+            </div>
+          </article>
+          <article className="w-full max-w-[250px]">
+            <div className="bg-[#A293FF] text-white p-6  rounded-[8px] flex flex-col justify-between relative md:h-[490px]">
+              <div className="flex justify-end">
+                <Link to="/services">
+                  <img src={RedirectIcon} alt="redirect" />
+                </Link>
+              </div>
+              <div>
+                <div className="w-[79px] h-[54px] flex justify-center my-12 mx-auto">
+                  <SliderVirtualCare />
+                </div>
+                <h2 className={`capitalize ${i18n.language === "en" ? "text-3xl" : "text-[26px]"} pt-8 pb-4 font-semibold`}>
+                  {t('virtual-care')}
+                </h2>
+                <p className="font-body pb-4 text-sm">
+                  {t('connect-with-a-virtual-doctor-24-7-in-minutes-or-book-an-appointment-with-a-specialist')}
+                </p>
+              </div>
+            </div>
+          </article>
+          <article className="w-full max-w-[250px]">
+            <div className="bg-[#90C387] text-white p-6  rounded-[8px] flex flex-col justify-between relative md:h-[490px]">
+              <div className="flex justify-end">
+                <Link to="/services/online-pharmacy">
+                  <img src={RedirectIcon} alt="redirect" />
+                </Link>
+              </div>
+              <div>
+                <div className="w-[79px] h-[54px] flex justify-center my-12 mx-auto">
+                  <SliderOnlinePharmacy />
+                </div>
+                <h2 className={`capitalize ${i18n.language === "en" ? "text-3xl" : "text-[26px]"} pt-8 pb-4 font-semibold`}>
+                  {t('pharmacy-0')}
+                </h2>
+                <p className="font-body pb-4 text-sm">
+                  {
+                    t('get-prescriptions-filled-or-renewed-using-our-affiliate-online-pharmacy-and-delivered-right-to-your-door')
+                  }
+                </p>
+              </div>
+            </div>
+          </article>
+          <article className="w-full max-w-[250px]">
+            <div className="bg-diagnostics text-white p-6  rounded-[8px] flex flex-col justify-between relative md:h-[490px]">
+              <div className="flex justify-end">
+                <Link to="/services/online-pharmacy">
+                  <img src={RedirectIcon} alt="redirect" />
+                </Link>
+              </div>
+              <div>
+                <div className="flex justify-center mt-12 mx-auto pb-6">
+                  <img className="w-[90px]" src={SliderDiagnostics} alt="Diagnostics" />
+                </div>
+                <div className="md:relative md:-top-1">
+                  <h2 className={`capitalize ${i18n.language === "en" ? "text-3xl" : "text-[26px]"} pb-4 font-semibold`}>
+                    Diagnostics
+                </h2>
+                  <p className="font-body pb-4 text-sm">
+                    {t('get-safe-and-professional-lab-testing-from-the-comfort-of-your-home-or-office')}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article className="w-full max-w-[250px]">
+            <div className="bg-[#F78839] text-white p-6  rounded-[8px] flex flex-col justify-between relative md:h-[490px]">
+              <div className="flex justify-end">
+                <Link to="/services/nursing-homecare">
+                  <img src={RedirectIcon} alt="redirect" />
+                </Link>
+              </div>
+              <div>
+                <div className="md:relative md:top-6 w-[79px] h-[54px] flex justify-center my-12 mx-auto">
+                  <SliderNursingHomecare />
+                </div>
+                <div className="md:relative md:top-6">
+                  <h2 className={`capitalize ${i18n.language === "en" ? "text-3xl" : "text-[26px]"} pt-8 pb-4 font-semibold`}>
+                    {t('nursing-and-homecare')}
+                  </h2>
+                  <p className="font-body pb-4 text-sm">
+                    {t('get-professional-care-from-our-team-of-nurses-and-homecare-helpers-that-come-directly-to-you')}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+        </div>
+        {/* <div className="flex flex-wrap">
+          <article className="w-[206px] flex justify-center">
             <Link className="block" to="/services/mental-health">
               <div className="bg-[#ECB118] text-white p-6 ml-6 h-[432px] w-[320px] 3xl:w-[330px] rounded-[8px] flex flex-col justify-end relative">
                 <div className="absolute bottom-0 left-0 w-full flex flex-row-reverse p-4">
@@ -74,7 +180,7 @@ const HomepageSlider = () => {
               </div>
             </Link>
           </article>
-          <article className="flex justify-center">
+          <article className="w-[206px] flex justify-center">
             <Link className="block" to="/services">
               <div className="bg-[#A293FF] text-white p-6 ml-6 mx-auto h-[432px] w-[320px] 3xl:w-[330px] rounded-[8px] flex flex-col justify-end relative">
                 <div className="absolute bottom-0 left-0 w-full flex flex-row-reverse p-4">
@@ -92,7 +198,7 @@ const HomepageSlider = () => {
               </div>
             </Link>
           </article>
-          <article className="flex justify-center">
+          <article className="w-[206px] flex justify-center">
             <Link className="block" to="/services/online-pharmacy">
               <div className="bg-[#90C387] text-white p-6 ml-6 mx-auto h-[432px] w-[320px] 3xl:w-[330px] rounded-[8px]  flex flex-col justify-end relative">
                 <div className="absolute bottom-0 left-0 w-full flex flex-row-reverse p-4">
@@ -112,7 +218,7 @@ const HomepageSlider = () => {
               </div>
             </Link>
           </article>
-          <article className="flex justify-center">
+          <article className="w-[206px] flex justify-center">
             <Link to="/services/nursing-homecare">
               <div className="bg-[#F78839] text-white p-6 ml-6 mx-auto h-[432px] w-[320px] 3xl:w-[330px] rounded-[8px] flex flex-col justify-end relative">
                 <div className="absolute bottom-0 left-0 w-full flex flex-row-reverse p-4">
@@ -132,7 +238,7 @@ const HomepageSlider = () => {
               </div>
             </Link>
           </article>
-          <article className="flex justify-center">
+          <article className="w-[206px] flex justify-center">
             <Link to="/services/diagnostics">
               <div className="bg-diagnostics text-white p-6 ml-6 mx-auto h-[432px] w-[320px] 3xl:w-[330px] rounded-[8px] flex flex-col justify-end relative">
                 <div className="absolute bottom-0 left-0 w-full flex flex-row-reverse p-4">
@@ -152,109 +258,10 @@ const HomepageSlider = () => {
               </div>
             </Link>
           </article>
-        </Slider>
+        </div> */}
       </div>
 
-      <div className="slick-mobile block lg:hidden font-main">
-        <Slider {...mobileSliderSettings}>
-          <article>
-            <Link className="block" to="/services/mental-health">
-              <div className="bg-[#ECB118] text-white p-6 w-[300px]  rounded-[8px] mx-auto flex flex-col justify-end relative">
-                <div className="absolute bottom-0 left-0 w-full flex flex-row-reverse p-4">
-                  <Link to="/services/mental-health">
-                    <img src={RedirectIcon} alt="redirect" />
-                  </Link>
-                </div>
-                <div className="flex justify-center pb-20">
-                  <SliderMentalHealth />
-                </div>
-                <h2 className={`${i18n.language === "en" ? "text-3xl" : "text-[26px]"} pb-4`}>{t('mental-health')}</h2>
-                <p className="font-body pb-4">
-                  {t("In today's faced-paced, modern and ultra-connected world, your mental health is more important than ever. With MDHUB, you can easily get mental health support when you need it most.")}
-                </p>
-              </div>
-            </Link>
-          </article>
-          <article>
-            <Link className="block" to="/services">
-              <div className="bg-[#A293FF] text-white p-6 w-[300px]  3xl:w-[330px] rounded-[8px] mx-auto flex flex-col justify-end relative">
-                <div className="absolute bottom-0 left-0 w-full flex flex-row-reverse p-4">
-                  <Link to="/services">
-                    <img src={RedirectIcon} alt="redirect" />
-                  </Link>
-                </div>
-                <div className="flex justify-center pb-20">
-                  <SliderVirtualCare />
-                </div>
-                <h2 className={`${i18n.language === "en" ? "text-3xl" : "text-[26px]"} pb-4`}>{t('virtual-care')}</h2>
-                <p className="font-body pb-4">
-                  {t('connect-with-a-virtual-doctor-in-minutes-24-7-or-book-an-appointment-with-a-specialist-no-referral-needed')}
-                </p>
-              </div>
-            </Link>
-          </article>
-          <article>
-            <Link className="block" to="/services/online-pharmacy">
-              <div className="bg-[#90C387] text-white p-6 w-[300px]  3xl:w-[330px] mx-auto rounded-[8px] flex flex-col justify-end relative">
-                <div className="absolute bottom-0 left-0 w-full flex flex-row-reverse p-4">
-                  <Link to="/services/online-pharmacy">
-                    <img src={RedirectIcon} alt="redirect" />
-                  </Link>
-                </div>
-                <div className="flex justify-center pb-20">
-                  <SliderOnlinePharmacy />
-                </div>
-                <h2 className={`${i18n.language === "en" ? "text-3xl" : "text-[26px]"} pb-4`}>{t('pharmacy-0')}</h2>
-                <p className="font-body pb-4">
-                  {
-                    t('get-prescriptions-filled-or-renewed-using-our-affiliate-online-pharmacy-and-delivered-right-to-your-door')
-                  }
-                </p>
-              </div>
-            </Link>
-          </article>
-          <article>
-            <Link to="/services/nursing-homecare">
-              <div className="bg-[#F78839] text-white p-6 w-[300px]  mx-auto 3xl:w-[330px] rounded-[8px] flex flex-col justify-end relative">
-                <div className="absolute bottom-0 left-0 w-full flex flex-row-reverse p-4">
-                  <Link to="/services/nursing-homecare">
-                    <img src={RedirectIcon} alt="redirect" />
-                  </Link>
-                </div>
-                <div className="flex justify-center pb-20">
-                  <SliderNursingHomecare />
-                </div>
-                <h2 className={`${i18n.language === "en" ? "text-3xl" : "text-[20px]"} pb-4`}>
-                  {t('nursing-and-homecare')}
-                </h2>
-                <p className="font-body pb-4">
-                  {t('get-professional-care-from-our-team-of-nurses-and-homecare-helpers-that-come-directly-to-you')}
-                </p>
-              </div>
-            </Link>
-          </article>
-          <article>
-            <Link to="/services/diagnostics">
-              <div className="bg-diagnostics text-white p-6 w-[300px]  mx-auto 3xl:w-[330px] rounded-[8px] flex flex-col justify-end relative">
-                <div className="absolute bottom-0 left-0 w-full flex flex-row-reverse p-4">
-                  <Link to="/services/diagnostics">
-                    <img src={RedirectIcon} alt="redirect" />
-                  </Link>
-                </div>
-                <div className="flex justify-center pb-20">
-                  <img src={SliderDiagnostics} alt="Diagnostics" />
-                </div>
-                <h2 className={`${i18n.language === "en" ? "text-3xl" : "text-[20px]"} pb-4`}>
-                  {t('lab-testing-and-diagnostics-0')}
-                </h2>
-                <p className={`font-body pb-4 ${i18n.language === "en" ? "text-base" : "text-[16.5px]"}`}>
-                  {t('get-safe-and-professional-lab-testing-from-the-comfort-of-your-home-or-office')}
-                </p>
-              </div>
-            </Link>
-          </article>
-        </Slider>
-      </div>
+
     </section>
   )
 }
