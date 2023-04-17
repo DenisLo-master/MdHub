ee6df63e6fa35c4a943bd8898c3e329a2a7be4f5
asfand687 <asfandyar687@gmail.com>
2023-04-17 15:55:12 +0500

    removed margin top

--- src/sections/Pricing/HeadspaceSection.jsx
+++ src/sections/Pricing/HeadspaceSection.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next'
 const HeadspaceSection = () => {
   const { t } = useTranslation()
   return (
-    <div className="py-32 text-white bg-headspace font-main px-4">
+    <div className="md:mt-0 py-32 text-white bg-headspace font-main px-4">
       <article className="flex flex-col-reverse lg:flex-row-reverse justify-around items-center">
         <h3 className="text-3xl max-w-[600px] text-center lg:text-left">
           {t('free-3-months-of-headspace-membership-included-in-all-our-plans')}. ($36 {t('value')})
