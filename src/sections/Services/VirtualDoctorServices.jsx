d18fcaefe3e2c6fed2c3dd035a5dae59b87e07a8
asfand687 <asfandyar687@gmail.com>
2023-04-19 14:29:29 +0500

    fixed background colors

--- src/sections/Services/VirtualDoctorServices.jsx
+++ src/sections/Services/VirtualDoctorServices.jsx
@@ -20,7 +20,7 @@ const VirtualDoctorServices = () => {
   const { i18n, t } = useTranslation()
 
   return (
-    <section className="bg-virtualDoctor text-dark w-full lg:h-[530px] py-10 relative">
+    <section className="bg-[#DDD8FF] text-dark w-full lg:h-[530px] py-10 relative">
       <div className="max-w-[83rem] mx-auto flex flex-col justify-center h-full">
         <article className="px-4 space-y-5 flex flex-col items-center lg:items-start text-center">
           <h2 className="text-5xl lg:text-[48px] text_black font-semibold max-w-lg text-left">
@@ -32,10 +32,10 @@ const VirtualDoctorServices = () => {
             </p>
           </div>
           <Link className="block" to="/register">
-            <button className="rounded-full w-[480px] mt-[48px] text-lg flex justify-center py-3 border bg-[#3F3F3F] border-none hover:opacity-80 transition-opacity ease-in-out duration-300 text-white font-helvetica">
+            <button className="rounded-full w-[480px] mt-[48px] text-lg flex justify-center py-3 border bg-[#3F3F3F] hover:bg-[#1EBC91] border-none hover:opacity-80 transition-all ease-in-out duration-300 text-white font-helvetica">
               {t('sign-up-today')}
             </button>
-            <button className="rounded-full w-[480px] mt-8 text-lg flex justify-center py-3 border bg-white border-none hover:opacity-80 transition-opacity ease-in-out duration-300 text-[#3F3F3F] font-helvetica">
+            <button className="rounded-full w-[480px] mt-8 text-lg flex justify-center py-3 border bg-white hover:bg-[#1EBC91] border-none hover:opacity-80 transition-all ease-in-out duration-300 text-[#3F3F3F] hover:text-white font-helvetica">
               {t('get-care-now')}
             </button>
           </Link>
